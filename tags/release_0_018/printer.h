f2da957f60457643efffad47e732ca7a4517ea35
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1998-04-02 13:36:47 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_018'.

---
+++ tags/release_0_018/printer.h
@@ -0,0 +1,35 @@
+// printer.h --- Support for printing alist structures.
+
+#ifndef PRINTER_H
+#define PRINTER_H
+
+#include "librarian.h"
+
+class Printer
+{
+  // Content.
+public:
+  const string name;
+
+  // Interface.
+public:
+  // Print comment.
+  virtual void print_comment (const string& comment) = 0;
+  // Print content of alist.
+  virtual void print_alist (const AttributeList& alist, const Syntax&) = 0;
+  // Print all elements in all libraries associated with `filename'.
+  virtual void print_library_file (const string& filename) = 0;
+
+  // True iff no errors have occured.
+  virtual bool good () = 0;
+
+  // Create and Destroy.
+protected:
+  Printer (const string& name);
+public:
+  virtual ~Printer ();
+};
+
+static Librarian<Printer> Printer_init ("printer");
+
+#endif PRINTER_H
---
+++ tags/release_0_018/surface.h
@@ -0,0 +1,54 @@
+// surface.h
+
+#include "uzmodel.h"
+#include "im.h"
+
+struct AttributeList;
+struct Log;
+struct Filter;
+struct SoilWater;
+struct Soil;
+
+class Surface : public UZtop
+{
+  const double minimal_matter_flux;
+  const double lake;
+  double pond;
+  bool flux;
+  double EvapSoilSurface;
+  double Eps;
+  double T;
+  IM im;
+  IM im_flux;
+
+public:
+
+  // Communication with soil.
+  bool flux_top () const;	// From UZtop.
+  double q () const;
+  void flux_top_on ();
+  void flux_top_off ();
+  bool accept_top (double);
+  double ponding() const;
+  double temperature () const;
+
+  const IM& matter_flux ();
+
+  void SoilSurfaceConditions (double Theta, double h);
+
+  void clear ();
+
+  // Manager.
+  void fertilize (const IM&);
+
+  void output (Log&, Filter&) const;
+
+  // Communication with bioclimate.
+  double evaporation (double PotSoilEvaporation, double Water, double temp,
+		      const Soil&, const SoilWater&);
+
+  // Create.
+  static void load_syntax (Syntax&, AttributeList&);
+  Surface (const AttributeList& par);
+};
+
