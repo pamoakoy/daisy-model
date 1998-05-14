e97d91721eeea6e22088b43a64b2622be3cdd7c9
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1998-05-14 18:49:04 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_024'.

---
+++ tags/release_0_024/alist.h
@@ -0,0 +1,78 @@
+// alist.h -- attribute list
+
+#ifndef ALIST_H
+#define ALIST_H
+
+#include "common.h"
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <list>
+
+struct Time;
+struct CSMP;
+struct AttributeList;
+struct Syntax;
+
+class AttributeList
+{
+  // Content.
+  struct Implementation;
+public:				// EGCS require this to be public...
+  Implementation& impl;
+
+public:
+  static const AttributeList empty;
+
+  // Is `key' an element of this alist?
+  bool check (const string& key) const;
+  // Is this alist a subset of `other'?
+  bool subset (const AttributeList& other, const Syntax& syntax) const;
+  // Is the element `key' in this alist a subset of the correspi
+  bool subset (const AttributeList& other, const Syntax& syntax,
+	       const string& key) const;
+  int size (const string& key) const; // BUG: UNIMPLEMENTED.
+
+  double number (string) const;
+  const string& name (string) const;
+  bool flag (string) const;
+  const CSMP& csmp (string) const;
+  AttributeList& alist (string) const;
+  int integer (string) const;
+  const Time& time (string) const;
+  const vector<double>& number_sequence (string) const;
+  const vector<string>& name_sequence (string key) const;
+  const vector<bool>& flag_sequence (string key) const;
+  const vector<int>& integer_sequence (string key) const;
+  const vector<const Time*>& time_sequence (string key) const;
+  const vector<const CSMP*>& csmp_sequence (string key) const;
+  const vector<AttributeList*>& alist_sequence (string key) const;
+
+  // Create and Destroy.
+  void add (const string&, double);
+  void add (const string&, const char*);
+  void add (const string&, const string&);
+  void add (const string&, bool);
+  void add (const string&, int);
+  void add (const string&, AttributeList&);
+  void add (const string&, const CSMP&);
+  void add (const string&, const Time&);
+#if 1
+  void add (const string&, const vector<double>&);
+  void add (const string&, const vector<string>&);
+  void add (const string&, const vector<bool>&);
+  void add (const string&, const vector<int>&);
+  void add (const string&, const vector<AttributeList*>&);
+  void add (const string&, const vector<const CSMP*>&);
+  void add (const string&, const vector<const Time*>&);
+#endif
+  void operator += (const AttributeList&);
+  AttributeList (const AttributeList& old);
+  AttributeList ();
+  ~AttributeList ();
+};
+
+#endif ALIST_H
+
+// alist.h ends here
