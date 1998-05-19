8cb33a9694ea1a9986998f97611dd9de1c03be4b
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1998-05-19 17:04:04 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_027'.

---
+++ tags/release_0_027/im.C
@@ -0,0 +1,146 @@
+// im.C
+
+#include "im.h"
+#include "log.h"
+#include "alist.h"
+#include "syntax.h"
+
+void
+IM::output (Log& log, Filter& filter) const
+{
+  log.output ("NO3", filter, NO3);
+  log.output ("NH4", filter, NH4);
+}
+
+void IM::clear ()
+{ 
+  NO3 = 0.0;
+  NH4 = 0.0;
+}
+
+void
+IM::operator += (const IM& n)
+{ 
+  NO3 += n.NO3;
+  NH4 += n.NH4;
+}
+
+void
+IM::operator -= (const IM& n)
+{ 
+  NO3 -= n.NO3;
+  NH4 -= n.NH4;
+}
+
+void
+IM::operator *= (double n)
+{ 
+  NO3 *= n;
+  NH4 *= n;
+}
+
+void
+IM::operator /= (double n)
+{ 
+  *this *= (1.0 / n);
+}
+
+bool
+IM::empty () const
+{
+  return NO3 < 1e-20 && NH4 < 1e-20;
+}
+
+IM
+IM::operator* (double flux) const
+{
+  return IM (*this, flux);
+}
+
+IM
+IM::operator+ (const IM& im) const
+{
+  IM result (*this);
+  result += im;
+  return result;
+}
+
+double 
+IM::N_left (const AttributeList& al)
+{
+  const double N = al.number ("total_N_fraction");
+  const AttributeList& im  = al.alist ("im");
+  return N * (1.0 - im.number ("NO3") - im.number ("NH4"));
+}
+
+static double IM_get_NO3 (const AttributeList& al)
+{
+  if (al.check ("weight"))
+    {
+      assert (al.name ("syntax") == "organic");
+
+      const double weight = al.number ("weight") 
+	* al.number ("dry_matter_fraction") 
+	* 0.1;			// kg / m² --> g / cm²
+      const double N = weight * al.number ("total_N_fraction");
+
+      IM im (al.alist ("im"));
+      
+      return N * im.NO3;
+    }
+  else 
+    return al.number ("NO3");
+}
+
+static double IM_get_NH4 (const AttributeList& al)
+{
+  if (al.check ("weight"))
+    {
+      assert (al.name ("syntax") == "organic");
+
+      const double weight = al.number ("weight") 
+	* al.number ("dry_matter_fraction") 
+	* 0.1;			// kg / m² --> g / cm²
+      const double N = weight * al.number ("total_N_fraction");
+
+      IM im (al.alist ("im"));	// This extract NH4 evaporation.
+      
+      return N * im.NH4;
+    }
+  else 
+    return al.number ("NH4") * (1.0 - al.number ("NH4_evaporation"));
+}
+
+IM::IM ()
+  : NO3 (0.0),
+    NH4 (0.0)
+{ }
+
+IM::IM (const IM& im)
+  : NO3 (im.NO3),
+    NH4 (im.NH4)
+{ }
+
+IM::IM (const AttributeList& al)
+  : NO3 (IM_get_NO3 (al)),
+    NH4 (IM_get_NH4 (al))
+{ }
+
+IM::IM (const IM& n, double flux)
+  : NO3 (n.NO3 * flux),
+    NH4 (n.NH4 * flux)
+{ }
+
+IM::~IM ()
+{ }
+
+void 
+IM::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  syntax.add ("NO3", Syntax::Number, Syntax::State);
+  alist.add ("NO3", 0.0);
+  syntax.add ("NH4", Syntax::Number, Syntax::State);
+  alist.add ("NH4", 0.0);
+  syntax.add ("NH4_evaporation", Syntax::Number, Syntax::Const);
+  alist.add ("NH4_evaporation", 0.0);
+}
---
+++ tags/release_0_027/nitrification_solute.C
@@ -0,0 +1,145 @@
+// nitrification_solute.C
+
+#include "nitrification.h"
+#include "soil.h"
+#include "soil_water.h"
+#include "soil_heat.h"
+#include "soil_NH4.h"
+#include "soil_NO3.h"
+#include "csmp.h"
+#include "log.h"
+#include "mathlib.h"
+#include "groundwater.h"
+
+class NitrificationSolute : public Nitrification
+{
+  // Parameters.
+private: 
+  const bool active_underground; // True, iff turnover happens below rootzone.
+  const bool active_groundwater; // True, iff turnover happens in groundwater.
+  const double k;
+  const double k_10;
+
+  // Log variable.
+private:
+  vector<double> converted;
+  
+  // Simulation.
+public:
+  void output (Log&, Filter&) const;
+  void tick (const Soil&, const SoilWater&, const SoilHeat&, 
+	     SoilNO3&, SoilNH4&, const Groundwater&);
+
+  // Create.
+public:
+  NitrificationSolute (const AttributeList&);
+};
+
+static double f_h (double h)
+{
+  if (h >= 0.0)
+    return 0.6;
+
+  const double pF = h2pF (h);
+
+  if (pF <= 0.0)
+    return 0.0;
+  if (pF <= 1.5)
+    return pF / 1.5;
+  if (pF <= 2.5)
+    return 1.0;
+  if (pF <= 5.0)
+    return 1.0 - (pF - 2.5) / (5.0 - 2.5);
+  
+  return 0.0;
+}
+      
+static double f_T (double T)
+{
+#if 1
+  if (T < 2.0)
+    return 0.0;
+  if (T < 6.0)
+    return 0.15 * (T - 2.0);
+  if (T < 20.0)
+    return 0.10 * T;
+  if (T < 40)
+    return exp (0.47 - 0.027 * T + 0.00193 * T * T);
+#else
+  if (T < 1.0)
+    return 0.0;
+  if (T < 5.0)
+    return 0.125 * (T - 1.0);
+  if (T < 20.0)
+    return 0.10 * T;
+  if (T < 40)
+    return exp (0.47 - 0.027 * T + 0.00193 * T * T);
+#endif
+  assert (false);
+  return -42.42e42;
+}
+
+void
+NitrificationSolute::output (Log& log, Filter& filter) const
+{
+  log.output ("converted", filter, converted, true);
+}
+
+void 
+NitrificationSolute::tick (const Soil& soil, const SoilWater& soil_water,
+			   const SoilHeat& soil_heat,
+			   SoilNO3& soil_NO3, SoilNH4& soil_NH4,
+			   const Groundwater& groundwater)
+{
+  converted.erase (converted.begin (), converted.end ());
+
+  unsigned int size = soil.size ();
+  if (!active_underground)
+    size = min (size, soil.interval_plus (soil.MaxRootingDepth ()));
+  if (!active_groundwater && !groundwater.flux_bottom ())
+    size = min (size, soil.interval_plus (groundwater.table ()));
+
+  for (unsigned int i = 0; i < size; i++)
+    {
+      const double C = soil_NH4.C (i);
+      const double h = soil_water.h (i);
+      const double T = soil_heat.T (i);
+      const double rate = k_10 * f_h (h) * f_T (T) * C / (k + C);
+      assert (rate >= 0.0);
+      const double M_new = min (rate, soil_NH4.M_left (i) / dt);
+      converted.push_back (M_new);
+    }
+  soil_NH4.add_to_sink (converted);
+  soil_NO3.add_to_source (converted);
+}
+
+NitrificationSolute::NitrificationSolute (const AttributeList& al)
+  : Nitrification (al.name ("type")),
+    active_underground (al.flag ("active_underground")),
+    active_groundwater (al.flag ("active_groundwater")),
+    k (al.number ("k")),
+    k_10 (al.number ("k_10"))
+{ }
+
+static struct NitrificationSoluteSyntax
+{
+  static Nitrification& make (const AttributeList& al)
+  {
+    return *new NitrificationSolute (al);
+  }
+
+  NitrificationSoluteSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    syntax.add ("active_underground", Syntax::Boolean, Syntax::Const);
+    alist.add ("active_underground", false);
+    syntax.add ("active_groundwater", Syntax::Boolean, Syntax::Const);
+    alist.add ("active_groundwater", false);
+    syntax.add ("converted", Syntax::Number, Syntax::LogOnly, 
+		Syntax::Sequence);
+    syntax.add ("k", Syntax::Number, Syntax::Const);
+    syntax.add ("k_10", Syntax::Number, Syntax::Const);
+    Librarian<Nitrification>::add_type ("solute", alist, syntax, &make);
+  }
+} NitrificationSolute_syntax;
