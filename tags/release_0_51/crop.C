c5e5515dc70f92dc78ea568e28640341f77db03f
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1998-12-22 10:58:58 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_51'.

---
+++ tags/release_0_51/crop.C
@@ -0,0 +1,48 @@
+ // crop.C
+
+#include "crop.h"
+#include "chemicals.h"
+
+const double Crop::DSremove = -5001.0;
+
+Librarian<Crop>::Content* Librarian<Crop>::content = NULL;
+
+double 
+Crop::water_stress () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::nitrogen_stress () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::rs_min () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::rs_max () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::albedo () const
+{ return 0.20; }
+
+void
+Crop::kill (const string& name, const Time& time, const Geometry& geometry,
+	    OrganicMatter& organic_matter, Bioclimate& bioclimate)
+{ 
+  Chemicals chemicals;
+  harvest (name, time, geometry, organic_matter, bioclimate,
+	   0.0, 0.0, 0.0, 0.0, true); 
+}
+
+bool
+Crop::ds_remove (const Crop* crop)
+{ return crop->DS () == Crop::DSremove; }
+
+Crop::Crop (const string& n)
+  : name (n)
+{ }
+
+Crop::~Crop ()
+{ }
---
+++ tags/release_0_51/snow.h
@@ -0,0 +1,42 @@
+// snow.h
+
+#ifndef SNOW_H
+#define SNOW_H
+
+struct AttributeList;
+struct Weather;
+struct Bioclimate;
+struct Syntax;
+struct Log;
+struct Filter;
+struct Soil;
+struct SoilWater;
+struct SoilHeat;
+
+class Snow
+{ 
+  struct Implementation;
+  Implementation& impl;
+
+  // Simulation.
+public:
+  void tick (const Soil&, const SoilWater&, const SoilHeat&,
+	     double Si, double q_h, double Prain,
+	     double Psnow, double T, double Epot);
+  void output (Log&, Filter&) const;
+
+  // Queries.
+public:
+  double percolation () const;
+  double temperature () const;
+  double evaporation () const;
+  double storage () const;
+
+  // Create & Destroy.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  Snow (const AttributeList& al);
+  ~Snow ();
+};
+
+#endif SNOW_H
