4195babb9ccd042472e2fdda8d91e39a8e9ff1e3
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1998-12-29 17:52:58 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_55'.

---
+++ tags/release_0_55/soil_NH4.h
@@ -0,0 +1,19 @@
+// soil_NH4.h
+
+#ifndef SOIL_NH4_H
+#define SOIL_NH4_H
+
+#include "solute.h"
+
+class SoilNH4 : public Solute
+{
+public:
+  // Substance specific constants.
+  double diffusion_coefficient () const; // in free solu. [m² / s]
+
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  SoilNH4 (const AttributeList&);
+};
+
+#endif SOIL_NH4_H
