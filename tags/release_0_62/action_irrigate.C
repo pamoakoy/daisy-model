8e14c89d3a90d6074a7e5d5096357e06f1ec16bf
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-01-13 18:04:54 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_62'.

---
+++ tags/release_0_62/action_irrigate.C
@@ -0,0 +1,87 @@
+// action_irrigate.C
+
+#include "action.h"
+#include "daisy.h"
+#include "weather.h"
+#include "field.h"
+#include "am.h"
+#include "im.h"
+
+struct ActionIrrigate : public Action
+{
+  static const double at_air_temperature;
+
+  const double flux;
+  const double temp;
+  const IM& sm;
+  
+  virtual void irrigate (Field&,
+			 double flux, double temp, const IM&) const = 0;
+
+  void doIt (Daisy& daisy)
+  {
+    COUT << "[Irrigating]\n";      
+    double t = temp;
+
+    if (temp == at_air_temperature) 
+      t = daisy.weather.hourly_air_temperature ();
+
+    irrigate (daisy.field, flux, t, sm);
+  }
+
+  ActionIrrigate (const AttributeList& al)
+    : Action (al.name ("type")),
+      flux (al.number ("flux")),
+      temp (al.number ("temperature")),
+      sm (*new IM (al.alist ("solute")))
+  { }
+public:
+  ~ActionIrrigate ()
+  { }
+};
+
+struct ActionIrrigateTop : public ActionIrrigate
+{
+  void irrigate (Field& f, double flux, double temp, const IM& im) const
+    { f.irrigate_top (flux, temp, im); }
+  ActionIrrigateTop (const AttributeList& al)
+    : ActionIrrigate (al)
+    { }
+};
+
+struct ActionIrrigateSurface : public ActionIrrigate
+{
+  void irrigate (Field& f, double flux, double temp, const IM& im) const
+    { f.irrigate_surface (flux, temp, im); }
+  ActionIrrigateSurface (const AttributeList& al)
+    : ActionIrrigate (al)
+    { }
+};
+
+
+const double ActionIrrigate::at_air_temperature = -500;
+
+#ifdef BORLAND_TEMPLATES
+template class add_submodule<IM>;
+#endif
+
+static struct ActionIrrigateSyntax
+{
+  static Action& make_top (const AttributeList& al)
+    { return *new ActionIrrigateTop (al); }
+  static Action& make_surface (const AttributeList& al)
+    { return *new ActionIrrigateSurface (al); }
+  ActionIrrigateSyntax ()
+    { 
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      syntax.add ("flux", Syntax::Number, Syntax::Const);
+      syntax.order ("flux");
+      syntax.add ("temperature", Syntax::Number, Syntax::Const);
+      alist.add ("temperature", ActionIrrigate::at_air_temperature);
+      add_submodule<IM> ("solute", syntax, alist);
+      Librarian<Action>::add_type ("irrigate_top", alist, syntax, &make_top);
+      Librarian<Action>::add_type ("irrigate_surface", alist, syntax,
+				   &make_surface);
+    }
+} ActionIrrigate_syntax;
---
+++ tags/release_0_62/log.C
@@ -0,0 +1,53 @@
+// log.C
+
+#include "log.h"
+#include "alist.h"
+#include "library.h"
+#include "syntax.h"
+#include "common.h"
+
+Librarian<Log>::Content* Librarian<Log>::content = NULL;
+
+struct Log::Implementation
+{
+  list<const Geometry*> geometries;
+};
+
+void 
+Log::open_alist (const string& name, const AttributeList&)
+{ open (name); }
+
+void 
+Log::close_alist ()
+{ close (); }
+
+void 
+Log::open_geometry (const Geometry& g)
+{ 
+  impl.geometries.push_back (&g);
+}
+
+void 
+Log::close_geometry ()
+{
+  impl.geometries.pop_back ();
+}
+
+const Geometry*
+Log::geometry ()
+{
+  return impl.geometries.back ();
+}
+
+void 
+Log::done ()
+{ }
+
+Log::Log ()
+  : impl (*new Implementation ())
+{ }
+
+Log::~Log ()
+{
+  delete &impl;
+}
