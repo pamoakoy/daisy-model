39b57de9922e8c89baf10f65c4f145f3d9ec3da8
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-06-16 11:19:01 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_79'.

---
+++ tags/release_0_79/denitrification.C
@@ -0,0 +1,107 @@
+// denitrification.C
+
+#include "denitrification.h"
+#include "alist.h"
+#include "syntax.h"
+#include "soil.h"
+#include "soil_water.h"
+#include "soil_heat.h"
+#include "organic_matter.h"
+#include "soil_NO3.h"
+#include "groundwater.h"
+#include "csmp.h"
+#include "log.h"
+#include "submodel.h"
+
+static double f_Theta (double x)
+{
+  if (x < 0.8)
+    return 0.0;
+  if (x < 0.9)
+    return 2.0 * (x - 0.8);
+  
+  return 0.2 + 8.0 * (x - 0.9);
+}
+
+static double f_T (double T)
+{
+  if (T < 2.0)
+    return 0.0;
+  if (T < 6.0)
+    return 0.15 * (T - 2.0);
+  if (T < 20.0)
+    return 0.10 * T;
+  if (T < 40)
+    return exp (0.47 - 0.027 * T + 0.00193 * T * T);
+
+  assert (false);
+  return -42.42e42;
+}
+
+void
+Denitrification::output (Log& log) const
+{
+  log.output ("converted", converted);
+}
+
+void Denitrification::tick (const Soil& soil, const SoilWater& soil_water,
+			    const SoilHeat& soil_heat,
+			    SoilNO3& soil_NO3, 
+			    const OrganicMatter& organic_matter,
+			    const Groundwater& groundwater)
+{
+  converted.erase (converted.begin (), converted.end ());
+
+  unsigned int size = soil.size ();
+  if (!active_underground)
+    size = min (size, soil.interval_plus (soil.MaxRootingDepth ()));
+  if (!active_groundwater && !groundwater.flux_bottom ())
+    size = min (size, soil.interval_plus (groundwater.table ()));
+
+  for (unsigned int i = 0; i < size; i++)
+    {
+      const double CO2 = organic_matter.CO2 (i);
+      const double Theta = soil_water.Theta (i);
+      const double T = soil_heat.T (i);
+      const double rate = f_Theta (Theta / soil.Theta (i, 0.0)) 
+	* f_T (T) * alpha * CO2 ;
+      const double M = min (rate, K * soil_NO3.M_left (i) / dt);
+      converted.push_back (M);
+    }
+  soil_NO3.add_to_sink (converted);
+}
+
+void
+Denitrification::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  alist.add ("submodel", "Denitrification");
+  alist.add ("description", "Denitrification in soil (conversion\n\
+of nitrate to atmospheric nitrogen).  In this model, it is made\n\
+proportional to the CO2 development, as specified by the parameter\n\
+alpha, with a maximum rate specified by the parameter `K'.  The\n\
+denitrification is also affected by temperature and water pressure.");
+  syntax.add ("active_underground", Syntax::Boolean, Syntax::Const, "\
+Set this flag to turn on denitrification below the root zone.");
+  alist.add ("active_underground", false);
+  syntax.add ("active_groundwater", Syntax::Boolean, Syntax::Const, "\
+Clear this flag to turn off denitrification in groundwater.");
+  alist.add ("active_groundwater", true);
+  syntax.add ("converted", "g/cm^3/h", Syntax::LogOnly, Syntax::Sequence,
+	      "Amount of denitrification.");
+  syntax.add ("K", "h^-1", Syntax::Const, "\
+Maximum fraction of nitrate converted at each time step.");
+  alist.add ("K", 0.020833);
+  syntax.add ("alpha", "(g NO3-N/h)/(g CO2-C/h)", Syntax::Const, 
+	      "Anaerobic denitrification constant.");
+  alist.add ("alpha", 0.1);
+}
+
+Denitrification::Denitrification (const AttributeList& al)
+  : active_underground (al.flag ("active_underground")),
+    active_groundwater (al.flag ("active_groundwater")),
+    K (al.number ("K")),
+    alpha (al.number ("alpha"))
+{ }
+
+static Submodel::Register 
+denitrification_submodel ("Denitrification", Denitrification::load_syntax);
---
+++ tags/release_0_79/hydraulic.C
@@ -0,0 +1,61 @@
+// hydraulic.C
+
+#include "hydraulic.h"
+#include "library.h"
+#include "alist.h"
+#include "syntax.h"
+#include "csmp.h"
+#include <vector>
+#include <map>
+
+bool 
+Hydraulic::compact () const
+{
+  return false;
+}
+
+void
+Hydraulic::K_to_M (CSMP& csmp, const int intervals) const
+{
+  static const double h0 = -20000.0;
+  const double Ksat = K (0.0);
+  const double max_change = pow (Ksat / K (h0), 1.0 / intervals);
+  double step = (0 - h0) / 4.0;
+
+  double h = h0;
+  double sum = 0.0;
+  while (h < 0)
+    {
+      csmp.add (h, sum);
+      step *= 2;
+      while (K (h + step) / K (h) > max_change)
+	step /= 2;
+      sum += step * (K (h) + K (h + step)) / 2;
+      h += step;
+    }
+  csmp.add (h, sum);
+}
+
+void
+Hydraulic::load_syntax (Syntax& syntax, AttributeList& alist)
+{ 
+  syntax.add ("Theta_sat", "cm^3 H2O/cm^3", Syntax::Const,
+	      "Saturation point.");
+  syntax.add ("Theta_res", "cm^3 H2O/cm^3", Syntax::Const,
+	      "Soil residual water.");
+  alist.add ("Theta_res", 0.0);
+}
+
+Hydraulic::Hydraulic (const AttributeList& al)
+  : Theta_sat (al.number ("Theta_sat")),
+    Theta_res (al.number ("Theta_res"))
+{ }
+
+Hydraulic::~Hydraulic ()
+{ }
+
+Librarian<Hydraulic>::Content* Librarian<Hydraulic>::content = NULL;
+
+const char *const Hydraulic::description = "\
+This component is responsible for specifying the soils hydraulic\n\
+properties.";
