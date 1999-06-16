f8ec327879b004d89b3ff487adc7976f1c9c4c15
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-06-16 14:01:00 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_80'.

---
+++ tags/release_0_80/denitrification.h
@@ -0,0 +1,45 @@
+// denitrification.h
+
+#ifndef DENITRIFICATION_H
+#define DENITRIFICATION_H
+
+#include "common.h"
+#include <vector>
+class AttributeList;
+class Syntax;
+class Soil;
+class SoilWater;
+class SoilHeat;
+class SoilNO3;
+class OrganicMatter;
+class CSMP;
+class Log;
+class Groundwater;
+
+class Denitrification
+{
+  // Parameters.
+private: 
+  const bool active_underground; // True, iff turnover happens below rootzone.
+  const bool active_groundwater; // True, iff turnover happens in groundwater.
+  const double K;
+  const double alpha;
+
+  // Log variable.
+private:
+  vector<double> converted;
+  
+  // Simulation.
+public:
+  void output (Log&) const;
+  void tick (const Soil&, const SoilWater&, const SoilHeat&, SoilNO3&,
+	     const OrganicMatter&, const Groundwater&);
+
+  // Create.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  Denitrification (const AttributeList&);
+};
+
+
+#endif DENITRIFICATION_H
---
+++ tags/release_0_80/soil_NO3.h
@@ -0,0 +1,19 @@
+// soil_NO3.h
+
+#ifndef SOIL_NO3_H
+#define SOIL_NO3_H
+
+#include "solute.h"
+
+class SoilNO3 : public Solute
+{
+public:
+  // Substance specific constants.
+  double diffusion_coefficient () const; // in free solu. [cm² / h]
+ 
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  SoilNO3 (const AttributeList&);
+};
+
+#endif SOIL_NO3_H
