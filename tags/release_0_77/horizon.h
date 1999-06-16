a2904ebc25c5d48260dbb2d9ab3e05ebf8ef7385
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-06-16 09:53:17 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_77'.

---
+++ tags/release_0_77/horizon.h
@@ -0,0 +1,50 @@
+// horizon.h
+
+#ifndef HORIZON_H
+#define HORIZON_H
+
+#include "librarian.h"
+
+struct Hydraulic;
+struct Tortuosity;
+
+// Weigth of mineral particles. [g / cm³]
+GLOBAL_CONSTANT const double rho_mineral = 2.65;	
+// Weight of humus. [g / cm³]
+GLOBAL_CONSTANT const double rho_humus = 1.3; 
+GLOBAL_CONSTANT const double c_fraction_in_humus = 0.587;
+
+class Horizon 
+{
+  // Content.
+private:
+  struct Implementation;
+  Implementation& impl;
+public:
+  static const char *const description;
+
+  // Water.
+public:
+  const Hydraulic& hydraulic;
+  double heat_conductivity (double Theta, double Ice) const;
+  double heat_capacity (double Theta, double Ice) const;
+  
+  // Texture.
+public:
+  const Tortuosity& tortuosity;
+  double dry_bulk_density () const;
+  double clay () const;
+  double humus () const;
+  double SOM_C (unsigned int pool) const;
+  double SOM_C_per_N (unsigned int pool) const;
+
+  // Create and Destroy.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  Horizon (const AttributeList&);
+  virtual ~Horizon ();
+};
+
+static Librarian<Horizon> Horizon_init ("horizon");
+
+#endif HORIZON_H
---
+++ tags/release_0_77/log_clone.C
@@ -0,0 +1,48 @@
+// log_clone.C
+//
+// Clone an object by using its log function.
+
+#include "log_clone.h"
+
+#ifdef BORLAND_PRAGMA
+#pragma warn -rvl
+#endif
+
+bool
+LogClone::match (const Daisy&)
+{ assert (false); return false; }
+
+#ifdef BORLAND_PRAGMA
+#pragma warn +rvl
+#endif
+
+bool 
+LogClone::check (const string &) const
+{ assert (false); return false; }
+void
+LogClone::done ()
+{ assert (false); }
+
+const AttributeList& 
+LogClone::result ()
+{ return alist (); }
+
+LogClone::LogClone (const string& name, 
+		    const Syntax& syntax, const AttributeList& alist)
+  : LogAList ()
+{ 
+  push (name, syntax, alist);
+}
+
+LogClone::~LogClone ()
+{
+  // Check stacks.
+  assert (syntax_stack.size () == 1U);
+  assert (alist_stack.size () == 1U);
+  assert (library_stack.size () == 1U);
+  
+  // Cleanup.
+  delete &alist ();
+  pop ();
+  assert (nested == 0);
+}
