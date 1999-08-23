b39cddc4dc3a06123f1046f8c368e62b407a98fe
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-08-23 16:10:38 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_88'.

---
+++ tags/release_0_88/groundwater.C
@@ -0,0 +1,35 @@
+// groundwater.C
+
+#include "groundwater.h"
+#include "log.h"
+
+Librarian<Groundwater>::Content* Librarian<Groundwater>::content = NULL;
+
+const char *const Groundwater::description = "\
+The `groundwater' component is responsible for specifying the\n\
+groundwater table at each timestep.";
+
+void
+Groundwater::output (Log& log) const
+{ 
+  log.output ("height", table ());
+}
+
+void
+Groundwater::load_syntax (Syntax& syntax, AttributeList&)
+{
+  syntax.add ("height", "cm", Syntax::LogOnly,
+	      "Groundwater level.  Positive numbers indicate free drainage.");
+}
+
+void
+Groundwater::initialize (const Time&, const Soil&)
+{ }
+
+Groundwater::Groundwater (const AttributeList& al)
+  : name (al.name ("type"))
+{ }
+
+Groundwater::~Groundwater ()
+{ }
+
