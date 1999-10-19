ad071c88369135c1dc7030082ab21c1410ffb1cf
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-10-19 14:37:01 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_94'.

---
+++ tags/release_0_94/weather.C
@@ -0,0 +1,201 @@
+// weather.C
+
+#include "weather.h"
+#include "time.h"
+#include "net_radiation.h"
+#include "log.h"
+
+Librarian<Weather>::Content* Librarian<Weather>::content = NULL;
+
+const char *const Weather::description = "\
+Meteorological data, as well as the global positioning, are the\n\
+responsibility of the `weather' component, typically be reading the\n\
+data from a file.  The meteorological data are common to all columns.";
+
+void 
+Weather::tick (const Time& time)
+{
+  day_length_ = day_length (time);
+  assert (day_length_ >= 0.0);
+  assert (day_length_ <= 24.0);
+
+  if (time.hour () > 12 + day_length () || time.hour () < 12 - day_length ())
+    day_cycle_ = 0.0;
+  else
+    day_cycle_ = max (0.0, M_PI_2 / day_length ()
+		     * cos (M_PI * (time.hour () - 12) / day_length ()));
+  assert (day_cycle_ >= 0.0);
+  assert (day_cycle_ <= 1.0);
+}
+
+void
+Weather::output (Log& log) const
+{ 
+  log.output ("hourly_air_temperature", hourly_air_temperature ());
+  log.output ("daily_air_temperature", daily_air_temperature ());
+  log.output ("hourly_global_radiation", 
+	      hourly_global_radiation ());
+  log.output ("daily_global_radiation", 
+	      daily_global_radiation ());
+  log.output ("daily_extraterrastial_radiation", 
+	      daily_extraterrastial_radiation ());
+  log.output ("reference_evapotranspiration", 
+	      reference_evapotranspiration ());
+  log.output ("rain", rain ());
+  log.output ("snow", snow ());
+  log.output ("cloudiness", cloudiness ());
+  log.output ("vapor_pressure", vapor_pressure ());
+  log.output ("wind", wind ()); 
+  log.output ("day_length", day_length ());
+  log.output ("day_cycle", day_cycle ());
+}
+
+double
+Weather::day_length (const Time& time) const
+{
+  double t = 2 * M_PI / 365 * time.yday ();
+  
+  double Dec = (0.3964 - 22.97 * cos (t) + 3.631 * sin (t)
+		- 0.03885 * cos (2 * t) 
+		+ 0.03838 * sin (2 * t) - 0.15870 * cos (3 * t) 
+		+ 0.07659 * sin (3 * t) - 0.01021 * cos (4 * t));
+  t = (24 / M_PI
+       * acos (-tan (M_PI / 180 * Dec) * tan (M_PI / 180 * latitude)));
+  return (t < 0) ? t + 24.0 : t;
+}
+
+double
+Weather::LatentHeatVaporization (double Temp) // [MJ/kg]
+{ return (2.501 - 2.361e-3 * Temp); }
+
+double 
+Weather::PsychrometricConstant (double AtmPressure, double Temp) // [kPa/K]
+{ return (0.00163 * AtmPressure / LatentHeatVaporization (Temp)); }
+
+double 
+Weather::AirDensity (double AtmPressure, double Temp) // [kg/m3]
+{
+  const double Tvirtuel = 1.01 * (Temp + 273);
+  return (3.486 * AtmPressure / Tvirtuel);
+}
+
+double 
+Weather::SaturationVapourPressure (double Temp) // [kPa]
+{ return (0.611 * exp (17.27 * Temp / (Temp + 237.3))); }
+
+double 
+Weather::SlopeVapourPressureCurve (double Temp) // [kPa/K]
+{ return (4098 * SaturationVapourPressure (Temp) / pow (Temp + 237.3, 2)); }
+
+double 
+Weather::AtmosphericPressure ()	const // [kPa]
+{ return (101.3 * pow ((293 - 0.0065 * elevation) / 293, 5.26)); }
+
+double 
+Weather::CloudinessFactor_Arid (const Time& time, double Si) const
+{
+  const double a = 1.35;
+  const double x = Si / 0.75 / ExtraterrestrialRadiation (time);
+  return (a * min (1.0, x) + 1 - a);
+}
+
+double 
+Weather::CloudinessFactor_Humid (const Time& time, double Si) const
+{
+  const double a = 1.00;
+  const double x = Si / 0.75 / ExtraterrestrialRadiation (time);
+  const double cfh = (a * min (1.0, x) + 1 - a);
+  return cfh;
+}
+
+double 
+Weather::RefNetRadiation (const Time& time, double Si,
+			  double Temp, double ea) const
+{
+  static NetRadiation* net_radiation = NULL;
+  if (net_radiation == NULL)
+    {
+      Syntax syntax;
+      AttributeList alist;
+      alist.add ("type", "brunt");
+      net_radiation = &Librarian<NetRadiation>::create (alist);
+    }
+
+  const double albedo = 0.23;
+  net_radiation->tick (CloudinessFactor_Arid (time, Si),
+		       Temp, ea, Si, albedo);
+  return net_radiation->net_radiation ();
+}
+
+double 
+Weather::SolarDeclination (const Time& time) // [rad]
+{
+  return (0.409 * sin (2.0 * M_PI * time.yday () / 365.0 - 1.39));
+}
+
+double 
+Weather::RelativeSunEarthDistance (const Time& time)
+{
+  return (1.0 + 0.033 * cos (2.0 * M_PI * time.yday () / 365.0));
+}
+
+double 
+Weather::SunsetHourAngle (double Dec, double Lat) // [rad]
+{
+  return (acos (-tan (Dec) * tan (Lat)));
+}
+
+double 
+Weather::ExtraterrestrialRadiation (const Time& time) const // [MJ/m2/d]
+{
+  const double Dec = SolarDeclination (time);
+  const double Lat = M_PI / 180 * latitude;
+  const double x1 = SunsetHourAngle (Dec, Lat) * sin (Lat) * sin (Dec);
+  const double x2 = cos (Lat) * cos (Dec) * sin (SunsetHourAngle (Dec, Lat));
+  return (37.6 * RelativeSunEarthDistance (time) * (x1 + x2));
+}
+
+Weather::Weather (const AttributeList& al)
+  : name (al.name ("type")),
+    latitude (-42.42e42),
+    longitude (-42.42e42),
+    elevation (-42.42e42),
+    T_average_ (-42.42e42),
+    T_amplitude_ (-42.42e42),
+    rad_per_day_ (2.0 * M_PI / 365.0),
+    max_Ta_yday_ (-42.42e42),
+    day_length_ (-42.42e42),
+    day_cycle_ (-42.42e42)
+{ }
+
+Weather::~Weather ()
+{ }
+
+void
+Weather::load_syntax (Syntax& syntax, AttributeList&)
+{
+  // Logs.
+  syntax.add ("hourly_air_temperature", "dg C", Syntax::LogOnly,
+	      "Temperature this hour.");
+  syntax.add ("daily_air_temperature", "dg C", Syntax::LogOnly,
+	      "Average temperatures this day.");
+  syntax.add ("hourly_global_radiation", "W/m^2", Syntax::LogOnly,
+	      "Global radiation this hour.");
+  syntax.add ("daily_global_radiation", "W/m^2", Syntax::LogOnly,
+	      "Average radiation this day.");
+  syntax.add ("reference_evapotranspiration", "mm/h", Syntax::LogOnly,
+	      "Reference evapotranspiration this hour");
+  syntax.add ("daily_extraterrastial_radiation", "MJ/m^2/d", Syntax::LogOnly,
+	      "Extraterrestrial radiation this day.");
+  syntax.add ("rain", "mm/h", Syntax::LogOnly, "Rain this hour.");
+  syntax.add ("snow", "mm/h", Syntax::LogOnly, "Snow this hour.");
+  syntax.add ("cloudiness", Syntax::None (), Syntax::LogOnly,
+	      "Fraction of sky covered by clouds [0-1].");
+  syntax.add ("vapor_pressure", "Pa", Syntax::LogOnly, "Humidity.");
+  syntax.add ("wind", "m/s", Syntax::LogOnly, "Wind speed.");
+  syntax.add ("day_length", "h", Syntax::LogOnly,
+	      "Number of light hours this day.");
+  syntax.add ("day_cycle", Syntax::None (), Syntax::LogOnly, 
+	      "Fraction of daily radiation received this hour.");
+}
+
---
+++ tags/release_0_94/weather_file.C
@@ -0,0 +1,146 @@
+// weather_file.C
+
+#include "weather_old.h"
+#include "time.h"
+#include "log.h"
+#include <fstream.h>
+
+struct WeatherFile : public WeatherOld
+{
+  // Weather file.
+  Time date;
+  const string file_name;
+  ifstream file;
+  int line;			// Current line number in weather file.
+
+  // Values.
+  double precipitation;
+  double reference_evapotranspiration_;
+  double air_temperature;
+
+  // Simulation.
+  void tick (const Time&);
+
+  // Communication with Bioclimate.
+  double daily_air_temperature (void) const // [°C]
+    { return air_temperature; }
+  double reference_evapotranspiration () const // [mm/h]
+    { 
+      if (reference_evapotranspiration_ < -1.0e11)
+	return WeatherOld::reference_evapotranspiration ();
+      return reference_evapotranspiration_ * day_cycle (); 
+    }
+
+  // Communication with external model.
+  void put_precipitation (double prec)
+    { WeatherOld::distribute (prec / 24.0); }
+  void put_air_temperature (double T)
+    { air_temperature = T; }
+  void put_reference_evapotranspiration (double ref)
+    { reference_evapotranspiration_ = ref; }
+
+  // Create and Destroy.
+  WeatherFile (const AttributeList& al)
+    : WeatherOld (al),
+      date (42, 1, 1, 0),
+      file_name (al.name ("file")),
+      file (Options::find_file (al.name ("file"))),
+      line (0),
+      precipitation (-42.42e42),
+      reference_evapotranspiration_ (-42.42e42),
+      air_temperature (-42.42e42)
+    { }
+  ~WeatherFile ()
+    { 
+#if 0
+      // Code guard claims the file handle is bad.
+      close (file.rdbuf ()->fd ()); 
+#endif
+    }
+};
+
+void
+WeatherFile::tick (const Time& time)
+{ 
+  WeatherOld::tick (time);
+
+  if (!(date < time))
+    return;
+
+  if (!file.good ())
+    {
+      CERR << file_name << ":" << line << ": file error";
+      throw ("read error");
+    }
+  int year;
+  int month; 
+  int day;
+  int end;
+
+  double global_radiation;
+
+  while (date < time)
+    {
+      file >> year >> month >> day
+	   >> global_radiation >> air_temperature >> precipitation;
+      end = file.get ();
+      if (year < 100)
+	year += 1900;
+      while (file.good () && strchr (" \t", end))
+	end = file.get ();
+      
+      if (!file.good ())
+	throw ("No more climate data.");
+
+      if (end == '\n')
+	reference_evapotranspiration_ = -42.42e42;
+      else
+	{
+	  // BCC wants this:
+	  file.putback ((char) end);
+	  // G++ used this:
+	  // file.unget ();
+	  file >> reference_evapotranspiration_;
+	  while (file.good () && file.get () != '\n')
+	    /* do nothing */;
+	}
+      date = Time (year, month, day, 23);
+
+      assert (global_radiation >= 0 && global_radiation < 700);
+      assert (air_temperature >= -70 && air_temperature < 60);
+      assert (precipitation >= 0 && precipitation < 1000);
+      assert (reference_evapotranspiration_ <= 20);
+    }
+  assert (time.year () == date.year ());
+  assert (time.month () == date.month ());
+  assert (time.mday () == date.mday ());
+
+  // Update the daily values.
+  put_global_radiation (global_radiation);
+
+  // Hourly value.
+  distribute (precipitation / 24.0);
+}
+
+static struct WeatherFileSyntax
+{
+  static Weather& make (const AttributeList& al)
+    { return *new WeatherFile (al); }
+
+  WeatherFileSyntax ()
+    { 
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description", "Read weather data from a file.\n\
+Each line should have the following whitespace separated fields:\n\
+year, month, day, global radiation [W/m^2], air temperature [dg C],\n\
+precipitation [mm/d], and reference evapotranspiration [mm/d].  The\n\
+last field is optional, it is only used if you select the `weather'\n\
+model in the `pet' component");
+      WeatherOld::load_syntax (syntax, alist);
+      syntax.add ("file", Syntax::String, Syntax::Const,
+		  "File to read weather data from.");
+      syntax.order ("file");
+      Librarian<Weather>::add_type ("file", alist, syntax, &make);
+    }
+} WeatherFile_syntax;
