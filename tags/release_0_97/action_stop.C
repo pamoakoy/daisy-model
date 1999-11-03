d4290abb0451e1e309c4ffe1f2e4e08743950c5a
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
1999-11-03 03:42:00 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_97'.

---
+++ tags/release_0_97/action_stop.C
@@ -0,0 +1,29 @@
+// action_stop.C
+
+#include "action.h"
+#include "daisy.h"
+
+struct ActionStop : public Action
+{
+  void doIt (Daisy& daisy)
+    { daisy.running = false; }
+
+  ActionStop (const AttributeList& al)
+    : Action (al)
+    { }
+};
+
+static struct ActionStopSyntax
+{
+  static Action& make (const AttributeList& al)
+  {
+    return *new ActionStop (al);
+  }
+  ActionStopSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    alist.add ("description", "Stop the simulation.");
+    Librarian<Action>::add_type ("stop", alist, syntax, &make);
+  }
+} ActionStop_syntax;
