caf6129af748f7db20703fa080eaf01196cd818e
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2000-08-10 14:42:51 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_30'.

---
+++ tags/release_1_30/harvest.h
@@ -0,0 +1,45 @@
+// harvest.h
+
+#ifndef HARVEST_H
+#define HARVEST_H
+
+#include "chemicals.h"
+#include "time.h"
+#include <string>
+
+class AttributeList;
+class Syntax;
+class Log;
+
+class Harvest
+{
+  // Content:
+public:
+  const string column;
+  const Time time;
+  const string crop;
+  const double stem_DM;
+  const double stem_N;
+  const double dead_DM;
+  const double dead_N;
+  const double leaf_DM;
+  const double leaf_N;
+  const double sorg_DM;
+  const double sorg_N;
+  const Chemicals chemicals;
+
+  // Simulation:
+public:
+  void output (Log&) const;
+
+  // Create and Destroy.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  Harvest (const AttributeList& al);
+  Harvest (string col, Time t, string crp, 
+	   double sC, double sN, double dC, double dN, 
+	   double lC, double lN, double oC, double oN,
+	   const Chemicals& chem);
+};      
+
+#endif HARVEST_H
---
+++ tags/release_1_30/vegetation.C
@@ -0,0 +1,62 @@
+ // vegetation.C
+
+#include "vegetation.h"
+#include "log.h"
+
+Librarian<Vegetation>::Content* Librarian<Vegetation>::content = NULL;
+
+const char *const Vegetation::description = "\
+That green stuff.";
+
+void
+Vegetation::output (Log& log) const
+{
+  log.output ("LAI", LAI ());
+  log.output ("height", height ());
+  log.output ("cover", cover ());
+  log.output ("LAIvsH", LAIvsH ());
+  log.output ("HvsLAI", HvsLAI ());
+  log.output ("ACExt", ACExt ());
+  log.output ("ACRef", ACRef ());
+  log.output ("ARExt", ARExt ());
+  log.output ("ARExt", ARExt ());
+  log.output ("EpFactor", EpFactor ());
+  log.output ("albedo", albedo ());
+  log.output ("interception_capacity", interception_capacity ());
+}
+
+void
+Vegetation::load_syntax (Syntax& syntax, AttributeList&)
+{
+    syntax.add ("LAI", "m^2/m^2", Syntax::LogOnly,
+		"Total LAI of all crops on this column");
+    syntax.add ("height", "cm", Syntax::LogOnly,
+		"Max crop height in canopy");
+    syntax.add ("cover", "m^2/m^2", Syntax::LogOnly,
+		"Fraction of soil covered by crops");
+    syntax.add ("LAIvsH", "m^2/m^2", "cm", Syntax::LogOnly,
+		"Total canopy LAI below given height");
+    syntax.add ("HvsLAI", "cm", "m^2/m^2", Syntax::LogOnly, "\
+Height in which there is a given LAI below in total canopy");
+    syntax.add ("ACExt", Syntax::None (), Syntax::LogOnly,
+		"Canopy extinction coefficient \
+\(how fast the light dim as a function of LAI passed)");
+    syntax.add ("ACRef", Syntax::None (), Syntax::LogOnly,
+		"Canopy reflection coefficient");
+    syntax.add ("ARExt", Syntax::None (), Syntax::LogOnly,
+		"Radiation Extinction coefficient \
+\(like ACExt, but for all radiation, not just light)");
+    syntax.add ("EpFactor", Syntax::None (), Syntax::LogOnly,
+		"Reference to potential evapotranspiration");
+    syntax.add ("albedo", Syntax::None (), Syntax::LogOnly,
+		"Another reflection factor");
+    syntax.add ("interception_capacity", "mm", Syntax::LogOnly,
+		"Canopy water storage capacity");
+}
+
+Vegetation::Vegetation (const AttributeList& al)
+  : name (al.name ("type"))
+{ }
+
+Vegetation::~Vegetation ()
+{ }
