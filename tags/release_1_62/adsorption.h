ef49b23fc8182c39a57809401ee7af7fe305c6e1
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2000-10-30 15:29:35 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_62'.

---
+++ tags/release_1_62/adsorption.h
@@ -0,0 +1,32 @@
+// adsorption.h
+
+#ifndef ADSORPTION_H
+#define ADSORPTION_H
+
+#include "librarian.h"
+
+class Soil;
+
+class Adsorption
+{
+  // Content.
+public:
+  static const char *const description;
+  const string name;
+
+  // Simulation.
+public:
+  virtual void output (Log&) const;
+  virtual double C_to_M (const Soil&, double Theta, int i, double C) const = 0;
+  virtual double M_to_C (const Soil&, double Theta, int i, double M) const = 0;
+
+  // Create and Destroy.
+protected:
+  Adsorption (const string& name);
+public:
+  virtual ~Adsorption ();
+};
+
+static Librarian<Adsorption> Adsorption_init ("adsorption");
+
+#endif ADSORPTION_H
---
+++ tags/release_1_62/ridge.h
@@ -0,0 +1,40 @@
+// ridge.h
+
+#ifndef RIDGE_H
+#define RIDGE_H
+
+struct AttributeList;
+struct Syntax;
+struct Log;
+struct Soil;
+struct SoilWater;
+
+#include <vector>
+using namespace std;
+
+class Ridge
+{ 
+  struct Implementation;
+  Implementation& impl;
+
+  // Simulation.
+public:
+  void tick (const Soil&, const SoilWater&, 
+	     double external_ponding /* [mm] */);
+  void update_water (const Soil&, const vector<double>& S_,
+		     vector<double>& h_, vector<double>& Theta_,
+		     vector<double>& q, const vector<double>& q_p);
+  void output (Log&) const;
+  int last_node () const;
+  double h () const;		// [cm]
+  double exfiltration () const;	// [mm]
+
+  // Create & Destroy.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  void initialize (const Soil&, const SoilWater&);
+  Ridge (const AttributeList& al);
+  ~Ridge ();
+};
+
+#endif RIDGE_H
