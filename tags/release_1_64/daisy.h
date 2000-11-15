bd17045bfb758e61ed0cde77722387dff6fa6171
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2000-11-15 17:41:15 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_64'.

---
+++ tags/release_1_64/daisy.h
@@ -0,0 +1,54 @@
+// daisy.h
+
+#ifndef DAISY_H
+#define DAISY_H
+
+#include "time.h"
+#include <vector>
+
+class Action;
+class Harvest;
+class Weather;
+class Log;
+class Field;
+class Syntax;
+class AttributeList;
+class Condition;
+
+class Daisy
+{
+  // Initial content.
+public:
+  static const char *const default_description;
+  const Syntax* syntax;
+  const AttributeList& alist;
+
+  // Content.
+public:
+  bool running;
+  const vector<Log*>& logs;
+  Condition& activate_output;
+  Time time;
+  Action& action;
+  Weather* weather;
+  Field& field;
+  vector<const Harvest*>& harvest;
+
+  // Simulation.
+public:
+  void tick_columns ();
+  void tick_logs ();
+  void tick ();
+  void run ();
+  bool check (ostream& err);
+
+  // Create and Destroy.
+public:
+  void initialize (const Syntax&);
+  static void load_syntax (Syntax&, AttributeList&);
+  Daisy (const AttributeList&);
+  ~Daisy ();
+};
+
+#endif // DAISY_H
+
---
+++ tags/release_1_64/hydraulic.C
@@ -0,0 +1,91 @@
+// hydraulic.C
+
+#include "hydraulic.h"
+#include "plf.h"
+#include "log.h"
+
+void 
+Hydraulic::set_porosity (double Theta)
+{ 
+  assert (Theta > Theta_res);
+  Theta_sat = Theta; 
+}
+
+void 
+Hydraulic::output (Log& log) const
+{
+  log.output ("Theta_sat", Theta_sat); 
+}
+
+void
+Hydraulic::K_to_M (PLF& plf, const int intervals) const
+{
+  static const double h0 = -20000.0;
+  const double Ksat = K (0.0);
+  const double max_change = pow (Ksat / K (h0), 1.0 / intervals);
+  double step = (0 - h0) / 4.0;
+
+  double h = h0;
+  double sum = 0.0;
+  while (h < 0)
+    {
+      plf.add (h, sum);
+      step *= 2;
+      while (K (h + step) / K (h) > max_change)
+	step /= 2;
+      sum += step * (K (h) + K (h + step)) / 2;
+      h += step;
+    }
+  plf.add (h, sum);
+}
+
+static bool
+check_alist (const AttributeList& al, ostream& err)
+{
+  bool ok = true;
+
+  const double Theta_res = al.number ("Theta_res");
+  const double Theta_sat = al.number ("Theta_sat");
+
+  non_negative (Theta_res, "Theta_res", ok, err);
+
+  if (Theta_sat >= 0.9)
+    {
+      err << "Theta_sat should be below 0.9 (is " << Theta_sat << ")\n";
+      ok = false;
+    }
+
+  if (Theta_res >= Theta_sat)
+    {
+      err << "Theta_sat should be above Theta_res\n";
+      ok = false;
+    }
+  return ok;
+}  
+
+
+void
+Hydraulic::load_syntax (Syntax& syntax, AttributeList& alist)
+{ 
+  syntax.add_check (check_alist);
+  syntax.add ("Theta_sat", "cm^3 H2O/cm^3", Syntax::State,
+	      "Saturation point.");
+  syntax.add ("Theta_res", "cm^3 H2O/cm^3", Syntax::Const,
+	      "Soil residual water.");
+  alist.add ("Theta_res", 0.0);
+}
+
+Hydraulic::Hydraulic (const AttributeList& al)
+  : name (al.name ("type")),
+    Theta_sat (al.number ("Theta_sat")),
+    Theta_res (al.number ("Theta_res"))
+{ }
+
+Hydraulic::~Hydraulic ()
+{ }
+
+Librarian<Hydraulic>::Content* Librarian<Hydraulic>::content = NULL;
+
+const char *const Hydraulic::description = "\
+This component is responsible for specifying the soils hydraulic\n\
+properties.";
