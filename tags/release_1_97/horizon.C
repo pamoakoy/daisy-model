e38af9300ab6041e7d1eecd2586a5a7194f1bb22
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2001-11-16 16:07:35 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_97'.

---
+++ tags/release_1_97/horizon.C
@@ -0,0 +1,515 @@
+// horizon.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "horizon.h"
+#include "library.h"
+#include "alist.h"
+#include "syntax.h"
+#include "plf.h"
+#include "hydraulic.h"
+#include "mathlib.h"
+#include "tortuosity.h"
+#include "log.h"
+#include <vector>
+#include <map>
+#include <numeric>
+
+struct Horizon::Implementation
+{
+  // Content.
+  const double clay;
+  const double silt;
+  const double fine_sand;
+  const double coarse_sand;
+  const double humus;
+  const double quarts_in_clay;
+  const double quarts_in_silt;
+  const double quarts_in_sand;
+  double dry_bulk_density;
+
+  // Organic matter.
+  const vector<double> SOM_C_per_N;
+  const vector<double> SOM_fractions;
+  double C_factor;
+
+  // Strange things.
+  const double quarts_form_factor;
+  const double mineral_form_factor;
+
+  // Heat Capacity and Conductivity.
+  double C_soil;
+  vector<double> K_water;
+  vector<double> K_ice;
+  
+  // Initialize.
+  // Note:  These variables are really not used after initialization.
+  enum constituents
+  { 
+    Water, Ice, Air, Quarts, Minerals, Organic_Matter,
+    Constituents_End,
+    Constituents_Start = Water
+  };
+  double content[Constituents_End];
+  double Theta_pF_high;
+  const Hydraulic* hydraulic;
+  void initialize (const Hydraulic&);
+  double HeatCapacity ();
+  double DepolationsFactor (const constituents medium, const double alfa);
+  double ThermalConductivity (constituents medium);
+  const int intervals;
+  double rho_soil_particles ();
+
+  static const double heat_capacity[Constituents_End];
+  
+  // Create.
+  static double weight (const AttributeList& al, string name);
+  Implementation (const AttributeList& al);
+};
+
+const double 
+Horizon::Implementation::heat_capacity[Constituents_End] = // [erg / cm³ / °C]
+// Ice is given as equivalent amount of water.
+{ 4.2e7, 1.9e7 * (1.0 / 0.92), 1.25e4, 2.0e7, 2.0e7, 2.5e7 }; 
+
+void 
+Horizon::Implementation::initialize (const Hydraulic& hydro)
+{
+  hydraulic = &hydro;
+
+  // Did we specify 'dry_bulk_density'?  Else calculate it now.
+  if (dry_bulk_density < 0.0)
+    dry_bulk_density = rho_soil_particles () * (1.0 - hydraulic->porosity ());
+
+  // C factor is the specific C content in horizon [g C/cm³]
+  const double C_divisor 
+    = accumulate (SOM_fractions.begin (), SOM_fractions.end (), 0.0);
+  
+  C_factor = dry_bulk_density * humus * c_fraction_in_humus;
+  if (C_divisor > 0.0)
+    C_factor /= C_divisor;
+  else
+    throw ("Horizon: No C fractions given");
+
+  // The particles are not in a real continuous medium.  Try to correct.
+  const double continuum_correction_factor = 1.25;
+      
+  // Quarts content in soil.
+  const double quarts = clay * quarts_in_clay
+    + silt * quarts_in_silt
+    + (fine_sand + coarse_sand) * quarts_in_sand;
+
+  // Above this pF heat is mostly tranfered by Air.
+  Theta_pF_high = hydraulic->Theta (pF2h (4.2));
+      
+  // Below this pf heat is mostly transfered by Water or Ice.
+  const double Theta_pF_low
+    = (hydraulic->Theta (pF2h (2.0)) + Theta_pF_high) / 2.0;
+
+  // Water that won't freeze.
+  const double LiquidWater = Theta_pF_high; 
+  
+  // Relative content of various constituents in soil.
+  content[Quarts] = quarts * (1.0 - hydraulic->Theta_sat);
+  content[Minerals] = ((clay + silt + fine_sand + coarse_sand) - quarts)
+    * (1.0 - hydraulic->Theta_sat);
+  content[Organic_Matter] = humus * (1.0 - hydraulic->Theta_sat);
+
+  // Find capasity of dry soil.
+  content[Air] = hydraulic->porosity ();
+  content[Water] = 0.0;
+  content[Ice] = 0.0;
+  C_soil = HeatCapacity ();
+
+  // We calculate for water between Theta_res and Theta_sat.
+  const int from = (int) floor (intervals * hydraulic->Theta_res);
+  const int to = (int) ceil (intervals * hydraulic->Theta_sat);
+  
+  assert (0 <= from);
+  assert (from < to);
+  assert (to < intervals);
+
+  // Make room, make room...
+  K_water.insert (K_water.end (), intervals, 0.0);
+  K_ice.insert (K_ice.end (), intervals, 0.0);
+
+  for (int i = from; i < to; i++)
+    {
+      // Fill out water, ice, and air for pure water system.
+      content[Water] = (i + 0.0) / (intervals + 0.0);
+      content[Ice] = 0.0;
+      content[Air] = hydraulic->porosity () - content[Water];
+
+      // Calculate termal attributes for this combination.
+      const double K_water_wet = ThermalConductivity (Water);
+      const double K_water_dry = continuum_correction_factor
+	* ThermalConductivity (Air);
+      
+      // Find actual conductivity in combined water and air system.
+      if (content[Water] < Theta_pF_high)
+	K_water[i] = K_water_dry;
+      else if (content[Water] > Theta_pF_low)
+	K_water[i] = K_water_wet;
+      else
+	K_water[i] = K_water_dry 
+	  + (K_water_wet - K_water_dry)
+	  * (content[Water] - Theta_pF_high)
+	  / (Theta_pF_low - Theta_pF_high);
+      
+      // Fill out water, ice, and air for pure ice system.
+      content[Water] = min (LiquidWater, (i + 0.0) / (intervals + 0.0));
+      content[Ice] = max ((i + 0.0) / (intervals + 0.0) - LiquidWater, 0.0);
+      content[Air] = hydraulic->Theta_sat - (content[Water] + content[Ice]);
+      
+      // Calculate termal attributes for this combination.
+      const double K_ice_wet = ThermalConductivity (Ice);
+      const double K_ice_dry = continuum_correction_factor 
+	* ThermalConductivity (Air);
+      
+      // Find actual conductivity in combined ice and air system.
+      if (content[Water] + content[Ice] < Theta_pF_high)
+	K_ice[i] = K_ice_dry;
+      else if (content[Water] + content[Ice] > Theta_pF_low)
+	K_ice[i] = K_ice_wet;
+      else
+	K_ice[i] = K_ice_dry 
+	  + (K_ice_wet - K_ice_dry)
+	  * (content[Water] + content[Ice] - Theta_pF_high)
+	  / (Theta_pF_low - Theta_pF_high);
+    }
+  for (int i = to; i < intervals; i++)
+    {
+      K_water[i] = K_water[to-1];
+      K_ice[i] = K_ice[to-1];
+    }
+  for (int i = 0; i < from; i++)
+    {
+      K_water[i] = K_water[from];
+      K_ice[i] = K_ice[from];
+    }
+}
+
+double 
+Horizon::Implementation::HeatCapacity ()
+{
+  double C = 0.0;
+  for (int i = 0; i < Constituents_End; i++)
+    C += heat_capacity[i] * content[i];
+  
+  return C;
+}
+
+double 
+Horizon::Implementation::DepolationsFactor (const constituents medium, 
+					    const double alfa)
+{
+  if (medium == Air)
+    return 0.333 - (0.333 - 0.070) * content[Air] / (hydraulic->porosity()
+						     - Theta_pF_high);
+
+  const double a = 1 - alfa * alfa;
+  
+  if (alfa < 1)
+    return 1.0 / (2.0 * a)
+      + alfa * alfa / (4.0 * a * sqrt (a)) 
+      * log ((1.0 - sqrt (a)) / (1.0 + sqrt (a)));
+  if (alfa == 1.0)
+    return 1.0 / 3.0;
+  if (alfa > 1.0)
+    return (alfa * alfa / sqrt (-a) * (M_PI_2 - atan (sqrt (-1.0 / a))) - 1.0)
+      / (2.0 * -a);
+  assert (false);
+  return 42.42e42;
+}
+
+double 
+Horizon::Implementation::ThermalConductivity (constituents medium)
+{
+  // Thermal conductivity of each medium.
+  double thermal_conductivity[Constituents_End] =
+  { 0.57e5, 2.2e5, 0.025e5, 8.8e5, 2.9e5, 0.25e5 }; // [erg/s/cm/dg C]
+
+  // Air conductivity is modified by water vapour.
+  const double vapour_conductivity = 0.040e5;
+  thermal_conductivity[Air] 
+    += vapour_conductivity * min (1.0, (content[Water] / Theta_pF_high));
+  
+  double S1 = content[medium] * thermal_conductivity[medium];
+  double S2 = content[medium];
+  
+  for (constituents i = Constituents_Start;
+       i < Constituents_End;
+       // C++ enums SUCKS!
+       i = constituents (i + 1))
+    {
+      if (i != medium && content[i] > 0.0)
+	{
+	  const double a = thermal_conductivity[i] 
+	    / thermal_conductivity[medium] - 1.0;
+	  double k = -42.42e42;
+	  switch (i)
+	    {
+	    case Water:
+	    case Ice:
+	      k = (1.0 / (1.0 + a)) / 3.0;
+	      break;
+	    case Quarts:
+	    case Minerals:
+	    case Air:
+	      {
+		const double g = DepolationsFactor (i, 
+						    (i == Quarts)
+						    ? quarts_form_factor
+						    : mineral_form_factor);
+		k = (2.0 / (1.0 + a * g)
+		     + 1.0 / (1.0 + a * (1.0 - 2.0 * g)))
+		  / 3.0;
+	      }
+	      break;
+	    case Organic_Matter:
+	      {
+		const double Alfa = -3.0;
+		k = (1.0 / (1.0 + a / (1.0 - Alfa))
+		     + 1.0 / (1.0 - a * Alfa / (1.0 - Alfa))) / 3.0;
+	      }
+	    break;
+	    case Constituents_End:
+	      abort ();
+	    }
+	  S1 += k * content[i] * thermal_conductivity[i];
+	  S2 += k * content[i];
+	}
+    }
+  return S1 / S2;
+}
+
+double
+Horizon::Implementation::rho_soil_particles ()
+{ 
+  return (clay + silt + fine_sand + coarse_sand) * rho_mineral
+    + humus * rho_humus; 
+}
+
+double 
+Horizon::Implementation::weight (const AttributeList& al, string name)
+{
+  return al.number (name) / (al.number ("clay")
+			     + al.number ("silt")
+			     + al.number ("fine_sand")
+			     + al.number ("coarse_sand")
+			     + al.number ("humus"));
+}
+
+Horizon::Implementation::Implementation (const AttributeList& al)
+  : clay (weight (al, "clay")),
+    silt (weight (al, "silt")),
+    fine_sand (weight (al, "fine_sand")),
+    coarse_sand (weight (al, "coarse_sand")),
+    humus (weight (al, "humus")),
+    quarts_in_clay (al.number ("quarts_in_clay")),
+    quarts_in_silt (al.number ("quarts_in_silt")),
+    quarts_in_sand (al.number ("quarts_in_sand")),
+    SOM_C_per_N (al.number_sequence ("SOM_C_per_N")),
+    SOM_fractions (al.number_sequence ("SOM_fractions")),
+    quarts_form_factor (al.number ("quarts_form_factor")),
+    mineral_form_factor (al.number ("mineral_form_factor")),
+    intervals (al.integer ("intervals"))
+{ 
+  if (al.check ("C_soil"))
+    C_soil = al.number ("C_soil");
+  if (al.check ("K_water"))
+    K_water = al.number_sequence ("K_water");
+  if (al.check ("K_ice"))
+    K_ice = al.number_sequence ("K_ice");
+  if (al.check ("dry_bulk_density"))
+    dry_bulk_density = al.number ("dry_bulk_density");
+  else 
+    dry_bulk_density = -1.0;
+}
+
+double
+Horizon::dry_bulk_density () const
+{ return impl.dry_bulk_density; }
+
+double 
+Horizon::clay () const 
+{ return impl.clay; }
+
+double 
+Horizon::humus () const 
+{ return impl.humus; }
+
+double 
+Horizon::SOM_C (unsigned int pool) const
+{
+  if (pool < impl.SOM_fractions.size ())
+    // Specified, cool.
+    return impl.SOM_fractions[pool] * impl.C_factor;
+  else if (pool == 0)
+    // Else, everything in the first (slow) pool.
+    return  impl.C_factor;
+    
+  return 0.0;
+}
+
+double 
+Horizon::SOM_C_per_N (unsigned int pool) const
+{
+  if (pool < impl.SOM_C_per_N.size ())
+    // Specied, fine.
+    return impl.SOM_C_per_N[pool];
+  else if (impl.SOM_C_per_N.size () > 0)
+    // Used last specified number.
+    return impl.SOM_C_per_N[impl.SOM_C_per_N.size () - 1];
+  // Give up.  Guess.
+  throw ("Horizon: SOM: no C_per_N");
+}
+
+double
+Horizon::heat_conductivity (double Theta, double Ice) const
+{
+  const int entry = int ((Theta + Ice) * impl.intervals);
+  assert (entry >= 0);
+  assert (entry < impl.intervals);
+  return ((impl.K_ice[entry] * Ice + impl.K_water[entry] * Theta) 
+	  / (Theta + Ice))
+    * 3600;			// erg/s / cm / K -> erg/h / cm / K
+}
+
+double
+Horizon::heat_capacity (double Theta, double Ice) const
+{
+  return impl.C_soil 
+    + impl.heat_capacity[Implementation::Water] * Theta
+    + impl.heat_capacity[Implementation::Ice] * Ice;
+}
+
+static bool
+check_alist (const AttributeList& al, Treelog& err)
+{
+  bool ok = true;
+
+  if (!(al.number ("humus") > 0.0))
+    {
+      err.entry ("humus must be a positive number");
+      ok = false;
+    }
+  if (al.size ("SOM_C_per_N") < 1)
+    {
+      err.entry ("must specify at least one SOM_C_per_N");
+      ok = false;
+    }
+  return ok;
+}
+
+void 
+Horizon::output (Log& log) const
+{ output_derived (hydraulic, "hydraulic", log); }
+
+
+void
+Horizon::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  syntax.add_check (check_alist);
+  syntax.add ("description", Syntax::String, Syntax::OptionalConst,
+	      "Description of this particular soil type.");
+  alist.add ("description",
+	     "The physical properties of a particular soil type.");
+  syntax.add ("hydraulic", Librarian<Hydraulic>::library (), 
+	      "The hydraulic propeties of the soil.");
+  syntax.add ("tortuosity", Librarian<Tortuosity>::library (), 
+	      "The soil tortuosity.");
+  AttributeList& tortuosity = *new AttributeList ();
+  tortuosity.add ("type", "M_Q");
+  alist.add ("tortuosity", tortuosity);
+  syntax.add ("clay", Syntax::None (), Syntax::Const,
+	      "Relative fraction of clay in soil.");
+  syntax.add ("silt", Syntax::None (), Syntax::Const,
+	      "Relative fraction of silt in soil.");
+  syntax.add ("fine_sand", Syntax::None (), Syntax::Const,
+	      "Relative fraction of fine sand in soil.");
+  syntax.add ("coarse_sand", Syntax::None (), Syntax::Const,
+	      "Relative fraction of coarse sand in soil.");
+  syntax.add ("humus", Syntax::None (), Syntax::Const,
+	      "Relative fraction of humus in soil.");
+// Data adopted from Møberg et al. 1988 (Tinglev & Roskilde Soil)
+  syntax.add ("quarts_in_clay", Syntax::Fraction (), Syntax::Const,
+	      "Quarts fraction in clay.");
+  alist.add ("quarts_in_clay", 0.15);
+  syntax.add ("quarts_in_silt", Syntax::Fraction (), Syntax::Const,
+	      "Quarts fraction in silt.");
+  alist.add ("quarts_in_silt", 0.60);
+  syntax.add ("quarts_in_sand", Syntax::Fraction (), Syntax::Const,
+	      "Quarts fraction in sand.");
+  alist.add ("quarts_in_sand", 0.70);
+  syntax.add ("dry_bulk_density", "g/cm^3", Syntax::OptionalConst,
+	      "The soils dry bulk density.\n\
+By default, this is calculated from the soil constituents.");
+  syntax.add ("SOM_C_per_N", "g C/g N", Syntax::Const, Syntax::Sequence,
+	      "C/N ratio for each SOM pool in this soil.");
+  syntax.add ("SOM_fractions",
+	      Syntax::Fraction (), Syntax::Const, Syntax::Sequence,
+	      "Fraction of humus in each SOM pool, from slowest to fastest.");
+  syntax.add ("quarts_form_factor", Syntax::None (), Syntax::Const,
+	      "Gemetry factor used for conductivity calculation.");
+  alist.add ("quarts_form_factor", 2.0);
+  syntax.add ("mineral_form_factor", Syntax::None (), Syntax::Const,
+	      "Gemetry factor used for conductivity calculation.");
+  alist.add ("mineral_form_factor", 4.0);
+  syntax.add ("intervals", Syntax::Integer, Syntax::Const, "\
+Number of numeric intervals to use in the heat coductivity table.");
+  alist.add ("intervals", 100);
+  syntax.add ("C_soil", "erg/cm^3/dg C", Syntax::OptionalConst,
+	      "The soils heat capacity.\n\
+By default, this is calculated from the soil constituents.");
+  syntax.add ("K_water",
+	      "erg/s/cm/dg C", Syntax::OptionalConst, Syntax::Sequence,
+	      "Heat conductivity table for water in soil.\n\
+By default, this is calculated from the soil constituents.");
+  syntax.add ("K_ice",
+	      "erg/s/cm/dg C", Syntax::OptionalConst, Syntax::Sequence,
+	      "Heat conductivity table for solid frozen soil.\n\
+By default, this is calculated from the soil constituents.");
+}
+
+Horizon::Horizon (const AttributeList& al)
+  : impl (*new Implementation (al)),
+    name (al.name ("type")),
+    hydraulic (Librarian<Hydraulic>::create (al.alist ("hydraulic"))),
+    tortuosity (Librarian<Tortuosity>::create (al.alist ("tortuosity")))
+{ 
+  if (impl.K_water.size () == 0)
+    {
+      impl.initialize (hydraulic);
+    }
+}
+
+Horizon::~Horizon ()
+{ }
+
+// Create Horizon library.
+EMPTY_TEMPLATE
+Librarian<Horizon>::Content* Librarian<Horizon>::content = NULL;
+
+const char *const Horizon::description = "\
+A 'horizon' is a soil type with specific physical properties.  It is\n\
+the responsibility of the 'horizon' component to specify these\n\
+properties.";
---
+++ tags/release_1_97/pet_PM.C
@@ -0,0 +1,302 @@
+// pet_PM.C -- Potential evopotranspiration using Penman-Monteith.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "pet.h"
+#include "weather.h"
+#include "soil.h"
+#include "surface.h"
+#include "soil_heat.h"
+#include "bioclimate.h"
+#include "net_radiation.h"
+#include "vegetation.h"
+#include "log.h"
+#include "message.h"
+
+class PetPM : public Pet
+{
+public:
+  // FAO utilities.
+  static double ETaero (double AtmPressure /* [Pa] */,
+			double Temp /* [dg C] */,
+			double ea /* [Pa] */,
+			double ra /* [s/m] */,
+			double rc); // [kg/m2/s]
+  static double RefETaero (double AtmPressure /* [Pa] */,
+			   double Temp /* [dg C] */,
+			   double ea /* [Pa] */,
+			   double U2);// [kg/m2/s]
+  static double ETrad (double AtmPressure /* [Pa] */,
+		       double Temp /* [dg C] */,
+		       double Rn /* [W/m2] */,
+		       double G /* [W/m2] */,
+		       double ra /* [s/m] */,
+		       double rc); // [kg/m2/s]
+  static double PenmanMonteith (double CropHeight /* [m] */,
+				double ScreenHeight /* [m] */,
+				double LAI /* [m^2/m^2] */,
+				double Rn /* [W/m2] */,
+				double G /* [W/m2] */,
+				double Temp /* [dg C] */,
+				double ea /* [Pa] */,
+				double U2 /* [m/s] */,
+				double AtmPressure); // [kg/m2/s]
+  static double RefPenmanMonteith (double Rn /* [W/m2/] */,
+				   double G /* [W/m2] */,
+				   double Temp /* [dg C] */,
+				   double ea /* [Pa] */,
+				   double U2 /* [m/s] */,
+				   double AtmPressure); // [kg/m2/s]
+  static double RefPenmanMonteithWet (double Rn /* [W/m2] */,
+				      double G /* [W/m2] */,
+				      double Temp /* [dg C] */,
+				      double ea /* [Pa] */,
+				      double U2 /* [m/s] */,
+				      double AtmPressure); // [kg/m2/s]
+
+
+  // State.
+  double reference_evapotranspiration_wet;
+  double reference_evapotranspiration_dry;
+  double potential_evapotranspiration_wet;
+  double potential_evapotranspiration_dry;
+
+  // Net radiation.
+  NetRadiation& net_radiation;
+
+  // Simulation.
+  void tick (const Weather& weather, const Vegetation& crops,
+	     const Surface& surface, const Soil& soil,
+	     const SoilHeat& soil_heat, const SoilWater& soil_water);
+
+  void output (Log& log) const
+    {
+      Pet::output (log);
+      log.output ("reference_evapotranspiration",
+		  reference_evapotranspiration_dry);
+      output_derived (net_radiation, "net_radiation", log);
+    }
+
+  double wet () const
+    {
+      return potential_evapotranspiration_wet;
+    }
+
+  double dry () const
+    {
+      return potential_evapotranspiration_dry;
+    }
+
+  // Create & Destroy.
+  PetPM (const AttributeList& al)
+    : Pet (al),
+      net_radiation (Librarian<NetRadiation>::create
+		     (al.alist ("net_radiation")))
+    { }
+  ~PetPM ()
+    {
+      delete &net_radiation;
+    }
+};
+
+double
+PetPM::ETaero (double AtmPressure, double Temp, double ea, double ra,
+	       double rc)
+{
+  const double x1 = Weather::SlopeVapourPressureCurve (Temp) +
+    Weather::PsychrometricConstant (AtmPressure, Temp) * (1 + rc / ra);
+  const double x2 = Weather::AirDensity (AtmPressure, Temp) * 1.013 /
+    Weather::LatentHeatVaporization (Temp);
+  const double x3 = (Weather::SaturationVapourPressure (Temp) - ea) / ra;
+  return (1.0 / x1) * x2 * x3;   // [kg/m2/s]
+}
+
+double
+PetPM::RefETaero (double AtmPressure, double Temp, double ea, double U2)
+{
+  double x1 = Weather::SlopeVapourPressureCurve (Temp)
+    / Weather::PsychrometricConstant (AtmPressure, Temp);
+  x1 = 1 / (x1 + 1 + 0.34 * U2);
+  const double x2 = 0.9 / (Temp + 273) * U2
+    * (Weather::SaturationVapourPressure (Temp) - ea);
+  return (x1 * x2) / 86400.0; // [kg/m2/s]
+}
+
+double
+PetPM::ETrad (double AtmPressure, double Temp, double Rn, double G,
+	      double ra, double rc)
+{
+  double x1 = Weather::SlopeVapourPressureCurve (Temp)
+    / Weather::PsychrometricConstant (AtmPressure, Temp);
+  x1 /= x1 + 1 + rc / ra;
+  const double x2 = (Rn - G) / Weather::LatentHeatVaporization (Temp);
+  return (x1 * x2);
+}
+
+double
+PetPM::PenmanMonteith (double CropHeight, double ScreenHeight, 
+		       double LAI, double Rn,
+		       double G, double Temp, double ea, double U2,
+		       double AtmPressure)
+{
+  const double ra = Bioclimate::AerodynamicResistance (CropHeight, 
+						       ScreenHeight, U2);
+  const double rc = Bioclimate::CanopyResistance (LAI);
+  const double E1 = ETrad (AtmPressure, Temp, Rn, G, ra, rc);
+  const double E2 = ETaero (AtmPressure, Temp, ea, ra, rc);
+  return (E1 + E2);
+}
+
+double
+PetPM::RefPenmanMonteith (double Rn, double G, double Temp, double ea,
+			  double U2, double AtmPressure)
+{
+  double E3 = 0.03525 * Weather::SlopeVapourPressureCurve (Temp) * (Rn - G) +
+    Weather::PsychrometricConstant (AtmPressure, Temp)
+    * 0.9 / (Temp + 273) * U2 *
+    (Weather::SaturationVapourPressure (Temp) - ea);
+  E3 /= Weather::SlopeVapourPressureCurve (Temp) +
+    Weather::PsychrometricConstant (AtmPressure, Temp) * (1 + 0.34 * U2);
+#if 1
+  if (Rn>750.0)
+    {
+      COUT << "Rn          " << Rn << "\n";
+      COUT << "G           " << G << "\n";
+      COUT << "Temp        " << Temp << "\n";
+      COUT << "es          " << Weather::SaturationVapourPressure (Temp) << "\n";
+      COUT << "ea          " << ea << "\n";
+      COUT << "U2          " << U2 << "\n";
+      COUT << "AtmPressure " << AtmPressure << "\n";
+      COUT << "Delta       " << Weather::SlopeVapourPressureCurve (Temp) << "\n";
+      COUT << "Gamma       " << Weather::PsychrometricConstant (AtmPressure, Temp) << "\n";
+      COUT << "Ep (mm/d)   " << E3 << "\n";
+    }
+#endif
+  return E3 / 86400.0; // [kg/m2/s]
+}
+
+double
+PetPM::RefPenmanMonteithWet (double Rn, double G, double Temp, double ea,
+			     double U2, double AtmPressure)
+{
+  const double ra = Bioclimate::RefAerodynamicResistance (U2);
+  const double rc = 0.0;
+  const double E1 = ETrad (AtmPressure, Temp, Rn, G, ra, rc);
+  const double E2 = ETaero (AtmPressure, Temp, ea, ra, rc);
+  return (E1 + E2);
+}
+
+void
+PetPM::tick (const Weather& weather, const Vegetation& crops,
+	     const Surface& surface, const Soil& soil,
+	     const SoilHeat& soil_heat, const SoilWater& soil_water)
+{
+  // Weather.
+  const double Cloudiness = weather.hourly_cloudiness ();
+  const double Temp = weather.hourly_air_temperature ();
+  const double VaporPressure = weather.vapor_pressure ();
+  const double Si = weather.hourly_global_radiation ();
+  const double U2 = weather.wind ();
+  const double AtmPressure = weather.AtmosphericPressure ();
+
+  // Albedo.
+  const double LAI = crops.LAI ();
+  double Albedo;
+  if (LAI > 0.0)
+    {
+      const double crop_cover = crops.cover ();
+      Albedo = crops.albedo () * crop_cover
+	+ surface.albedo (soil, soil_water) * (1.0 - crop_cover);
+    }
+  else
+    Albedo =  surface.albedo (soil, soil_water);
+
+  // Net Radiation.
+  net_radiation.tick (Cloudiness, Temp, VaporPressure, Si, Albedo);
+  const double Rn = net_radiation.net_radiation ();
+
+  // Ground heat flux.
+  const double G = soil_heat.top_flux (soil, soil_water);
+
+
+  if (LAI > 0.0)
+    {
+      const double CropHeight = 0.01 * crops.height (); //cm -> m
+      const double ScreenHeight = weather.screen_height ();
+      // Dry.
+      reference_evapotranspiration_dry
+	= PenmanMonteith (CropHeight, ScreenHeight, 
+			  LAI, Rn, G, Temp, VaporPressure,
+			  U2, AtmPressure) * 3600;
+      potential_evapotranspiration_dry
+	= max (0.0, reference_evapotranspiration_dry);
+
+      // Wet.
+      const double ra
+	= Bioclimate::AerodynamicResistance (CropHeight, ScreenHeight, U2);
+      const double rc = 0.0;
+      const double E1 = ETrad (AtmPressure, Temp, Rn, G, ra, rc);
+      const double E2 = ETaero (AtmPressure, Temp, VaporPressure, ra, rc);
+      reference_evapotranspiration_wet = (E1 + E2) * 3600;
+      potential_evapotranspiration_wet
+	= max (0.0, reference_evapotranspiration_wet);
+
+    }
+  else
+    {
+      reference_evapotranspiration_dry
+	= RefPenmanMonteith (Rn, G, Temp, VaporPressure, U2, AtmPressure)
+	* 3600;
+
+      potential_evapotranspiration_dry
+	= reference_to_potential (crops, surface,
+				  reference_evapotranspiration_dry);
+
+      reference_evapotranspiration_wet
+	= RefPenmanMonteith (Rn, G, Temp, VaporPressure, U2, AtmPressure)
+	* 3600;
+      potential_evapotranspiration_wet
+	= reference_to_potential (crops, surface,
+				  reference_evapotranspiration_wet);
+    }
+}
+
+static struct PetPMSyntax
+{
+  static Pet&
+  make (const AttributeList& al)
+    { return *new PetPM (al); }
+  PetPMSyntax ()
+    {
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description",
+		 "Potential evopotranspiration using Penman-Monteith.");
+      Pet::load_syntax (syntax, alist);
+      Syntax Rn_syntax;
+      AttributeList Rn_alist;
+      Rn_alist.add ("type", "brunt");
+      syntax.add ("net_radiation", Librarian<NetRadiation>::library (),
+		  "Net radiation.");
+      alist.add ("net_radiation", Rn_alist);
+      Librarian<Pet>::add_type ("PM", alist, syntax, &make);
+    }
+} PetPM_syntax;
