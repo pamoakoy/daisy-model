6e67a86f17001d7882b374ce7e67b22f3b00cf53
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2002-02-21 18:35:23 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_12'.

---
+++ tags/release_2_12/library.h
@@ -0,0 +1,75 @@
+// library.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "common.h"
+#include <vector>
+
+class Syntax;
+class AttributeList;
+class Treelog;
+
+typedef void (*derive_fun) (const string& name, AttributeList& al,
+			    const string& super);
+
+class Library
+{
+  // Content.
+  struct Implementation;
+  Implementation& impl;
+
+public:
+  // Find a specific library.
+  static Library& find (const string& name);
+  static void all (vector<string>& libraries);
+  static int get_sequence ();
+
+  // Use.
+  const string& name () const;
+  const char* description () const;
+  AttributeList& lookup (const string&) const;
+  bool check (const string&) const;
+  void add (const string&, AttributeList&, const Syntax&);
+  void add_derived (const string& name, AttributeList& al,
+		    const string& super);
+  const Syntax& syntax (const string&) const;
+  void entries (vector<string>&) const;
+
+  // Dependencies.
+  void remove (const string&);
+
+  // File handling.
+  static void clear_all_parsed ();
+  static void refile_parsed (const string& from, const string& to);
+
+  // Create and destroy.
+  static void load_syntax (Syntax&, AttributeList&);
+private: 
+  Library (const Library&);
+public:
+  Library (const char* name, derive_fun derive, const char* description);
+  ~Library ();
+};
+
+#endif // LIBRARY_H
---
+++ tags/release_2_12/mactrans_std.C
@@ -0,0 +1,149 @@
+// mactrans_std.C -- Standard preferential flow model.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "mactrans.h"
+#include "soil_water.h"
+#include "soil.h"
+#include "plf.h"
+#include "mathlib.h"
+#include "message.h"
+
+struct MactransStandard : public Mactrans
+{
+  // Simulation.
+ void tick (const Soil& soil, const SoilWater&,
+	    const vector<double>& C,
+	    vector<double>& S,
+	    vector<double>& S_p,
+	    vector<double>& J_p);
+  void output (Log&) const
+    { }
+
+  // Create and Destroy.
+  MactransStandard (const AttributeList& al)
+    : Mactrans (al)
+    { }
+  ~MactransStandard ()
+    { }
+};
+
+void 
+MactransStandard::tick (const Soil& soil, const SoilWater& soil_water,
+			const vector<double>& C,
+			vector<double>& S_m,
+			vector<double>& S_p,
+			vector<double>& J_p)
+{ 
+  for (unsigned int i = 0; i < soil.size (); i++)
+    {
+      // Amount of water entering this layer through macropores.
+      const double water_in_above = -soil_water.q_p (i); // [cm]
+      const double water_out_below = -soil_water.q_p (i+1); // [cm]
+      const double delta_water = water_in_above - water_out_below; // [cm]
+      
+      // Amount of matter entering this layer through macropores.
+      const double matter_in_above = -J_p[i]; // [g/cm^2]
+      double delta_matter;	// [g/cm^2]
+
+      if (delta_water < -1.0e-60)
+	// Water leaves the layer.
+	{
+	  // Water leaves with the concentration in this layer.
+	  delta_matter = C[i] * delta_water;
+	  assert (delta_water <= 0.0);
+	}
+      else if (delta_water > 1.0e-60)
+	// Water enters the layer.
+	{
+	  // Fraction of water entering the layer through the
+	  // macropore, which also stayes here.
+	  if (water_in_above > 0.0)
+	    {
+	      /*const*/ double water_fraction 
+		= approximate (delta_water, water_in_above) 
+		? 1.0
+		: delta_water / water_in_above;
+	      if (water_fraction < 0.0 || water_fraction > 1.0)
+		{
+		  CERR << __FILE__ << ":" <<  __LINE__
+		       << ": BUG: water fraction from macropore = " 
+		       << water_fraction << "\n";
+		  set_bound (0.0, water_fraction, 1.0);
+		}
+
+	      // Matter stayes with the water.
+	      delta_matter = matter_in_above * water_fraction;
+	      assert (delta_matter >= 0.0);
+	    }
+	  else
+	    {
+	      // Water through macropores from below... 
+	      delta_matter = 0.0; // Just assume pure water.
+	    }
+	}
+      else
+	delta_matter = 0.0;
+
+      // Find amount of stuff leaving the layer.
+      if (delta_matter < 1e-60)
+	{
+	  // Everything go to the bottom.
+	  J_p[i+1] = J_p[i];
+	  S_p[i] = 0.0;
+	}
+      else if (approximate (matter_in_above, delta_matter))
+	{
+	  // Everything go to the layer.
+	  J_p[i+1] = 0.0;
+	  S_p[i] = matter_in_above / soil.dz (i) / dt;
+	  S_m[i] += S_p[i];
+	}
+      else
+	{
+	  // We split between layer and bottom.
+	  J_p[i+1] = -(matter_in_above - delta_matter);
+	  assert (J_p[i+1] < 0.0);
+	  S_p[i] = delta_matter / soil.dz (i) / dt;
+	  S_m[i] += S_p[i];
+	}
+    }
+  
+    // Check that the sink terms add up.
+  if (fabs (soil.total (S_p) + J_p[0] - J_p[soil.size ()]) > 1.0e-21)
+    CERR << __FILE__ << ":" <<  __LINE__
+	 << ": BUG: mactrans Total S_p = '" << (soil.total (S_p) + J_p[0])
+	 << "' solute\n";
+}
+
+static struct MactransStandardSyntax
+{
+  static Mactrans&
+  make (const AttributeList& al)
+    { return *new MactransStandard (al); }
+  MactransStandardSyntax ()
+    {
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description", "Solute follows water.");
+      Librarian<Mactrans>::add_type ("default", alist, syntax, &make);
+    }
+} MactransStandard_syntax;
