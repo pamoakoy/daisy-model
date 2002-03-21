714649deb60d80be478f9303b62af6b299b425b9
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2002-03-21 12:06:46 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_23'.

---
+++ tags/release_2_23/crop.C
@@ -0,0 +1,65 @@
+ // crop.C
+
+#include "crop.h"
+#include "chemicals.h"
+#include "om.h"
+
+const double Crop::DSremove = -5001.0;
+
+EMPTY_TEMPLATE
+Librarian<Crop>::Content* Librarian<Crop>::content = NULL;
+
+const char *const Crop::description = "\
+The 'crop' component simulates a specific crop on the field, typically\n\
+averaged over one square meter, not individual plants.  Of particular\n\
+interest is water and nitrogen uptake at different depths, and the\n\
+vertical leaf area distribution, which are used for competition with\n\
+other crops.";
+
+double 
+Crop::water_stress () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::nitrogen_stress () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::rs_min () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::rs_max () const
+{ assert (false); return -42.42e42; }
+
+double 
+Crop::albedo () const
+{ return 0.20; }
+
+void
+Crop::kill (const string& name, const Time& time, const Geometry& geometry,
+	    Bioclimate& bioclimate, vector<AM*>& residuals, Treelog& out)
+{ 
+  harvest (name, time, geometry, bioclimate,
+	   0.0, 0.0, 0.0, 0.0, true, residuals, out); 
+}
+
+void 
+Crop::force_production_stress  (double)
+{ }
+
+bool
+Crop::ds_remove (const Crop* crop)
+{ return crop->DS () == Crop::DSremove; }
+
+void
+Crop::initialize (Treelog& msg, const Geometry& geometry, OrganicMatter&)
+{ initialize (msg, geometry); }
+
+Crop::Crop (const AttributeList& al)
+  : alist (al),
+    name (al.name ("type"))
+{ }
+
+Crop::~Crop ()
+{ }
