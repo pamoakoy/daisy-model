5d8ac7f4e086f03ad82a0e6fdee61445ec7aac62
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2002-03-25 15:03:47 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_25'.

---
+++ tags/release_2_25/select_flux_bottom.C
@@ -0,0 +1,103 @@
+// select_flux_bottom.C --- Select a state variable.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "select.h"
+#include "geometry.h"
+
+struct SelectFluxBottom : public Select
+{
+  // Content.
+  double height;
+  double value;	
+
+  // Output routines.
+  void output_array (const string& name, const vector<double>& array, 
+		     const Geometry* geometry)
+    { 
+      if (!is_active ())
+	return;
+
+      if (valid (name))
+	{
+	  int index = ((height > 0.0) 
+		       ? geometry->size ()
+		       : min (geometry->interval_border (height) + 1,
+			      geometry->size ()));
+	  assert (array.size () > index);
+	  if (count == 0)	 
+	    value = array[index];	
+	  else
+	    value += array[index];	
+	  count++;
+	}
+    }
+
+  // Print result at end of time step.
+  void done (Destination& dest)
+    {
+      if (count == 0)
+	dest.missing (tag);
+      else 
+	dest.add (tag, value * factor + offset);
+
+      if (!accumulate)
+	count = 0;
+    }
+  // Create and Destroy.
+  void initialize (const string_map& conv, 
+		   double default_from, double default_to, 
+		   const string& timestep )
+    {
+      Select::initialize (conv, default_from, default_to, timestep);
+
+      // Overwrite default height.
+      if (default_to < 0.0)
+	height = default_to;
+    }
+  SelectFluxBottom (const AttributeList& al)
+    : Select (al),
+      height (1.0),
+      value (al.number ("value"))
+    { }
+};
+
+static struct SelectFluxBottomSyntax
+{
+  static Select& make (const AttributeList& al)
+    { return *new SelectFluxBottom (al); }
+
+  SelectFluxBottomSyntax ()
+    { 
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      Select::load_syntax (syntax, alist);
+
+      alist.add ("description", 
+		 "Extract flux at bottom of specified interval.\n\
+By default, log the first member of the sequence.");
+      syntax.add ("value", Syntax::Unknown (), Syntax::State,
+		  "The current accumulated value.");
+      alist.add ("value", 0.0);
+
+      Librarian<Select>::add_type ("flux_bottom", alist, syntax, &make);
+    }
+} Select_syntax;
---
+++ tags/release_2_25/vegetation.h
@@ -0,0 +1,114 @@
+// vegetation.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef VEGETATION_H
+#define VEGETATION_H
+
+#include "librarian.h"
+#include <vector>
+
+class Time;
+class Bioclimate;
+class Geometry;
+class Soil;
+class OrganicMatter;
+class SoilHeat;
+class SoilWater;
+class SoilNH4;
+class SoilNO3;
+class PLF;
+class Harvest;
+class AM;
+
+class Vegetation
+{ 
+  // Content.
+public:
+  const string name;
+  static const char *const description;
+
+  // Canopy queries.
+public:
+  virtual double LAI () const = 0; // Total LAI of all crops [0-]
+  virtual double height () const = 0; // Max crop height in canopy [cm]
+  virtual double cover () const = 0; // Fraction of soil covered by crops [0-1]
+  virtual const PLF& LAIvsH () const = 0; // LAI below height [f: cm -> R]
+  virtual const PLF& HvsLAI () const = 0; // Height with LAI below [f: R -> cm]
+  virtual double ACExt () const = 0;	// Canopy extinction coefficient
+  virtual double ACRef () const = 0;	// Canopy reflection coefficient 
+  virtual double ARExt () const = 0;	// Radiation Extinction coefficient
+  virtual double EpFactor () const = 0;	// Reference to pot. evapotransp
+  virtual double albedo () const = 0;	// Another reflection factor
+  virtual double interception_capacity () const = 0;// Canopy water cap. [mm]
+
+  // Individual crop queries.
+public:
+  virtual double DS_by_name (const string& name) const = 0;// [-1:2]/DSremove
+  virtual double DM_by_name (const string& name) const = 0;// Shoot DM [kg/ha]
+
+  // Simulation
+public:
+  virtual void tick (const Time&, const Bioclimate&, const Soil&,
+		     OrganicMatter&, const SoilHeat&, const SoilWater&,
+		     // Allow plants to grow (hourly).
+		     SoilNH4&, SoilNO3&, Treelog&) = 0;
+  virtual void tick (const Time&, const Bioclimate&, const Soil&,
+		     const SoilHeat&, const SoilWater&, Treelog&) = 0;
+  virtual double transpiration (// Actual trans. [mm/h]
+				double potential_transpiration,	
+				double canopy_evaporation,
+				const Soil& soil, SoilWater& soil_water,
+				Treelog&) = 0;
+  virtual void force_production_stress  (double pstress);
+  virtual void kill_all (const string&, const Time&, const Geometry&,
+			 Bioclimate&, vector<AM*>& residuals, Treelog&) = 0;
+  virtual void harvest (const string& column_name,
+			const string& crop_name,
+			const Time&, const Geometry&, 
+			Bioclimate& bioclimate,
+			double stub_length,
+			double stem_harvest,
+			double leaf_harvest, 
+			double sorg_harvest,
+			vector<const Harvest*>& harvest,
+			vector<AM*>& residuals,
+			double& harvest_DM, 
+			double& harvest_N, double& harvest_C,
+			Treelog&) = 0;
+  virtual double sow (Treelog& msg, 
+		      const AttributeList& al, // Return kg N/ha in seed.
+		      const Geometry&, OrganicMatter&) = 0;
+  virtual void sow (Treelog& msg, const AttributeList& al,
+		    const Geometry&) = 0;
+  virtual void output (Log&) const;
+
+  // Create and Destroy.
+public:
+  virtual void initialize (Treelog&, const Soil& soil, OrganicMatter&) = 0;
+  static void load_syntax (Syntax&, AttributeList&);
+  Vegetation (const AttributeList&);
+  virtual ~Vegetation ();
+};
+
+static Librarian<Vegetation> Vegetation_init ("vegetation");
+
+#endif // VEGETATION_H
