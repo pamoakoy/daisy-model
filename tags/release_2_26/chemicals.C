9f9658dc3abe35bfe53043c464e72759b5a54abd
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2002-04-08 11:33:20 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_26'.

---
+++ tags/release_2_26/chemicals.C
@@ -0,0 +1,442 @@
+// chemicals.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "chemicals.h"
+#include "log.h"
+#include "syntax.h"
+#include "alist.h"
+#include "chemical.h"
+#include "submodel.h"
+#include "treelog.h"
+#include <map>
+
+struct Chemicals::Implementation
+{ 
+  // Types.
+  typedef map<const Chemical*, double, less<const Chemical*>/**/> chemical_map;
+  typedef map<string, const Chemical*, less<string>/**/> chemistry_map;
+  
+  // Class variables.
+  static chemistry_map* chemistry;
+  static const Chemical* lookup (const string& name);
+
+  // Instance variables.
+  chemical_map chemicals;
+
+  // Utilities.
+  static void move_fraction (Implementation& from, Implementation& to,
+			     double fraction);
+  static void copy_fraction (const Implementation& from, Implementation& to,
+			     double fraction);
+  
+  // Canopy functions.
+  void canopy_update (const Implementation& canopy_chemicals_in, 
+		      double canopy_water_storage,
+		      double canopy_water_out);
+  void canopy_dissipate (Implementation& canopy_chemicals_dissipate) const;
+  void canopy_out (Implementation& canopy_chemicals_out,
+		   double canopy_water_storage,
+		   double canopy_water_out) const;
+
+  // Simulation
+  void output (Log&) const;
+  void add (const string& name, double amount) // [g/m^2]
+    { add (lookup (name), amount); }
+  void add (const Chemical* chemical, double amount) // [g/m^2]
+    { chemicals[chemical] += amount; }
+  void set_to (const string& name, double amount) // [g/m^2]
+    { set_to (lookup (name), amount); }
+  void set_to (const Chemical* chemical, double amount) // [g/m^2]
+    { chemicals[chemical] = amount; }
+  double amount (const string& name) const; // [g/m^2]
+  typedef set<string, less<string>/**/> string_set;
+  void find_missing (const string_set& all, string_set& missing) const;
+
+  // Create and Destroy.
+  void clear ();
+  void cleanup (Implementation& out);
+  void operator += (const Implementation&);
+  Implementation ()
+    : chemicals ()
+    { }
+  Implementation (const vector<AttributeList*>&);
+  Implementation (const Implementation& impl)
+    : chemicals (impl.chemicals)
+    { }
+  ~Implementation ()
+    { }
+};
+
+
+Chemicals::Implementation::chemistry_map* 
+Chemicals::Implementation::chemistry = NULL;
+
+const Chemical* 
+Chemicals::Implementation::lookup (const string& name)
+{
+  // Create map first time.
+  if (chemistry == NULL)
+    chemistry = new chemistry_map ();
+
+  // Look if name is already in map.
+  chemistry_map::const_iterator entry = chemistry->find (name);
+  if (entry != chemistry->end ())
+    return (*entry).second;
+
+  // Otherwise, check that it exists and is complate.
+  const Library& library = Librarian<Chemical>::library ();
+  assert (library.check (name));
+  const Syntax& syntax = library.syntax (name);
+  const AttributeList& alist = library.lookup (name);
+  assert (syntax.check (alist, Treelog::null ()));
+  AttributeList child (alist);
+  child.add ("type", name);
+
+  // Then add it.
+  const Chemical* chemical = &Librarian<Chemical>::create (child);
+  (*chemistry)[name] = chemical;
+  return chemical;
+}
+
+void 
+Chemicals::Implementation::move_fraction (Implementation& from,
+					  Implementation& to,
+					  double fraction)
+{ 
+  for (chemical_map::iterator i = from.chemicals.begin (); 
+       i != from.chemicals.end (); 
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double amount = (*i).second * fraction;
+      
+      to.add (chemical, amount);
+      (*i).second -= amount;
+    }
+}
+
+void 
+Chemicals::Implementation::copy_fraction (const Implementation& from, 
+					  Implementation& to,
+					  double fraction)
+{ 
+  for (chemical_map::const_iterator i = from.chemicals.begin ();
+       i != from.chemicals.end ();
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double amount = (*i).second * fraction;
+      
+      to.add (chemical, amount);
+    }
+}
+
+void
+Chemicals::Implementation::canopy_update (const Implementation& in, 
+					  double water_storage,
+					  double water_out)
+{ 
+  // new = (old + in * dt) / (1 + dt (K + water_out / (Kd + new)))
+
+  // Calculate the factor
+  for (chemical_map::const_iterator i = in.chemicals.begin ();
+       i != in.chemicals.end ();
+       i++)
+    add ((*i).first, (*i).second * dt);
+
+  // Divide with the divisor.
+  for (chemical_map::iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double K = chemical->canopy_dissipation_rate ();
+      const double Kd = chemical->canopy_washoff_coefficient ();
+      const double divisor = 1.0 + dt * (K + water_out / (Kd + water_storage));
+      assert (divisor > 0.0);
+      (*i).second /= divisor;
+    }
+}
+
+void
+Chemicals::Implementation::canopy_dissipate (Implementation& dissipate) const
+{
+  dissipate.clear ();
+  for (chemical_map::const_iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double Sm = (*i).second;
+      const double K = chemical->canopy_dissipation_rate ();
+      dissipate.add (chemical, K * Sm);
+    }
+}
+
+void
+Chemicals::Implementation::canopy_out (Implementation& out,
+				       double water_storage,
+				       double water_out) const
+{
+  out.clear ();
+  for (chemical_map::const_iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double Sm = (*i).second;
+      const double Kd = chemical->canopy_washoff_coefficient ();
+      out.add (chemical, water_out * Sm / (Kd + water_storage));
+    }
+}
+
+void
+Chemicals::Implementation::output (Log& log) const
+{
+  for (chemical_map::const_iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    { 
+      const string& name = (*i).first->name;
+      const double amount = (*i).second;
+      Log::Named named (log, name);
+      log.output ("chemical", name);
+      log.output ("amount", amount);
+    }
+}
+
+double
+Chemicals::Implementation::amount (const string& name) const
+{
+  const Chemical* chemical = lookup (name);
+
+  chemical_map::const_iterator i = chemicals.find (chemical);
+  
+  if (i == chemicals.end ())
+    return 0.0;
+
+  return  (*i).second;
+}
+
+void 
+Chemicals::Implementation::find_missing (const string_set& all,
+					 string_set& missing) const
+{
+  for (chemical_map::const_iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    {
+      const string name = (*i).first->name;
+      string_set::const_iterator found = all.find (name);
+      if (found == all.end ())
+	missing.insert (name);
+    }
+}
+
+void
+Chemicals::Implementation::clear ()
+{ 
+  for (chemical_map::iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    (*i).second = 0.0;
+}
+
+void
+Chemicals::Implementation::cleanup (Implementation& out)
+{ 
+  for (chemical_map::iterator i = chemicals.begin ();
+       i != chemicals.end ();
+       i++)
+    {
+      const Chemical* chemical = (*i).first;
+      const double amount = (*i).second;
+      
+      if (amount > 0.0 && amount < 1.e-18) // Less than one molecule...
+	{
+	  out.add (chemical, amount);
+	  (*i).second = 0.0;
+	}
+    }
+}
+
+void
+Chemicals::Implementation::operator += (const Implementation& other)
+{ 
+  for (chemical_map::const_iterator i = other.chemicals.begin ();
+       i != other.chemicals.end ();
+       i++)
+    add ((*i).first, (*i).second);
+}
+
+Chemicals::Implementation::Implementation (const vector<AttributeList*>& al)
+{
+  for (unsigned int i = 0; i < al.size (); i++)
+    add (al[i]->name ("chemical"), al[i]->number ("amount"));
+}
+
+const Chemical&
+Chemicals::lookup (const string& name)
+{ return *Implementation::lookup (name); }
+
+void
+Chemicals::move_fraction (Chemicals& from, Chemicals& to, double fraction)
+{ Implementation::move_fraction (from.impl, to.impl, fraction); }
+
+void
+Chemicals::copy_fraction (const Chemicals& from, Chemicals& to,
+			  double fraction)
+{ Implementation::copy_fraction (from.impl, to.impl, fraction); }
+
+void
+Chemicals::canopy_update (const Chemicals& canopy_chemicals_in, 
+			  double canopy_water_storage,
+			  double canopy_water_out)
+{ impl.canopy_update (canopy_chemicals_in.impl, 
+		      canopy_water_storage, canopy_water_out); }
+
+void
+Chemicals::canopy_dissipate (Chemicals& canopy_chemicals_dissipate) const
+{ impl.canopy_dissipate (canopy_chemicals_dissipate.impl); }
+
+void
+Chemicals::canopy_out (Chemicals& canopy_chemicals_out,
+		       double canopy_water_storage,
+		       double canopy_water_out) const
+{ impl.canopy_out (canopy_chemicals_out.impl, 
+		   canopy_water_storage, canopy_water_out); }
+
+void
+Chemicals::output (Log& log) const
+{ impl.output (log); }
+
+void 
+Chemicals::add (const string& chemical, double amount)
+{ impl.add (chemical, amount); }
+
+void 
+Chemicals::set_to (const string& chemical, double amount)
+{ impl.set_to (chemical, amount); }
+
+double
+Chemicals::amount (const string& chemical) const
+{ return impl.amount (chemical); }
+
+void 
+Chemicals::find_missing (const string_set& all, string_set& missing) const
+{ impl.find_missing (all, missing); }
+
+void 
+Chemicals::clear ()
+{ impl.clear (); }
+
+void
+Chemicals::cleanup (Chemicals& out)
+{ impl.cleanup (out.impl); }
+
+void 
+Chemicals::operator += (const Chemicals& other)
+{ impl += other.impl; }
+
+void 
+Chemicals::operator = (const Chemicals& other)
+{ impl.chemicals = other.impl.chemicals; }
+  
+Chemicals::Chemicals (const Chemicals& other)
+  : impl (*new Implementation (other.impl))
+{ }
+
+static bool
+check_alist_entry (const AttributeList& al, Treelog& err)
+{
+  bool ok = true;
+  
+  const Library& library = Librarian<Chemical>::library ();
+  const string chemical = al.name ("chemical");
+  const double amount = al.number ("amount");
+
+  if (!library.check (chemical))
+    {
+      err.entry (string ("Unknown chemical '") + chemical + "'");
+      ok = false;
+    }
+  else
+    {
+      const Syntax& syntax = library.syntax (chemical);
+      const AttributeList& alist = library.lookup (chemical);
+      if (!syntax.check (alist, err))
+	ok = false;
+    }
+  if (!(amount >= 0.0))
+    {
+      err.entry ("Amount must be non-negative");
+      ok = false;
+    }
+  return ok;
+}
+
+// KLUDGE: Ugly hack to be able to use the standard 'load_syntax' form.
+static Syntax::category chemicals_default_category = Syntax::State;
+
+static void chemicals_load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  syntax.add_check (check_alist_entry);
+  alist.add ("description", "Name and amount of chemical compounds.");
+  alist.add ("submodel", "Chemicals");
+  syntax.add ("chemical", Syntax::String, Syntax::State, 
+	      "Name of chemical.");
+  syntax.add ("amount", "g/m^2", chemicals_default_category,
+	      "Amount of chemical compound");
+  syntax.order ("chemical", "amount");
+}
+
+void 
+Chemicals::add_syntax (const char* name,
+		       Syntax& syntax, AttributeList& alist,
+		       Syntax::category cat, 
+		       const string& description)
+{
+  // KLUDGE: Ugly hack to be able to use the standard 'load_syntax' form.
+  chemicals_default_category = cat;
+  Syntax& entry_syntax = *new Syntax ();
+  AttributeList entry_alist;
+  chemicals_load_syntax (entry_syntax, entry_alist);
+  syntax.add (name, entry_syntax, entry_alist,
+	      cat, Syntax::Sequence, description);
+  alist.add (name, vector<AttributeList*> ());
+  // KLUDGE: Ugly hack to be able to use the standard 'load_syntax' form.
+  chemicals_default_category = Syntax::State;
+}
+  
+Chemicals::Chemicals (const vector<AttributeList*>& al)
+  : impl (*new Implementation (al))
+{ }
+  
+Chemicals::Chemicals ()
+  : impl (*new Implementation ())
+{ }
+  
+Chemicals::~Chemicals ()
+{ delete &impl; }
+
+static Submodel::Register chemicals_submodel ("Chemicals", 
+					      chemicals_load_syntax);
---
+++ tags/release_2_26/harvesting.C
@@ -0,0 +1,386 @@
+// harvesting.C -- Harvest parameters for the default crop model.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include "harvesting.h"
+#include "production.h"
+#include "am.h"
+#include "om.h"
+#include "crop.h"		// for Crop::DSremove.
+#include "harvest.h"
+#include "log.h"
+#include "mathlib.h"
+#include "submodel.h"
+#include "check_range.h"
+
+#include <numeric>
+
+// Dimensional conversion.
+static const double m2_per_cm2 = 0.0001;
+// Based on Penning de Vries et al. 1989, page 63
+// E is the assimilate conversion effiency
+static double DM_to_C_factor (double E)
+{
+   return 12.0/30.0 * (1.0 - (0.5673 - 0.5327 * E)) / E;
+}
+
+const Harvest&
+Harvesting::operator() (const string& column_name,
+			const string& crop_name,
+			const vector<double>& density,
+			const Time& time,
+			const Geometry& geometry,
+			Production& production,
+			double& DS,
+			const double stem_harvest,
+			const double leaf_harvest,
+			const Chemicals& chemicals,
+			const double stem_harvest_frac,
+			const double leaf_harvest_frac,
+			const double sorg_harvest_frac,
+			const bool kill_off,
+			vector<AM*>& residuals)
+{
+
+  // Import DM from production.
+  const double WStem = production.WStem;
+  const double WLeaf = production.WLeaf;
+  const double WSOrg = production.WSOrg;
+  const double WRoot = production.WRoot;
+  const double WDead = production.WDead;
+  const double NStem = production.NStem;
+  const double NLeaf = production.NLeaf;
+  const double NSOrg = production.NSOrg;
+  const double NRoot = production.NRoot;
+  const double NDead = production.NDead;
+  const double old_DM = production.DM ();
+  
+  // Find C concentrations.
+  const double C_C_Stem = DM_to_C_factor (production.E_Stem);
+  const double C_C_Leaf = DM_to_C_factor (production.E_Leaf);
+  const double C_C_Dead = C_C_Leaf;
+  const double C_C_SOrg = DM_to_C_factor (production.E_SOrg);
+  const double C_C_Root = DM_to_C_factor (production.E_Root);
+
+  // Part of crop we attempt to harvest.
+  const double dead_harvest = 1.0;
+  const double sorg_harvest = 1.0;
+
+  // Harvested yield and losses left in the field at harvest
+  const double Stem_W_Yield = stem_harvest_frac * stem_harvest * WStem;
+  const double Dead_W_Yield = stem_harvest_frac * dead_harvest * WDead;
+  const double Leaf_W_Yield = leaf_harvest_frac * leaf_harvest * WLeaf;
+  const double SOrg_W_Yield = sorg_harvest_frac * sorg_harvest * WSOrg;
+  const double Stem_C_Yield = C_C_Stem * Stem_W_Yield;
+  const double Dead_C_Yield = C_C_Dead * Dead_W_Yield;
+  const double Leaf_C_Yield = C_C_Leaf * Leaf_W_Yield;
+  const double SOrg_C_Yield = C_C_SOrg * SOrg_W_Yield;
+  const double Stem_N_Yield = stem_harvest_frac * stem_harvest * NStem;
+  const double Dead_N_Yield = stem_harvest_frac * dead_harvest * NDead;
+  const double Leaf_N_Yield = leaf_harvest_frac * leaf_harvest * NLeaf;
+  const double SOrg_N_Yield = sorg_harvest_frac * sorg_harvest * NSOrg;
+
+  // Part of economic yield removed at harvest
+  const double WEYRm
+    = EconomicYield_W * SOrg_W_Yield; 
+  const double NEYRm
+    = EconomicYield_N * SOrg_N_Yield; 
+  const double CEYRm		// EY_W is used for both DM and C.
+    = EconomicYield_W * SOrg_C_Yield;
+
+  const double Crop_N_Yield
+    = Stem_N_Yield + Dead_N_Yield + Leaf_N_Yield + NEYRm;
+
+  // Find losses.
+  double Stem_W_Loss = (1.0 - stem_harvest_frac) * stem_harvest * WStem;
+  double Dead_W_Loss = (1.0 - stem_harvest_frac) * dead_harvest * WDead;
+  double Leaf_W_Loss = (1.0 - leaf_harvest_frac) * leaf_harvest * WLeaf;
+  double SOrg_W_Loss = (1.0 - sorg_harvest_frac) * sorg_harvest * WSOrg
+    + (1.0 - EconomicYield_W) * SOrg_W_Yield;
+  double Stem_N_Loss = (1.0 - stem_harvest_frac) * stem_harvest * NStem;
+  double Dead_N_Loss = (1.0 - stem_harvest_frac) * dead_harvest * NDead;
+  double Leaf_N_Loss = (1.0 - leaf_harvest_frac) * leaf_harvest * NLeaf;
+  double SOrg_N_Loss = (1.0 - sorg_harvest_frac) * sorg_harvest * NSOrg 
+    + (1.0 - EconomicYield_N) * SOrg_N_Yield;
+  const double Crop_N_Loss 
+    = Stem_N_Loss + Dead_N_Loss + Leaf_N_Loss + SOrg_N_Loss;
+
+  production.WStem -= (Stem_W_Yield + Stem_W_Loss);
+  production.WDead -= (Dead_W_Yield + Dead_W_Loss);
+  production.WLeaf -= (Leaf_W_Yield + Leaf_W_Loss);
+  production.WSOrg -= (WEYRm + SOrg_W_Loss);
+  production.NStem -= (Stem_N_Yield + Stem_N_Loss);
+  production.NDead -= (Dead_N_Yield + Dead_N_Loss);
+  production.NLeaf -= (Leaf_N_Yield + Leaf_N_Loss);
+  production.NSOrg -= (NEYRm + SOrg_N_Loss);
+  production.NCrop -= 
+    (Crop_N_Yield + Crop_N_Loss - (Dead_N_Yield + Dead_N_Loss));
+
+  production.WStem = max(0.0, production.WStem);
+  production.WDead = max(0.0, production.WDead);
+  production.WLeaf = max(0.0, production.WLeaf);
+  production.WSOrg = max(0.0, production.WSOrg);
+  production.NStem = max(0.0, production.NStem);
+  production.NDead = max(0.0, production.NDead);
+  production.NLeaf = max(0.0, production.NLeaf);
+  production.NSOrg = max(0.0, production.NSOrg);
+  production.NCrop = max(0.0, production.NCrop);
+
+  // Put tiny losses away in the "Stem" pool.
+  if (Dead_W_Loss < 0.1)
+    {
+      Stem_W_Loss += Dead_W_Loss;
+      Stem_N_Loss += Dead_N_Loss;
+      Dead_W_Loss = 0.0;
+      Dead_N_Loss = 0.0;
+    }
+  if (Leaf_W_Loss < 0.1)
+    {
+      Stem_W_Loss += Leaf_W_Loss;
+      Stem_N_Loss += Leaf_N_Loss;
+      Leaf_W_Loss = 0.0;
+      Leaf_N_Loss = 0.0;
+    }
+  if (SOrg_W_Loss < 0.1)
+    {
+      Stem_W_Loss += SOrg_W_Loss;
+      Stem_N_Loss += SOrg_N_Loss;
+      SOrg_W_Loss = 0.0;
+      SOrg_N_Loss = 0.0;
+    }
+
+  // Add crop remains to the soil.
+  if (Stem_W_Loss > 0.0)
+    {
+      const double C = C_C_Stem * Stem_W_Loss;
+      const double N = Stem_N_Loss;
+      AM& am = AM::create (geometry, time, Stem, crop_name, "stem");
+      am.add (C * m2_per_cm2, N * m2_per_cm2);
+      assert (C == 0.0 || N > 0.0);
+      residuals.push_back (&am);
+      production.C_AM += C;
+      production.N_AM += N;
+    }
+ if (Dead_W_Loss > 0.0)
+   {
+     const double C = C_C_Dead * Dead_W_Loss;
+     const double N = Dead_N_Loss;
+     if (!production.AM_leaf)
+        production.AM_leaf
+	  = &AM::create (geometry, time, Dead, crop_name, "dead", 
+			 AM::Unlocked);
+     production.AM_leaf->add (C * m2_per_cm2, N * m2_per_cm2);
+     assert (C == 0.0 || N > 0.0);
+     production.C_AM += C;
+     production.N_AM += N;
+   }
+ if (Leaf_W_Loss > 0.0)
+   {
+     const double C = C_C_Leaf * Leaf_W_Loss;
+     const double N = Leaf_N_Loss;
+     AM& am = AM::create (geometry, time, Leaf, crop_name, "leaf");
+     assert (C == 0.0 || N > 0.0);
+     am.add ( C * m2_per_cm2, N * m2_per_cm2);
+     residuals.push_back (&am);
+     production.C_AM += C;
+     production.N_AM += N;
+   }
+ if (SOrg_W_Loss > 0.0)
+   {
+     const double C = C_C_SOrg * SOrg_W_Loss;
+     const double N = SOrg_N_Loss;
+     AM& am = AM::create (geometry, time, SOrg, crop_name, "sorg");
+     assert (C == 0.0 || N > 0.0);
+     am.add ( C * m2_per_cm2, N * m2_per_cm2);
+     residuals.push_back (&am);
+     production.C_AM += C;
+     production.N_AM += N;
+   }
+
+  // Dead or alive?
+  if (!kill_off && DS < DSmax && leaf_harvest < 1.0)
+    {
+      // Cut delay.
+      const double new_DM = production.DM ();
+      const double removed_DM = old_DM - new_DM;
+      production_delay = cut_delay (removed_DM);
+      if (!last_cut)
+	last_cut = new Time (time);
+      else
+	*last_cut = time;
+    }
+  else
+    {
+      DS = Crop::DSremove;
+
+     // Update and unlock locked AMs.
+      if (!production.AM_root)
+	production.AM_root = &AM::create (geometry, time, Root,
+					crop_name, "root", AM::Unlocked);
+      if (accumulate (density.begin (), density.end (), 0.0) > 0.0)
+	production.AM_root->add (geometry,
+			       WRoot * C_C_Root * m2_per_cm2,
+			       NRoot * m2_per_cm2,
+			       density);
+      else
+	production.AM_root->add (WRoot * C_C_Root * m2_per_cm2,
+			       NRoot * m2_per_cm2);
+      assert (WRoot == 0.0 || NRoot > 0.0);
+      if (production.AM_root->locked ())
+	production.AM_root->unlock (); // Stored in organic matter.
+      else
+	residuals.push_back (production.AM_root);	// No organic matter.
+      production.AM_root = NULL;
+
+      if (production.AM_leaf)
+	{
+	  if (production.AM_leaf->locked ())
+	    production.AM_leaf->unlock (); // Stored in organic matter.
+	  else
+	    residuals.push_back (production.AM_leaf);// No organic matter.
+	  production.AM_leaf = NULL;
+	}
+    }
+
+  return *new Harvest (column_name, time, crop_name,
+		       Stem_W_Yield, Stem_N_Yield, Stem_C_Yield,
+		       Dead_W_Yield, Dead_N_Yield, Dead_C_Yield,
+		       Leaf_W_Yield, Leaf_N_Yield, Leaf_C_Yield,
+		       WEYRm, NEYRm, CEYRm, chemicals);
+}
+
+void
+Harvesting::tick (const Time& time)
+{ 
+  if (production_delay < 0.01)
+    cut_stress = 0.0;
+  else
+    {
+      assert (last_cut);
+      const double days_between 
+	= (0.0 + Time::hours_between (*last_cut, time)) / 24.0;
+      assert (days_between >= 0.0);
+      
+      if (days_between >= production_delay)
+	{
+	  cut_stress = 0.0;
+	  production_delay = 0.0;
+	}
+      else
+	{
+	  cut_stress = 1.0 - (exp (M_LN2 / production_delay * days_between)
+			      - 1.0);
+	  assert (cut_stress >= 0.0 && cut_stress <= 1.0);
+	}
+    }
+}
+
+void 
+Harvesting::output (Log& log) const
+{ 
+  if (last_cut)
+    log.output ("last_cut", *last_cut);
+  log.output ("production_delay", production_delay);
+  log.output ("cut_stress", cut_stress);
+}
+
+void 
+Harvesting::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  // Submodel.
+  alist.add ("submodel", "Harvesting");
+  alist.add ("description", 
+	     "Information about what happens to the crop at harvest and cut.");
+
+  syntax.add_submodule_sequence ("Stem", Syntax::Const, 
+				 "Stem AOM parameters.", OM::load_syntax);
+  alist.add ("Stem", AM::default_AOM ());
+  syntax.add_submodule_sequence ("Leaf", Syntax::Const,
+				 "Leaf AM parameters.", OM::load_syntax);
+  alist.add ("Leaf", AM::default_AOM ());
+  syntax.add_submodule_sequence ("Dead", Syntax::Const,
+				 "Dead leaves AM parameters.",
+				 OM::load_syntax);
+  alist.add ("Dead", AM::default_AOM ());
+  syntax.add_submodule_sequence ("SOrg", Syntax::Const,
+				 "Storage organ AM parameters.", 
+				 OM::load_syntax);
+  alist.add ("SOrg", AM::default_AOM ());
+  syntax.add_submodule_sequence ("Root", Syntax::Const,
+				 "Root AM parameters.", OM::load_syntax);
+  alist.add ("Root", AM::default_AOM ());
+  syntax.add ("EconomicYield_W", Syntax::None (), Syntax::Const, "\
+Valuable fraction of storage organ (DM), e.g. grain or tuber.");
+  alist.add ("EconomicYield_W", 1.00);
+  syntax.add ("EconomicYield_N", Syntax::None (), Syntax::OptionalConst,
+               "Valuable fraction of storage organ (N).\n\
+By default the value for DM is used.");
+  static const RangeII range_max (0.0, 2.0);
+  syntax.add ("DSmax", Syntax::None (), range_max, Syntax::Const, "\
+Maximal development stage for which the crop survives harvest.");
+  alist.add ("DSmax", 0.80);
+  static const RangeII range_new (0.0, 1.0);
+  syntax.add ("DSnew", Syntax::None (), range_new, Syntax::OptionalConst,
+	       "New development stage after harvest.\n\
+If not specified, use the DS where the crop first reached the height\n\
+it now has after the cut.");
+  syntax.add ("last_cut", Syntax::Date, Syntax::OptionalState,
+	      "Date of last cut.  Used for calculating cut delay.");
+  syntax.add ("production_delay", "d", Syntax::State,
+	      "production delay caused by last cut");
+  alist.add ("production_delay", 0.0);
+  syntax.add ("cut_delay", "kg DM/ha", "d", Syntax::Const,
+	      "\
+Production and development delay in days as a function of the shoot DM\n\
+removed by harvest.  By default, there is no delay.");
+  PLF no_delay;
+  no_delay.add (0.0, 0.0);
+  no_delay.add (1.0, 0.0);
+  alist.add ("cut_delay", no_delay);
+  syntax.add_fraction ("cut_stress", Syntax::LogOnly, 
+		       "Stress induced due to last cut.");
+}
+
+Harvesting::Harvesting (const AttributeList& al)
+  : Stem (al.alist_sequence ("Stem")),
+    Leaf (al.alist_sequence ("Leaf")),
+    Dead (al.alist_sequence ("Dead")),
+    SOrg (al.alist_sequence ("SOrg")),
+    Root (al.alist_sequence ("Root")),
+    EconomicYield_W (al.number ("EconomicYield_W")),
+    EconomicYield_N (al.check ("EconomicYield_N")
+                     ? al.number ("EconomicYield_N")
+                     : al.number ("EconomicYield_W")),
+    DSmax (al.number ("DSmax")),
+    DSnew (al.check ("DSnew") ? al.number ("DSnew") : -44.0),
+    last_cut (al.check ("last_cut") ? new Time (al.time ("last_cut")) : NULL),
+    production_delay (al.number ("production_delay")),
+    cut_delay (al.plf ("cut_delay")),
+    cut_stress (0.0)
+{ }
+
+Harvesting::~Harvesting ()
+{ 
+  if (last_cut)
+    delete last_cut;
+}
+
+static Submodel::Register 
+soil_submodel ("Harvesting", Harvesting::load_syntax);
