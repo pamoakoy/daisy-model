108fc3e158886350469854fb4e818c66c4e0ad62
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2002-05-03 12:12:59 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_27'.

---
+++ tags/release_2_27/action_repeat.C
@@ -0,0 +1,108 @@
+// action_repeat.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "action.h"
+#include "log.h"
+
+struct ActionRepeat : public Action
+{
+  const AttributeList repeat;
+  Action* action;
+
+  void tick (const Daisy& daisy, Treelog& out)
+  { action->tick (daisy, out); }
+    
+  void doIt (Daisy& daisy, Treelog& out)
+  { 
+    if (action && action->done (daisy))
+      {
+	delete action;
+	action = NULL;
+      }
+    if (action == NULL)
+      action = &Librarian<Action>::create (repeat);
+    action->doIt (daisy, out);
+  }
+
+  bool done (const Daisy&) const
+  { 
+    return false;
+  }
+
+  void output (Log& log) const
+  { 
+    output_derived (*action, "do", log);
+  }
+
+  bool check (const Daisy& daisy, Treelog& err) const
+  { 
+    if (action)
+      return action->check (daisy, err);
+    else
+      return true;
+  }
+
+  static AttributeList add_do (const AttributeList& al)
+  {
+    AttributeList alist (al);
+    if (!alist.check ("do"))
+      alist.add ("do", alist.alist ("repeat"));
+    return alist;
+  }
+
+  ActionRepeat (const AttributeList& al)
+    : Action (add_do (al)),
+      repeat (al.alist ("repeat")),
+      action (&Librarian<Action>::create (al.check ("do") 
+					  ? al.alist ("do")
+					  : repeat))
+  { }
+
+  ~ActionRepeat ()
+  { 
+    if (action)
+      delete action;
+  }
+};
+
+static struct ActionRepeatSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionRepeat (al); }
+
+  ActionRepeatSyntax ()
+    {
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description", "\
+Perform all the specified action.  When done, repeat the action.\n\
+The action may take several timesteps.");
+      syntax.add ("repeat", Librarian<Action>::library (),
+		  Syntax::Const, Syntax::Singleton,
+		  "Action to perform repeatedly.");
+      syntax.add ("do", Librarian<Action>::library (), 
+		  Syntax::OptionalState, Syntax::Singleton,
+		  "Action currently being performed.");
+      syntax.order ("repeat");
+      Librarian<Action>::add_type ("repeat", alist, syntax, &make);
+    }
+} ActionRepeat_syntax;
---
+++ tags/release_2_27/crop.h
@@ -0,0 +1,130 @@
+// crop.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef CROP_H
+#define CROP_H
+
+#include "time.h"
+#include "librarian.h"
+#include <vector>
+
+struct Log;
+struct Time;
+struct AttributeList;
+struct Bioclimate;
+struct PLF;
+struct Library;
+struct Syntax;
+struct SoilWater;
+struct Soil;
+struct Geometry;
+struct OrganicMatter;
+struct SoilHeat;
+struct SoilNH4;
+struct SoilNO3;
+struct Column;
+struct Harvest;
+struct AM;
+
+class Crop 
+{
+  // Content.
+public:
+  const AttributeList alist;	// Remember attributes for checkpoint.
+  const string name;
+  static const char *const description;
+
+  // Communication with Bioclimate.
+public:
+  virtual double water_stress () const;	// [0-1] (0 = full production)
+  virtual double nitrogen_stress () const; // [0-1] (1 = no production)
+  virtual double rs_min () const; // Minimum transpiration resistance.
+  virtual double rs_max () const; // Maximum transpiration resistance.
+  virtual double height () const = 0;
+  virtual double LAI () const = 0;
+  virtual const PLF& LAIvsH () const = 0;
+  virtual double PARext () const = 0;
+  virtual double PARref () const = 0;
+  virtual double EPext () const = 0;
+  virtual double IntcpCap () const = 0; // Interception Capacity.
+  virtual double EpFac () const = 0; // Convertion to potential evapotransp.
+  virtual double albedo () const;
+  virtual void CanopyStructure () = 0;
+  virtual double ActualWaterUptake (double Ept, const Soil&, SoilWater&, 
+				    double EvapInterception, Treelog&) = 0;
+  virtual void force_production_stress  (double pstress);
+
+  // Simulation.
+public:
+  virtual void tick (const Time& time, const Bioclimate&, const Soil&,
+		     OrganicMatter*, const SoilHeat&, const SoilWater&,
+		     SoilNH4*, SoilNO3*, 
+		     double& residuals_DM,
+		     double& residuals_N_top, double& residuals_C_top,
+		     vector<double>& residuals_N_soil, 
+		     vector<double>& residuals_C_soil,
+		     Treelog&) = 0;
+  virtual const Harvest& harvest (const string& column_name,
+				  const Time&, const Geometry&, 
+				  Bioclimate& bioclimate,
+				  double stub_length,
+				  double stem_harvest,
+				  double leaf_harvest, 
+				  double sorg_harvest,
+				  bool kill_off,
+				  vector<AM*>& residuals,
+				  double& residuals_DM,
+				  double& residuals_N_top,
+				  double& residuals_C_top,
+				  vector<double>& residuals_N_soil,
+				  vector<double>& residuals_C_soil,
+				  Treelog&) = 0;
+  void kill (const string&, const Time&, const Geometry&, Bioclimate&,
+	     vector<AM*>& residuals, 
+	     double& residuals_DM, 
+	     double& residuals_N_top, double& residuals_C_top,
+	     vector<double>& residuals_N_soil, 
+	     vector<double>& residuals_C_soil,
+	     Treelog&);
+  virtual void output (Log&) const = 0;
+  
+  // Queries.
+public:
+  static bool ds_remove (const Crop*);
+  virtual double DS () const = 0; // Development stage, [-1:2] or DSremove.
+  static const double DSremove;
+  virtual double DM () const = 0; // Shoot dry matter, [kg DM/ha].
+  virtual double total_N () const = 0; // N content [kg N/ha]
+
+  // Create and Destroy.
+public:
+  virtual void initialize (Treelog&, const Geometry&, OrganicMatter&);
+  virtual void initialize (Treelog&, const Geometry&) = 0;
+protected:
+  Crop (const AttributeList& al);
+public:
+  virtual ~Crop ();
+};
+
+static Librarian<Crop> Crop_init ("crop");
+
+#endif // CROP_H
