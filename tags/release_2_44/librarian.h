b052e05f2e6c4c1769e669c22a60a9a0994ebd84
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-02-04 11:37:24 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_44'.

---
+++ tags/release_2_44/librarian.h
@@ -0,0 +1,222 @@
+// librarian.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+#include "library.h"
+#include "alist.h"
+#include "syntax.h"
+#include "treelog.h"
+#include "assertion.h"
+#include <map>
+
+class Log;
+
+template <class T>
+class Librarian
+{
+  // Types.
+private:
+  typedef T& (*constructor) (const AttributeList&);
+  typedef map<string, constructor, less<string>/**/> map_type;
+  // Content.
+private:
+  static struct Content
+  {
+    Library lib;
+    map_type constructors;
+    int count;
+    Content (const char *const name, derive_fun derive, 
+	     const char *const description)
+      : lib (name, derive, description),
+	constructors (),
+	count (0)
+    { }
+  } *content;
+
+  // Functions.
+public:
+  static T& create (const AttributeList& al)
+  {
+    daisy_assert (al.check ("type"));
+    const string name = al.name ("type");
+    daisy_assert (library ().check (name));
+    daisy_assert (library ().syntax (name).check (al, Treelog::null ()));
+    return (content->constructors)[name] (al);
+  }
+  static void add_type (const string& name, AttributeList& al,
+			const Syntax& syntax,
+			constructor cons)
+  {
+    library ().add (name, al, syntax);
+    content->constructors.insert(make_pair (name, cons));
+  }
+  static void derive_type (const string& name, AttributeList& al,
+			   const string& super)
+  {
+    add_type (name, al, library ().syntax (super),
+	      (content->constructors)[super]);
+  }
+  static Library& library ()
+  {
+    daisy_assert (content);
+    return content->lib;
+  }
+
+  // Create and Destroy.
+public:
+  Librarian (const char *const name)
+  { 
+    if (!content)
+      content = new Content (name, &derive_type, T::description);
+    content->count++;
+
+  }
+  ~Librarian ()
+  { 
+    daisy_assert (content);
+    content->count--;
+    if (content->count == 0)
+      {
+	delete content;
+	content = 0;
+      }
+    else
+      daisy_assert (content->count > 0);
+  }
+};
+
+// This cutie will create a vector of objects from a vector of alists.
+#ifndef BORLAND_TEMPLATES
+template <class T> 
+vector<T*>
+map_create (const vector<AttributeList*>& f)
+{ 
+  vector<T*> t;
+  for (vector<AttributeList*>::const_iterator i = f.begin ();
+       i != f.end ();
+       i++)
+    t.push_back (&Librarian<T>::create (**i));
+  return t;
+}
+
+template <class T> 
+vector<const T*>
+map_create_const (const vector<AttributeList*>& f)
+{ 
+  vector<const T*> t;
+  for (vector<AttributeList*>::const_iterator i = f.begin ();
+       i != f.end ();
+       i++)
+    t.push_back (&Librarian<T>::create (**i));
+  return t;
+}
+
+template <class T> 
+vector<T*>
+map_construct (const vector<AttributeList*>& f)
+{ 
+  vector<T*> t;
+  for (vector<AttributeList*>::const_iterator i = f.begin ();
+       i != f.end ();
+       i++)
+    t.push_back (new T (**i));
+  return t;
+}
+
+template <class T> 
+vector<const T*>
+map_construct_const (const vector<AttributeList*>& f)
+{ 
+  vector<const T*> t;
+  for (vector<AttributeList*>::const_iterator i = f.begin ();
+       i != f.end ();
+       i++)
+    t.push_back (new T (**i));
+  return t;
+}
+
+#else
+
+template <class T> 
+struct map_create
+{
+  vector<T*> t;
+  map_create (const vector<AttributeList*>& f)
+  { 
+    for (vector<AttributeList*>::const_iterator i = f.begin ();
+	 i != f.end ();
+	 i++)
+      t.push_back (&Librarian<T>::create (**i));
+  }
+  operator vector<T*> ()
+  { return t; }
+};
+
+template <class T> 
+struct map_create_const
+{
+  vector<const T*> t;
+  map_create_const (const vector<AttributeList*>& f)
+  { 
+    for (vector<AttributeList*>::const_iterator i = f.begin ();
+	 i != f.end ();
+	 i++)
+      t.push_back (&Librarian<T>::create (**i));
+  }
+  operator vector<const T*> ()
+  { return t; }
+};
+
+template <class T> 
+struct map_construct
+{
+  vector<T*> t;
+  map_construct (const vector<AttributeList*>& f)
+  { 
+    for (vector<AttributeList*>::const_iterator i = f.begin ();
+	 i != f.end ();
+	 i++)
+      t.push_back (new T (**i));
+  }
+  operator vector<T*> ()
+  { return t; }
+};
+
+template <class T> 
+struct map_construct_const
+{
+  vector<const T*> t;
+  map_construct_const (const vector<AttributeList*>& f)
+  { 
+    for (vector<AttributeList*>::const_iterator i = f.begin ();
+	 i != f.end ();
+	 i++)
+      t.push_back (new T (**i));
+  }
+  operator vector<const T*> ()
+  { return t; }
+};
+#endif
+
+#endif // LIBRARIAN_H
---
+++ tags/release_2_44/syntax.h
@@ -0,0 +1,253 @@
+// syntax.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef SYNTAX_H
+#define SYNTAX_H
+
+#include "common.h"
+#include "treelog.h"
+#include <vector>
+
+class AttributeList;
+class Library;
+class Check;
+
+class Syntax
+{ 
+  struct Implementation;
+  friend struct Implementation;
+  Implementation& impl;
+public:
+  // A syntax entry has an associated size.  If the size is a positive
+  // integer, the syntax entry specifies an array of that size.  The
+  // default size 'Singleton' indicating that the syntax entry match a
+  // single item of the specified type, while the 'Sequence' used for
+  // entries that contain an array of unspecified length. 
+  static const int Singleton;	
+  static const int Sequence;
+  static const int Unspecified;
+
+  // A syntax may have a dimension associated.
+  static const string& Unknown ();
+  static const string& None ();
+  static const string& Fraction ();
+
+  // Each syntax entry should have an associated type.
+  enum type 
+  { Number, AList, PLF, Boolean, String,
+    Date, Integer, Object, Library, Error };
+  static const char* type_name (type);
+  static type type_number (const char* name);
+    
+  // The requirements with regard to input and output varies with each
+  // syntax entry.
+  enum category
+  {
+    // This is a parameter, i.e. its value doesn't change during the
+    // compilation, and it cannot be written to the log.
+    Const,
+    // This a state variable, it must be provided at initialization
+    // and can be written to the log.
+    State,
+    // This is a state variable that can be computed from other
+    // parameters or state variables, and therefore does not need to
+    // be specified before the simulation starts. 
+    OptionalState, 
+    // This is a paramter that can be computer from other parameters,
+    // and therefore does not need to be specified before 
+    // the simulation starts. 
+    OptionalConst, 
+    // This is a variable that is only computed for logging purposes
+    // and not a part of the simulation state. 
+    LogOnly
+  };
+  static const char* category_name (category);
+  static int category_number (const char* name);
+
+  // This function will check that an alist conform to the syntax.
+  bool check (const AttributeList&, Treelog& err) const;
+  
+  // Check that a numeric value is within the allowed range.
+  void check (const string& key, double value) const;
+
+  // These functions will allow you to lookup information about a
+  // specific syntax entry. 
+  bool is_const (const string&) const;
+  bool is_optional (const string&) const;
+  bool is_log (const string&) const;
+  bool is_state (const string&) const;
+
+  type lookup (const string&) const;
+  const Syntax& syntax (const string&) const;
+public:
+  ::Library& library (const string&) const;
+  int  size (const string&) const;
+  const string& dimension (const string&) const;
+  const string& domain (const string&) const;
+  const string& range (const string&) const;
+  const string& description (const string&) const;
+  bool ordered () const;
+  const vector<string>& order () const;
+  int order (const string& name) const;	// Return index in order, or -1.
+  bool total_order () const;	// True iff all members are ordered.
+  const AttributeList& default_alist (const string&) const;
+
+  // Get a list of all entries.
+  void entries (vector<string>&) const;
+  unsigned int entries () const;
+
+  // Add syntax entries
+  void add (const string& key,	// Generic.
+	    type t, 
+	    category cat,
+	    int size,
+	    const string& description);
+  void add (const string& key,
+	    type t, 
+	    category cat,
+	    const string& description)
+  { add (key, t, cat, Singleton, description); }
+
+  void add (const string& key, // Number.
+	    const string& dim,
+	    category cat,
+	    int size,
+	    const string& description);
+  void add (const string& key, 
+	    const string& dim,
+	    category cat,
+	    const string& description)
+  { add (key, dim, cat, Singleton, description); } 
+  void add (const string& key,
+	    const string& dim,
+	    const Check& check,
+	    category cat,
+	    int size,
+	    const string& description);
+  void add (const string& key, 
+	    const string& dim,
+	    const Check& check,
+	    category cat,
+	    const string& description)
+  { add (key, dim, check, cat, Singleton, description); } 
+  void add_fraction (const string& key, 
+		     category cat,
+		     int size,
+		     const string& description);
+  void add_fraction (const string& key, 
+		     category cat,
+		     const string& description);
+
+  void add (const string& key, // PLF.
+	    const string& domain,
+	    const string& range,
+	    category cat,
+	    int size,
+	    const string& description);
+  void add (const string& key, 
+	    const string& domain,
+	    const string& range,
+	    category cat,
+	    const string& description)
+  { add (key, domain, range, cat, Singleton, description); } 
+  void add (const string& key,
+	    const string& domain,
+	    const string& range,
+	    const Check& check,
+	    category cat,
+	    int size,
+	    const string& description);
+  void add (const string& key, 
+	    const string& domain,
+	    const string& range,
+	    const Check& check,
+	    category cat,
+	    const string& description)
+  { add (key, domain, range, check, cat, Singleton, description); } 
+
+  void add (const string& key,  // AList
+	    const Syntax& syntax,
+	    int size,
+	    const string& description)
+  { add (key, syntax, State, size, description); }
+  void add (const string& key,  // AList
+	    const Syntax& syntax,
+	    const string& description)
+  { add (key, syntax, State, Singleton, description); }
+  void add (const string&, const Syntax&,
+	    category cat, int size, 
+	    const string& description);
+  void add (const string&, const Syntax&, const AttributeList&,	
+	    // Alist sequence with default element.
+	    category, int size, const string& description);
+
+  void add (const string& key, // Object
+	    ::Library& lib, 
+	    int size,
+	    const string& description)
+  { add (key, lib, State, size, description); }
+  void add (const string& key,
+	    ::Library& lib, 
+	    const string& description)
+  { add (key, lib, State, Singleton, description); }
+  void add (const string&, ::Library&,
+	    category, int size, const string& description);
+
+  void add_library (const string&, ::Library&);
+
+  typedef void (*load_syntax_fun) (Syntax& syntax, AttributeList& alist);
+  void add_submodule (const string& name, AttributeList& alist,
+		      Syntax::category cat, const string& description,
+		      load_syntax_fun load_syntax);
+  void add_submodule_sequence (const string& name, Syntax::category cat, 
+			       const string& description,
+			       load_syntax_fun load_syntax);
+		      
+  // It is possible to impose an order on the syntax entries, which
+  // will allow the input module to parse the entries without the user
+  // having to specify the names of the entries.  It is recommended
+  // *not* to use this in general, as it makes it more difficult to
+  // add new entries.
+  void order (const vector<string>&);
+  void order (const string&);
+  void order (const string&, const string&);
+  void order (const string&, const string&, const string&);
+  void order (const string&, const string&, const string&, const string&);
+  void order (const string&, const string&, const string&, const string&,
+	      const string&);
+
+  // Create and Destroy.
+
+  // A check_fun is a function used for extra syntax checking.
+  typedef bool (*check_fun)(const AttributeList&, Treelog& err);
+  typedef bool (*check_list_fun)(const vector<AttributeList*>&, Treelog& err);
+  void add_check (check_fun);
+  void add_check (check_list_fun);
+
+  Syntax ();
+  ~Syntax ();
+private:
+  Syntax (Syntax&);
+  Syntax& operator= (Syntax&);
+};
+
+#endif // SYNTAX_H
