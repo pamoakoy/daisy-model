c058d3eca3e4e746aac9fdeb6f4a0b8d09f4e9cc
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-02-06 13:32:09 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_46'.

---
+++ tags/release_2_46/groundwater.h
@@ -0,0 +1,69 @@
+// groundwater.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef GROUNDWATER_H
+#define GROUNDWATER_H
+
+#include "uzmodel.h"
+#include "librarian.h"
+
+class Time;
+class Treelog;
+class SoilHeat;
+
+class Groundwater : public UZbottom
+{
+  // Content.
+public:
+  const string name;
+  static const char *const description;
+
+  // Simulation.
+public:
+  virtual void tick (const Time& time, Treelog&) = 0;
+  virtual void update_water (const Soil&, const SoilHeat&,
+			     vector<double>& S_sum,
+			     vector<double>& S_drain,
+			     vector<double>& h,
+			     vector<double>& h_ice,
+			     vector<double>& Theta,
+			     vector<double>& q,
+			     vector<double>& q_p);
+  virtual void output (Log&) const;
+
+  // Accessors.
+public:
+  virtual double table () const = 0;
+
+    // Create and Destroy.
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  virtual void initialize (const Time& time, const Soil&, Treelog&);
+protected:
+  Groundwater (const AttributeList& al);
+public:
+  virtual ~Groundwater ();
+};
+
+static Librarian<Groundwater> Groundwater_init ("groundwater");
+
+#endif // GROUNDWATER_H
---
+++ tags/release_2_46/traverse_delete.C
@@ -0,0 +1,223 @@
+// traverse_delete.C -- FInd and remove dependencies in datastructures.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "traverse_delete.h"
+#include "traverse.h"
+#include "library.h"
+#include "syntax.h"
+#include "alist.h"
+#include "assertion.h"
+
+class TraverseDelete : public Traverse
+{
+private:
+  // What depend on this parameterization?
+  const string dep_lib;
+  const string dep_par;
+  string dep_super;
+
+  // The parameterization we currently test.
+  bool found;
+
+  // Create & Destroy.
+public:
+  TraverseDelete (const string& component, const string& parameterization);
+  ~TraverseDelete ();
+
+private:
+  // Implementation.
+  bool enter_library (Library& library, 
+		      const string& component);
+  void leave_library ();
+  bool enter_model (const Syntax&, AttributeList&, 
+		    const string& component, const string& model);
+  void leave_model (const string& component, const string& name);
+  bool enter_submodel (const Syntax& syntax, AttributeList& alist,
+  		       const AttributeList& default_alist,
+  		       const string& name);
+  void leave_submodel ();
+  bool enter_submodel_default (const Syntax& syntax, 
+			       const AttributeList& default_alist,
+			       const string& name);
+  void leave_submodel_default ();
+  bool enter_submodel_sequence (const Syntax& syntax,
+  				AttributeList& alist,
+  				const AttributeList& default_alist,
+  				const string& name, unsigned index);
+  void leave_submodel_sequence ();
+  bool enter_submodel_sequence_default (const Syntax& syntax, 
+  					const AttributeList& default_alist,
+  					const string& name);
+  void leave_submodel_sequence_default ();
+  bool enter_object (const Library&, 
+		     const Syntax& syntax, AttributeList& alist,
+  		     const AttributeList& default_alist,
+  		     const string& name);
+  void leave_object ();
+  bool enter_object_sequence (const Library&, const Syntax& syntax,
+  			      AttributeList& alist,
+  			      const AttributeList& default_alist,
+  			      const string& name, 
+  			      unsigned index);
+  void leave_object_sequence ();
+  bool enter_parameter (const Syntax&, AttributeList& alist, 
+			const AttributeList& default_alist, 
+			const string& name, const string& parameter);
+  void leave_parameter ();
+};
+
+bool
+TraverseDelete::enter_library (Library&, const string&)
+{ return true; }
+
+void
+TraverseDelete::leave_library ()
+{ }
+
+bool
+TraverseDelete::enter_model (const Syntax&, AttributeList& alist,
+			     const string& component, const string&)
+{
+  // Check if this model is inherited from the model we are examining.
+  if (dep_lib == component
+      && alist.check ("type") 
+      && alist.name ("type") == dep_par)
+    alist.add ("type", dep_super);
+
+  return true;
+}
+
+void
+TraverseDelete::leave_model (const string&, const string&)
+{ }
+
+bool
+TraverseDelete::enter_submodel (const Syntax&, AttributeList&,
+				const AttributeList&,
+				const string&)
+{ return true; }
+
+void
+TraverseDelete::leave_submodel ()
+{ }
+
+bool
+TraverseDelete::enter_submodel_default (const Syntax&, const AttributeList&, 
+					const string&)
+{ return false; }
+
+void
+TraverseDelete::leave_submodel_default ()
+{ daisy_assert (false); }
+
+bool
+TraverseDelete::enter_submodel_sequence (const Syntax&,
+					 AttributeList&,
+					 const AttributeList&,
+					 const string&, unsigned)
+{ return true; }
+
+void
+TraverseDelete::leave_submodel_sequence ()
+{ }
+
+bool
+TraverseDelete::enter_submodel_sequence_default (const Syntax&, 
+						 const AttributeList&,
+						 const string&)
+{ return false; }
+
+void
+TraverseDelete::leave_submodel_sequence_default ()
+{ daisy_assert (false); }
+
+bool
+TraverseDelete::enter_object (const Library& library, 
+			      const Syntax&, AttributeList& alist,
+			      const AttributeList&,
+			      const string&)
+{
+  daisy_assert (alist.check ("type"));
+  const string super = alist.name ("type");
+  if (dep_lib == library.name () && super == dep_par)
+    alist.add ("type", dep_super);
+
+  return true; 
+}
+
+void
+TraverseDelete::leave_object ()
+{ }
+
+bool
+TraverseDelete::enter_object_sequence (const Library& library, 
+				       const Syntax& syntax, 
+				       AttributeList& alist,
+				       const AttributeList& default_alist,
+				       const string&, unsigned)
+{ return enter_object (library, syntax, alist, default_alist, "dummy"); }
+
+void
+TraverseDelete::leave_object_sequence ()
+{ leave_object (); }
+
+bool
+TraverseDelete::enter_parameter (const Syntax&, AttributeList&, 
+				 const AttributeList&, 
+				 const string&, const string&)
+{ return true; }
+
+void 
+TraverseDelete::leave_parameter ()
+{ }
+
+TraverseDelete::TraverseDelete (const string& component,
+				const string& parameterization)
+  : dep_lib (component),
+    dep_par (parameterization),
+    found (false)
+{ 
+  const Library& library = Library::find (component);
+  daisy_assert (library.check (parameterization));
+  const AttributeList& alist = library.lookup (parameterization);
+  daisy_assert (alist.check ("type"));
+  dep_super = alist.name ("type");
+  daisy_assert (parameterization != dep_super);
+}
+
+TraverseDelete::~TraverseDelete ()
+{ }
+
+void
+remove_dependencies (const string& component, const string& parameterization)
+{
+  TraverseDelete depend (component, parameterization);
+  depend.traverse_all_libraries ();
+}
+
+void
+remove_dependencies (const string& component, const string& parameterization, 
+		     const Syntax& syntax, AttributeList& alist)
+{
+  TraverseDelete depend (component, parameterization);
+  depend.traverse_submodel (syntax, alist, AttributeList (), "dummy");
+}
