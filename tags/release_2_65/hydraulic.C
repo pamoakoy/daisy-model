43752046e03207fe723cf9d9b5d9da8feec5eda3
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-04-10 11:34:32 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_65'.

---
+++ tags/release_2_65/hydraulic.C
@@ -0,0 +1,143 @@
+// hydraulic.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "hydraulic.h"
+#include "plf.h"
+#include "log.h"
+#include "tmpstream.h"
+#include "check_range.h"
+#include "mathlib.h"
+
+void 
+Hydraulic::set_porosity (double Theta)
+{ 
+  daisy_assert (Theta > Theta_res);
+  Theta_sat = Theta; 
+}
+
+void 
+Hydraulic::output (Log& log) const
+{
+  log.output ("Theta_sat", Theta_sat); 
+}
+
+void
+Hydraulic::K_to_M (PLF& plf, const int intervals) const
+{
+  static const double h0 = -20000.0;
+  const double Ksat = K (0.0);
+  const double max_change = pow (Ksat / K (h0), 1.0 / intervals);
+  double step = (0 - h0) / 4.0;
+
+  double h = h0;
+  double sum = 0.0;
+  while (h < 0)
+    {
+      plf.add (h, sum);
+      step *= 2;
+      while (K (h + step) / K (h) > max_change)
+	{
+	  if (step < 1e-15)
+	    {
+	      TmpStream tmp;
+	      tmp () << "Hydraulic conductivity changes too fast in " 
+		     << name << "\n";
+	      tmp () << "h = " << h << ", step = " << step 
+		     << " and h + step = " << (h + step) << "\n";
+	      tmp () << "K (h) = " << K (h) << ", K (h + step) = "
+		     << K (h + step) << " and K (0) = " << Ksat << "\n";
+	      tmp () << "Change = " << K (h + step) / K (h) 
+		     << " > Max = " << max_change;
+	      daisy_warning (tmp.str ());
+	      break;
+	    }
+	  step /= 2;
+	}
+      sum += step * (K (h) + K (h + step)) / 2;
+      h += step;
+    }
+  plf.add (h, sum);
+}
+
+bool 
+Hydraulic::zero_Theta_res (const AttributeList& al, Treelog& err)
+{
+  bool ok = true;
+  
+  if (al.number ("Theta_res") != 0.0)
+    {
+      err.error ("Theta_res should be zero for this model");
+      ok = false;
+    }
+  return ok;
+}
+
+static bool
+check_alist (const AttributeList& al, Treelog& err)
+{
+  bool ok = true;
+
+  const double Theta_res = al.number ("Theta_res");
+  const double Theta_sat = al.number ("Theta_sat");
+
+  if (Theta_res >= Theta_sat)
+    {
+      err.error ("Theta_sat should be above Theta_res");
+      ok = false;
+    }
+  return ok;
+}  
+
+
+void
+Hydraulic::load_syntax (Syntax& syntax, AttributeList& alist)
+{ 
+  syntax.add_check (check_alist);
+  static RangeEI K_sat_range (0.0, 0.9);
+  syntax.add ("Theta_sat", "cm^3 H2O/cm^3", K_sat_range, Syntax::State,
+	      "Saturation point.");
+  syntax.add ("Theta_res", "cm^3 H2O/cm^3", Check::fraction (), Syntax::Const,
+	      "Soil residual water.");
+  alist.add ("Theta_res", 0.0);
+}
+
+void
+Hydraulic::initialize (double /* clay */, double /* silt */, double /* sand */,
+		       double /* humus */, double /* rho_b */,
+		       bool /* top_soil */, Treelog&)
+{ }
+
+Hydraulic::Hydraulic (const AttributeList& al)
+  : name (al.name ("type")),
+    Theta_sat (al.number ("Theta_sat")),
+    Theta_res (al.number ("Theta_res"))
+{ }
+
+Hydraulic::~Hydraulic ()
+{ }
+
+EMPTY_TEMPLATE
+Librarian<Hydraulic>::Content* Librarian<Hydraulic>::content = NULL;
+
+const char *const Hydraulic::description = "\
+This component is responsible for specifying the soils hydraulic\n\
+properties.";
---
+++ tags/release_2_65/log_harvest.C
@@ -0,0 +1,233 @@
+// log_harvest.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "log.h"
+#include "daisy.h"
+#include "harvest.h"
+#include "version.h"
+#include "tmpstream.h"
+#include <fstream>
+#include <time.h>
+
+struct LogHarvest : public Log
+{
+  // Filter function.
+  bool check_member (const string&) const
+  { return false; }
+  bool check_derived (const string&, const string&, const Library&) const
+  { return false; }
+
+  // Content.
+  unsigned int last_size;
+  string file;			// Filename.
+  ofstream out;			// Output stream.
+  bool print_header;		// Set if header should be printed.
+  bool print_tags;		// Set if tags should be printed.
+  bool print_dimension;		// Set if dimensions should be printed.
+  const bool print_N;		// Set if nitrogen content should be printed.
+  const bool print_C;		// Set if carbon content should be printed.
+
+  // Checking to see if we should log this time step.
+  bool match (const Daisy& daisy, Treelog&)
+  {
+    if (print_header)
+      {
+	print_dlf_header (out, daisy.alist);
+	print_header = false;
+      }
+    if (print_tags)
+      {
+	out << "year\tmonth\tday\tcolumn\tcrop"
+	    << "\tstem_DM\tdead_DM\tleaf_DM\tsorg_DM";
+	if (print_N)
+	  out << "\tstem_N\tdead_N\tleaf_N\tsorg_N";
+	if (print_C)
+	  out << "\tstem_C\tdead_C\tleaf_C\tsorg_C";
+	out << "\n";
+	print_tags = false;
+      }
+    if (print_dimension)
+      {
+	out << "\t\t\t\t"
+	    << "\tt/ha\tt/ha\tt/ha\tt/ha";
+	if (print_N)
+	  out << "\tkg/ha\tkg/ha\tkg/ha\tkg/ha";
+	if (print_C)
+	  out << "\tkg/ha\tkg/ha\tkg/ha\tkg/ha";
+	out << "\n";
+	print_dimension = false;
+      }
+    for (; last_size < daisy.harvest.size (); last_size++)
+      {
+	const Harvest& harvest = *(daisy.harvest[last_size]);
+	out << harvest.time.year ()
+	    << "\t" << harvest.time.month ()
+	    << "\t" << harvest.time.mday ()
+	    << "\t" << harvest.column
+	    << "\t" << harvest.crop
+	    << "\t" << harvest.stem_DM * 0.01
+	    << "\t" << harvest.dead_DM * 0.01
+	    << "\t" << harvest.leaf_DM * 0.01
+	    << "\t" << harvest.sorg_DM * 0.01;
+	if (print_N)
+	  out << "\t" << harvest.stem_N * 10.0
+	      << "\t" << harvest.dead_N * 10.0
+	      << "\t" << harvest.leaf_N * 10.0
+	      << "\t" << harvest.sorg_N * 10.0;
+	if (print_C)
+	  out << "\t" << harvest.stem_C * 10.0
+	      << "\t" << harvest.dead_C * 10.0
+	      << "\t" << harvest.leaf_C * 10.0
+	      << "\t" << harvest.sorg_C * 10.0;
+	out << "\n";
+	out.flush ();
+      }
+    return false;
+  }
+
+  void done ()
+  { daisy_assert (false); }
+
+  // Normal items.
+  void open (const string&)
+  { daisy_assert (false); }
+  void close ()
+  { daisy_assert (false); }
+
+  // Unnamed items.
+  void open_unnamed ()
+  { daisy_assert (false); }
+  void close_unnamed ()
+  { daisy_assert (false); }
+
+  // Derived items.
+  void open_derived (const string&, const string&)
+  { daisy_assert (false); }
+  void close_derived ()
+  { daisy_assert (false); }
+
+  // Derived items in a list.
+  void open_entry (const string&, const AttributeList&)
+  { daisy_assert (false); }
+  void close_entry ()
+  { daisy_assert (false); }
+
+  // Named derived items in a list.
+  void open_named_entry (const string&, const string&, const AttributeList&)
+  { daisy_assert (false); }
+  void close_named_entry ()
+  { daisy_assert (false); }
+
+  void output (const string&, const Time&)
+  { }
+  void output (const string&, const bool)
+  { }
+  void output (const string&, const double)
+  { }
+  void output (const string&, const int)
+  { }
+  void output (const string&, const string&)
+  { }
+  void output (const string&, const vector<double>&)
+  { }
+  void output (const string&, const PLF&)
+  { }
+
+  // Create and Destroy.
+  bool check (const Syntax&, Treelog& msg) const
+  { 
+    Treelog::Open nest (msg, name);
+    bool ok = true;
+    if (!out.good ())
+      {
+	TmpStream tmp;
+	tmp () << "Write error for '" << file << "'";
+	msg.error (tmp.str ());
+	ok = false;
+      }
+    return ok; 
+  }
+
+  LogHarvest (const AttributeList& al)
+    : Log (al),
+      last_size (0),
+      file (al.name ("where")),
+      out (file.c_str ()),
+      print_header (al.flag ("print_header")),
+      print_tags (al.flag ("print_tags")),
+      print_dimension (al.flag ("print_dimension")),
+      print_N (al.flag ("print_N")),
+      print_C (al.flag ("print_C"))
+  {
+    // Header.
+    if (print_header)
+      {
+	out << "dlf-0.0 -- harvest\n\n";
+	out << "VERSION: " << version  << "\n";
+	out << "FILE: " << file  << "\n";
+	time_t now = time (NULL);
+	out << "RUN: " << ctime (&now) << "\n";
+      }
+    out.flush ();
+  }
+
+  ~LogHarvest ()
+  {
+    if (!out.good ())
+      throw (string ("Problems writing to '") + file + "'");
+  }
+};
+
+static struct LogHarvestSyntax
+{
+  static Log& make (const AttributeList& al)
+  { return *new LogHarvest (al); }
+
+  LogHarvestSyntax ()
+  {  
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    alist.add ("description", "Create a log of all harvests.");
+    syntax.add ("where", Syntax::String, Syntax::Const,
+		"Name of the log file to create.");
+    alist.add ("where", "harvest.dlf");
+    syntax.add ("print_header", Syntax::Boolean, Syntax::Const,
+		"Print header section of the file.");
+    alist.add ("print_header", true);
+    syntax.add ("print_tags", Syntax::Boolean, Syntax::Const,
+		"Print a tag line in the file.");
+    alist.add ("print_tags", true);
+    syntax.add ("print_dimension", Syntax::Boolean, Syntax::Const,
+		"Print a line with units after the tag line.");
+    syntax.add ("print_N", Syntax::Boolean, Syntax::Const,
+		"Print nitrogen content of harvest.");
+    alist.add ("print_N", true);
+    syntax.add ("print_C", Syntax::Boolean, Syntax::Const,
+		"Print carbon content of harvest.");
+    alist.add ("print_C", false);
+    alist.add ("print_dimension", true);
+    alist.add ("flush", true);
+
+    Librarian<Log>::add_type ("harvest", alist, syntax, &make); 
+  }
+} LogHarvest_syntax;
+
