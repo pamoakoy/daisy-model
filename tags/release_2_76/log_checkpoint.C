513af01e8e0b34f65c683fdb4ea9feaa0da39ffe
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-05-05 14:23:35 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_76'.

---
+++ tags/release_2_76/log_checkpoint.C
@@ -0,0 +1,198 @@
+// log_checkpoint.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "log_alist.h"
+#include "condition.h"
+#include "daisy.h"
+#include "printer_file.h"
+#include "tmpstream.h"
+
+struct LogCheckpoint : public LogAList
+{
+  // Content.
+  const string file;		// Name of file to write checkpoint in.
+  const string description;	// Comment to go to the start of the file.
+  Condition& condition;		// Should we print a log now?
+  Time time;			// Time of current checkpoint.
+
+  // Start and end of time step.
+  bool check_member (symbol) const;
+  bool match (const Daisy& daisy, Treelog& out);
+  void done ();
+
+  // Create and Destroy.
+  LogCheckpoint (const AttributeList& al);
+  ~LogCheckpoint ();
+};
+
+bool 
+LogCheckpoint::check_member (symbol) const
+{ return true; }
+
+bool
+LogCheckpoint::match (const Daisy& daisy, Treelog& out)
+{
+  daisy_assert (nested == 0);
+  condition.tick (daisy, out);
+  is_active = condition.match (daisy);
+  if (is_active)
+    {
+      static const symbol daisy_symbol ("daisy");
+      push (daisy_symbol, *daisy.syntax, daisy.alist);
+      time = daisy.time;
+    }
+  return is_active;
+}
+
+void
+LogCheckpoint::done ()
+{
+  if (is_active)
+    {
+      // Check stacks.
+      daisy_assert (syntax_stack.size () == 1U);
+      daisy_assert (alist_stack.size () == 1U);
+      daisy_assert (library_stack.size () == 1U);
+
+      // Create file name.
+      TmpStream scratch;
+      scratch ().fill (0);
+      scratch ().width (2);
+      scratch () << file.c_str () 
+		 << "-" << time.year () << "-" << time.month () << "-" 
+	      << time.mday () << "+" << time.hour () << ".dai";
+      const string filename (scratch.str ());
+
+      // Start checkpoint from next timestep.
+      daisy_assert (alist ().check ("time"));
+      Time time (alist ().alist ("time"));
+      time.tick_hour ();
+      AttributeList new_time;
+      new_time.add ("year", time.year ());
+      new_time.add ("month", time.month ());
+      new_time.add ("mday", time.mday ());
+      new_time.add ("hour", time.hour ());
+      alist ().add ("time", new_time);
+
+      // Open log file.
+      PrinterFile printer (filename);
+      printer.print_comment (description);
+
+      // Print "directory" and "path" before inputs.
+      printer.print_entry (alist (), syntax (), "directory");
+      printer.print_entry (alist (), syntax (), "path");
+      alist ().remove ("directory"); // Avoid printing them twice.
+      alist ().remove ("path"); 
+      
+      // Print input files.
+      if (alist ().check ("parser_inputs"))
+	{
+	  const vector<AttributeList*> inputs 
+	    (alist ().alist_sequence ("parser_inputs"));
+	  printer.print_comment ("Input files.");
+	  for (unsigned int i = 0; i < inputs.size (); i++)
+	    printer.print_input (*inputs[i]);
+	}
+
+      // Print included files.
+      if (alist ().check ("parser_files"))
+	{
+	  const vector<symbol> files (alist ()
+				      .identifier_sequence ("parser_files"));
+	  const string lib_start = "From file '";
+	  const string lib_end = "':";
+	  for (unsigned int i = 0; i < files.size (); i++)
+	    {
+	      const symbol library = files[i];
+	      printer.print_comment (lib_start + library + lib_end);
+	      printer.print_library_file (library.name ());
+	    }
+	}
+
+      // Print cloned objects.
+      printer.print_comment ("Cloned objects:");
+      printer.print_library_file ("*clone*");
+
+      // Print content.
+      printer.print_comment ("Content");
+      static Syntax dummy_syntax;
+      static AttributeList default_alist;
+      if (dummy_syntax.entries () == 0)
+	Daisy::load_syntax (dummy_syntax, default_alist);
+
+      printer.print_alist (alist (), syntax (), default_alist);
+
+      if (!printer.good ())
+	{
+	  TmpStream tmp;
+	  tmp () << "Error writing to '" << filename << "'";
+	  Assertion::error (tmp.str ());
+	}
+      // Close stack.
+      delete &alist ();
+      pop ();
+    }
+  daisy_assert (nested == 0);
+}
+
+LogCheckpoint::LogCheckpoint (const AttributeList& al)
+  : LogAList (al),
+    file (al.name ("where")),
+    description (al.name ("description")),
+    condition (Librarian<Condition>::create (al.alist ("when"))),
+    time (1, 1, 1, 1)
+{ }
+
+LogCheckpoint::~LogCheckpoint ()
+{ delete &condition; }
+
+static struct LogCheckpointSyntax
+{
+  static Log& make (const AttributeList& al)
+    { return *new LogCheckpoint (al); }
+
+  LogCheckpointSyntax ()
+    { 
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description", "\
+Create a checkpoint of the entire simulation state, suitable for later\n\
+hot start.");
+      LogAList::load_syntax (syntax, alist);
+      syntax.add ("where", Syntax::String, Syntax::Const,
+		  "File name prefix for the generated checkpoint.\n\
+The time will be appended, together with the '.dai' suffix.");
+      alist.add ("where", "checkpoint");
+      syntax.add ("description", Syntax::String, Syntax::Const,
+		  "Description of this particular checkpoint.");
+      alist.add ("description", "\
+Create a checkpoint of the entire simulation state, suitable for later\n\
+hot start.");
+      syntax.add ("when", Librarian<Condition>::library (),
+		  "Make a checkpoint every time this condition is true.");
+      AttributeList finished_alist;
+      finished_alist.add ("type", "finished");
+      alist.add ("when", finished_alist);
+      Librarian<Log>::add_type ("checkpoint", alist, syntax, &make);
+    }
+} LogCheckpoint_syntax;
+
---
+++ tags/release_2_76/select.h
@@ -0,0 +1,157 @@
+// select.h --- Select a state variable.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef SELECT_H
+#define SELECT_H
+
+#include "condition.h"		// Needed for proper initialization.
+#include "librarian.h"
+#include "symbol.h"
+#include <map>
+#include <vector>
+
+struct Geometry;
+struct Daisy;
+
+typedef map<string, string, less<string>/**/> string_map;
+
+class Select
+{
+  // Content.
+private:
+  struct Implementation;
+  Implementation& impl;
+protected:
+  const bool accumulate;	// Accumulate numbers over time.
+  double convert (double) const; // Convert value.
+  int count;			// Number of accumulated values.
+public:
+  static const char *const description;
+  virtual const string& dimension () const;
+  virtual const string& tag () const;
+  symbol log_name () const;
+  virtual const Geometry* geometry () const; // For array tags.
+  virtual int size () const;	// For array tags.
+  
+  // Destination
+  class Destination
+  {
+    // Add data.
+  public:
+    virtual void error (const string& tag) = 0;
+    virtual void missing (const string& tag) = 0;
+    virtual void add (const string& tag, const vector<double>& value) = 0;
+    virtual void add (const string& tag, double value) = 0;
+    virtual void add (const string& tag, const string& value) = 0;
+
+    // Create and Destroy
+  public:
+    Destination ();
+    virtual ~Destination ();
+  };
+
+  // Nesting.
+private:
+  vector<symbol> path;		// Content of this entry.
+  const unsigned int path_size;	// #entries in path.
+  unsigned int current_path_index;// How nested in open's we are.
+  unsigned int last_valid_path_index;	// Remember the last valid level.
+  symbol current_name;
+  
+  bool valid () const
+  { 
+    // If the current path index is valid.
+    return (current_path_index == last_valid_path_index
+	    && current_path_index < path_size); 
+  }
+public:
+  bool valid (symbol name) const
+  {
+    static const symbol wildcard ("*");
+    // Is the next path index valid?
+    return current_name == wildcard || name == current_name; 
+  }
+  bool valid_level;
+
+  void open (symbol name)	// Open one leaf level.
+  { 
+    if (!valid_level)
+      current_path_index++;
+    else
+      {
+	if (valid (name))
+	  last_valid_path_index++;
+	current_path_index++;
+	valid_level = is_active && valid ();
+	if (valid_level)
+	  current_name = path[current_path_index];
+      }
+  }
+  void close ()		// Close one level.
+  { 
+    if (current_path_index == last_valid_path_index)
+      last_valid_path_index--;
+    current_path_index--;
+    if (!valid_level)
+      valid_level = is_active && valid ();
+    if (valid_level)
+      current_name = path[current_path_index];
+  }
+
+  // Output routines.
+  virtual void output_number (const double);
+  virtual void output_integer (const int);
+  virtual void output_name (const string&);
+  virtual void output_array (const vector<double>&, const Geometry*);
+
+  // Reset at start of time step.
+private:
+  bool is_active;		// Should we be accumulating now?
+public:
+  bool match (const Daisy& daisy, Treelog&, bool is_printing);
+
+  // Print result at end of time step.
+  virtual void done (Destination& dest) = 0;
+  virtual bool prevent_printing ();
+
+  // Create and Destroy.
+protected:
+  virtual const string default_dimension (const string& spec_dim) const;
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  virtual void initialize (const string_map& conv, double from, double to,
+			   const string& timestep);
+  virtual bool check (Treelog& err) const;
+protected:
+  Select (const AttributeList& al);
+public:
+  virtual ~Select ();
+};
+
+#if !defined (__BORLANDC__)
+EMPTY_TEMPLATE
+Librarian<Select>::Content* Librarian<Select>::content;
+#endif
+
+static Librarian<Select> Select_init ("select");
+
+#endif // SELECT_H
