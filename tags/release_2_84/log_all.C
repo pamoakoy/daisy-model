83f112f80fe155d12de537ebf9aff72e02731390
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-05-12 14:41:20 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_84'.

---
+++ tags/release_2_84/log_all.C
@@ -0,0 +1,272 @@
+// log_all.C
+// 
+// Copyright 2003 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "log_all.h"
+#include "assertion.h"
+
+bool 
+LogAll::check_leaf (symbol name) const
+{ 
+  daisy_assert (is_active);
+  daisy_assert (!active_leafs.empty ());
+
+  const vector<Select*>& current = active_leafs.top ();
+  for (vector<Select*>::const_iterator i = current.begin (); 
+       i < current.end (); 
+       i++)
+    if (name == (*i)->current_name)
+      return true;
+
+  return false;
+}
+
+bool 
+LogAll::check_interior (symbol name) const
+{ 
+  daisy_assert (is_active);
+  daisy_assert (!active_interiors.empty ());
+
+  const vector<Select*>& current = active_interiors.top ();
+  for (vector<Select*>::const_iterator i = current.begin (); 
+       i < current.end (); 
+       i++)
+    if ((*i)->valid (name))
+      return true;
+
+  return false;
+}
+
+void
+LogAll::insert_active ()
+{
+  daisy_assert (active_leafs.empty ());
+  active_leafs.push (vector<Select*> ());
+  vector<Select*>& leafs = active_leafs.top ();
+  daisy_assert (leafs.size () == 0);
+  daisy_assert (active_interiors.empty ());
+  active_interiors.push (vector<Select*> ());
+  vector<Select*>& interiors = active_interiors.top ();
+  daisy_assert (interiors.size () == 0);
+
+  const int depth = 0;
+  for (unsigned int i = 0; i < entries.size (); i++)
+    {
+      Select* sel = entries[i];
+
+      if (sel->is_active)
+	if (depth == sel->last_index)
+	  leafs.push_back (sel);
+	else
+	  interiors.push_back (sel);
+    }
+}
+
+
+bool 
+LogAll::match (const Daisy& daisy, Treelog& msg)
+{
+  is_active = false;
+  for (unsigned int i = 0; i < slaves.size (); i++)
+    if (slaves[i]->match (daisy, msg))
+      is_active = true;
+
+  if (is_active)
+    insert_active ();
+
+  return is_active;
+}
+
+void 
+LogAll::done (const Time& time)
+{
+  for (unsigned int i = 0; i < slaves.size (); i++)
+    if (slaves[i]->is_active)
+      slaves[i]->done (time);
+
+  active_leafs.pop ();
+  active_interiors.pop ();
+}
+
+bool 
+LogAll::initial_match (const Daisy& daisy, Treelog& msg)
+{
+  is_active = false;
+  for (unsigned int i = 0; i < slaves.size (); i++)
+    if (slaves[i]->initial_match (daisy, msg))
+      is_active = true;
+
+  if (is_active)
+    insert_active ();
+
+  return is_active;
+}
+
+void 
+LogAll::initial_done (const Time& time)
+{
+  for (unsigned int i = 0; i < slaves.size (); i++)
+    if (slaves[i]->is_active)
+      slaves[i]->initial_done (time);
+
+  active_leafs.pop ();
+  active_interiors.pop ();
+}
+
+void 
+LogAll::open (symbol name)
+{ 
+  daisy_assert (is_active);
+
+  const int depth = active_interiors.size ();
+  daisy_assert (depth > 0);
+  daisy_assert (depth == active_leafs.size ());
+
+  const vector<Select*>& old = active_interiors.top ();
+  active_interiors.push (vector<Select*> ());
+  vector<Select*>& interiors = active_interiors.top ();
+  active_leafs.push (vector<Select*> ());
+  vector<Select*>& leafs = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = old.begin (); 
+       i < old.end (); 
+       i++)
+    if ((*i)->open (name, depth))
+      if ((*i)->last_index == depth)
+	leafs.push_back (*i);
+      else
+	interiors.push_back (*i);
+}
+
+void 
+LogAll::close ()
+{ 
+  const int depth = active_interiors.size () - 2;
+  daisy_assert (depth >= 0);
+
+  const vector<Select*>& leafs = active_leafs.top ();
+  for (vector<Select*>::const_iterator i = leafs.begin (); 
+       i < leafs.end (); 
+       i++)
+    (*i)->close (depth);
+  const vector<Select*>& interiors = active_interiors.top ();
+  for (vector<Select*>::const_iterator i = interiors.begin (); 
+       i < interiors.end (); 
+       i++)
+    (*i)->close (depth);
+
+  active_leafs.pop ();
+  active_interiors.pop ();
+}
+
+void 
+LogAll::output (symbol, const bool)
+{ }
+
+void 
+LogAll::output (symbol name, const double value)
+{ 
+  const vector<Select*>& sels = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = sels.begin (); i < sels.end (); i++)
+    if (name == (*i)->current_name)
+      (*i)->output_number (value);
+}
+
+void 
+LogAll::output (symbol name, const int value)
+{ 
+  const vector<Select*>& sels = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = sels.begin (); i < sels.end (); i++)
+    if (name == (*i)->current_name)
+      (*i)->output_integer (value);
+}
+
+void 
+LogAll::output (symbol name, const string& value)
+{ 
+  const vector<Select*>& sels = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = sels.begin (); i < sels.end (); i++)
+    if (name == (*i)->current_name)
+      (*i)->output_name (value);
+}
+
+void 
+LogAll::output (symbol name, const vector<double>& value)
+{ 
+  const vector<Select*>& sels = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = sels.begin (); i < sels.end (); i++)
+    if (name == (*i)->current_name)
+      (*i)->output_array (value, geometry ());
+}
+
+void 
+LogAll::output (symbol, const PLF&)
+{ }
+
+void 
+LogAll::output (symbol name, const Time& value)
+{ 
+  const vector<Select*>& sels = active_leafs.top ();
+
+  for (vector<Select*>::const_iterator i = sels.begin (); i < sels.end (); i++)
+    if (name == (*i)->current_name)
+      (*i)->output_time (value);
+}
+
+const AttributeList& 
+LogAll::get_alist ()
+{
+  static AttributeList alist;
+  if (!alist.check ("type"))
+    {
+      Syntax syntax;
+      LogSelect::load_syntax (syntax, alist);
+      alist.add ("type", "all");
+      AttributeList when;
+      when.add ("type", "true");
+      alist.add ("when", when);
+      alist.add ("entries", vector<AttributeList*> ());
+      daisy_assert (syntax.check (alist, Treelog::null ()));
+    }
+  return alist;
+}
+
+LogAll::LogAll (const vector<Log*>& logs)
+  : LogSelect (get_alist ())
+{
+  // Combine entries.
+  for (unsigned int i = 0; i < logs.size (); i++)
+    if (LogSelect* log = dynamic_cast<LogSelect*> (logs[i]))
+      {
+	slaves.push_back (log);
+	for (unsigned int j = 0; j < log->entries.size (); j++)
+	  entries.push_back (log->entries[j]);
+      }
+}
+
+LogAll::~LogAll ()
+{ 
+  // Don't delete entries twice.
+  entries.erase (entries.begin (), entries.end ());
+}
---
+++ tags/release_2_84/pet.C
@@ -0,0 +1,80 @@
+// pet.C  -- Potential evopotranspiration
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "pet.h"
+#include "log.h"
+#include "vegetation.h"
+#include "surface.h"
+
+EMPTY_TEMPLATE
+Librarian<Pet>::Content* Librarian<Pet>::content = NULL;
+
+const char *const Pet::description = "\
+The 'pet' component should calculate the potential evapotranspiration\n\
+from meteorological data, as well as the crop and soil state.";
+
+double
+Pet::reference_to_potential (const Vegetation& crops, 
+			     const Surface& surface,
+			     double ref)
+{
+  const double LAI = crops.LAI ();
+  double EpFactor;
+  if (LAI < 0.01)
+    EpFactor = surface.EpFactor ();
+  else if (LAI > 0.99)
+    EpFactor = crops.EpFactor ();
+  else
+    EpFactor = LAI * crops.EpFactor () + (1.0 - LAI) * surface.EpFactor ();
+
+  return EpFactor * max (0.0, ref);
+}
+
+double
+Pet::dry () const 
+{ return wet (); }
+
+void
+Pet::output (Log& log) const
+{
+  output_value (wet (), "wet", log);
+  output_value (dry (), "dry", log);
+}
+
+void 
+Pet::load_syntax (Syntax& syntax, AttributeList&)
+{
+  syntax.add ("wet", "mm/h", Syntax::LogOnly, 
+	      "Potential evapotranspiration for a wet system.");
+  syntax.add ("dry", "mm/h", Syntax::LogOnly, 
+	      "Potential evapotranspiration for a dry system.");
+  syntax.add ("reference_evapotranspiration", "mm/h", Syntax::LogOnly, 
+	      "Reference evapotranspiration for a dry system.");
+}
+
+Pet::Pet (const AttributeList& al)
+  : name (al.identifier ("type"))
+{ }
+
+Pet::~Pet ()
+{ }
+
