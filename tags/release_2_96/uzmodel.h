8d78d24718092f8f09080744db59c1aebe8703de
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-06-01 09:26:46 +0000

    This commit was manufactured by cvs2svn to create tag 'release_2_96'.

---
+++ tags/release_2_96/uzmodel.h
@@ -0,0 +1,109 @@
+// uzmodel.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef UZMODEL_H
+#define UZMODEL_H
+
+#include "librarian.h"
+
+class Soil;
+class SoilHeat;
+class Library;
+class Syntax;
+class AttributeList;
+class Log;
+
+class UZtop
+{
+public:
+  virtual bool flux_top () const = 0;
+  virtual double q () const = 0;
+  double h () const
+  { return -q () * dt; }
+  virtual void flux_top_on () const = 0;
+  virtual void flux_top_off () const = 0;
+  virtual bool accept_top (Treelog&, double) = 0;
+  virtual bool soil_top () const;
+  virtual ~UZtop ();
+};
+
+class UZbottom
+{
+public:
+  enum type_t { pressure, lysimeter, forced_flux, free_drainage };
+  virtual type_t type () const = 0;
+  virtual double q_bottom () const;
+  virtual bool accept_bottom (double) = 0;
+  virtual ~UZbottom ();
+};
+
+class UZmodel : public UZtop, public UZbottom
+{
+  // Content.
+public: 
+  const symbol name;
+  static const char *const description;
+
+  // UZtop.
+public:
+  bool flux_top () const = 0;
+  double q () const = 0;
+  void flux_top_on () const = 0;
+  void flux_top_off () const = 0;
+  bool accept_top (Treelog&, double) = 0;
+
+  // UZbottom.
+public:
+  type_t type () const = 0;
+  bool accept_bottom (double) = 0;
+  
+  // Simulate.
+public:
+  virtual bool tick (Treelog&, const Soil& soil, const SoilHeat&,
+		     unsigned int first, const UZtop& top, 
+		     unsigned int last, const UZbottom& bottom, 
+		     const vector<double>& S,
+		     const vector<double>& h_old,
+		     const vector<double>& Theta_old,
+		     const vector<double>& h_ice,
+		     vector<double>& h,
+		     vector<double>& Theta,
+		     vector<double>& q) = 0;
+  virtual void output (Log&) const = 0;
+
+  // Create and Destroy.
+public:
+  virtual void has_macropores (bool); // Tell UZ that there is macropores.
+protected:
+  UZmodel (const AttributeList&);
+public:
+  virtual ~UZmodel ();
+};
+
+#if !defined (__BORLANDC__)
+EMPTY_TEMPLATE
+Librarian<UZmodel>::Content* Librarian<UZmodel>::content;
+#endif
+
+static Librarian<UZmodel> UZmodel_init ("uzmodel");
+
+#endif // UZMODEL_H
---
+++ tags/release_2_96/uznone.C
@@ -0,0 +1,94 @@
+// uznone.C --- no water flow.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "uzmodel.h"
+#include "soil.h"
+#include "log.h"
+#include "mathlib.h"
+
+class UZNone : public UZmodel
+{
+  // UZmodel.
+public:
+  bool flux_top () const
+    { return true; }
+  double q () const
+    { return 0.0; }
+  void flux_top_on () const
+    { }
+  void flux_top_off () const
+    { }
+  bool accept_top (Treelog&, double)
+    { return true; }
+  type_t type () const
+    { return free_drainage; }
+  bool accept_bottom (double)
+    { return true; }
+  void output (Log&) const
+    { }
+
+public:
+  bool tick (Treelog&, const Soil& /* soil */, const SoilHeat&,
+	     unsigned int first, const UZtop& /* top */, 
+	     unsigned int last, const UZbottom& /* bottom */, 
+	     const vector<double>& /* S */,
+	     const vector<double>& h_old,
+	     const vector<double>& Theta_old,
+	     const vector<double>& /* h_ice */,
+	     vector<double>& h,
+	     vector<double>& Theta,
+	     vector<double>& q)
+    {
+      for (int i = first; i <= last; i++)
+	{
+	  q[i] = 0.0;
+	  Theta[i] = Theta_old[i];
+	  h[i] = h_old[i];
+	}
+      q[last + 1] = 0.0;
+      return true;
+    }
+  // Create and Destroy.
+public:
+  UZNone (const AttributeList& al)
+    : UZmodel (al)
+    { }
+  ~UZNone ()
+    { }
+};
+
+// Add the UZNone syntax to the syntax table.
+static struct UZNoneSyntax
+{
+  static UZmodel& make (const AttributeList& al)
+    {
+      return *new UZNone (al);
+    }
+
+  UZNoneSyntax ()
+    {
+      Syntax& syntax = *new Syntax ();
+      AttributeList& alist = *new AttributeList ();
+      alist.add ("description", "No water movement, and no sink.");
+      Librarian<UZmodel>::add_type ("none", alist, syntax, &make);
+    }
+} UZNone_syntax;
