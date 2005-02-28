e6093dae6f109e62264937a725ad935dd8f168a0
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-02-28 13:12:33 +0000

    This commit was manufactured by cvs2svn to create tag 'release_3_70'.

---
+++ tags/release_3_70/action_irrigate.C
@@ -0,0 +1,374 @@
+// action_irrigate.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "action.h"
+#include "daisy.h"
+#include "field.h"
+#include "im.h"
+#include "check.h"
+#include "tmpstream.h"
+
+struct ActionIrrigate : public Action
+{
+  const int days;
+  const int hours;
+  bool activated;
+  Time end_time;
+
+  static const double at_air_temperature;
+
+  const double flux;
+  const double temp;
+  const IM sm;
+  
+  virtual void irrigate (Field&,
+			 double flux, double temp, const IM&) const = 0;
+
+  void doIt (Daisy& daisy, Treelog& out)
+  {
+    if (!activated)
+      {
+	activated = true;
+	end_time = daisy.time;
+	end_time.tick_hour (hours - 1);
+	end_time.tick_day (days);
+        TmpStream tmp;
+        if (daisy.time == end_time)
+          tmp () << "Irrigating " << flux << " mm";
+        else
+          tmp () << "Irrigating " << flux << " mm/h for "
+                 << days * 24 + hours << " hours";
+        // [kg/ha] -> [g/cm^2]
+        const double conv = (1000.0 / ((100.0 * 100.0) * (100.0 * 100.0)));
+        // [mm * mg N/ l] = [l/m^2 * mg N/l] = [mg/m^2] -> [g N/cm^2]
+        static const double irrigate_solute_factor = 1.0e-7;
+        double N = (sm.NO3 + sm.NH4) * flux * (days * 24 + hours) 
+          * irrigate_solute_factor / conv;
+        if (N > 1e-10)
+          tmp () << "; " << N << " kg N/ha";
+        out.message (tmp.str ());      
+      }
+    else if (daisy.time == end_time)
+      out.message ("Irrigating done");
+
+    irrigate (daisy.field, flux, temp, sm);
+  }
+
+  bool done (const Daisy& daisy) const
+  {
+    daisy_assert (activated);
+    return daisy.time >= end_time; 
+  }
+
+  static bool check_alist (const AttributeList& alist, Treelog& err)
+  {
+    bool ok = true;
+
+    const int days = alist.integer ("days");
+    const int hours = alist.integer ("hours", (days > 0) ? 0 : 1);
+
+    if (days * 24 + hours < 1)
+      {
+	err.entry ("you must irrigate at least 1 hour");
+	ok = false;
+      }
+    return ok;
+  }
+
+  static void load_syntax (Syntax& syntax, AttributeList& alist)
+  {
+    syntax.add_check (check_alist);	
+    syntax.add ("days", Syntax::Integer, Syntax::Const, 
+                "Irrigate this number of days.");
+    alist.add ("days", 0);
+    syntax.add ("hours", Syntax::Integer, Syntax::OptionalConst, 
+                "Irrigate this number of hours.\n\
+By default, irrigate 1 hour if days is 0, and 0 hours plus the specified\n\
+number of days else.");
+    syntax.add_submodule ("end_time", alist, Syntax::OptionalState,
+                          "Irrigate until this date.\
+Setting this overrides the 'days' and 'hours' parameters.", Time::load_syntax);
+    syntax.add ("flux", "mm/h", Check::non_negative (), Syntax::Const, 
+		"Amount of irrigation applied.");
+    syntax.order ("flux");
+    syntax.add ("temperature", "dg C", 
+		Check::positive (), Syntax::OptionalConst,
+		"Temperature of irrigation (default: air temperature).");
+    syntax.add_submodule ("solute", alist, Syntax::Const, "\
+Nitrogen content of irrigation water [mg N/l] (default: none).",
+			  IM::load_ppm);
+  }
+
+  ActionIrrigate (const AttributeList& al)
+    : Action (al),
+      days (al.integer ("days")),
+      hours (al.integer ("hours", (days > 0) ? 0 : 1)),
+      activated (al.check ("end_time")),
+      end_time (1, 1, 1, 1),
+      flux (al.number ("flux")),
+      temp (al.number ("temperature", at_air_temperature)),
+      sm (al.alist ("solute"))
+  { }
+  ~ActionIrrigate ()
+  { }
+};
+
+const double ActionIrrigate::at_air_temperature = -500;
+
+struct ActionIrrigateOverhead : public ActionIrrigate
+{
+  void irrigate (Field& f, double flux, double temp, const IM& im) const
+  { 
+    if (temp == at_air_temperature)
+      f.irrigate_overhead (flux, im); 
+    else
+      f.irrigate_overhead (flux, temp, im); 
+  }
+  ActionIrrigateOverhead (const AttributeList& al)
+    : ActionIrrigate (al)
+  { }
+};
+
+struct ActionIrrigateSurface : public ActionIrrigate
+{
+  void irrigate (Field& f, double flux, double temp, const IM& im) const
+  {
+    if (temp == at_air_temperature)
+      f.irrigate_surface (flux, im);
+    else
+      f.irrigate_surface (flux, temp, im); 
+  }
+  ActionIrrigateSurface (const AttributeList& al)
+    : ActionIrrigate (al)
+  { }
+};
+
+struct ActionIrrigateSubsoil : public ActionIrrigate
+{
+  const double from;
+  const double to;
+
+  void irrigate (Field& f, double flux, double /* temp */, const IM& im) const
+  { f.irrigate_subsoil (flux, im, from, to); }
+  ActionIrrigateSubsoil (const AttributeList& al)
+    : ActionIrrigate (al),
+      from (al.number ("from")),
+      to (al.number ("to"))
+  { }
+};
+
+struct ActionIrrigateSubsoilStart : public Action
+{
+  const double flux;
+  const double from;
+  const double to;
+  const IM sm;
+
+  void doIt (Daisy& daisy, Treelog& out)
+  {
+    daisy.field.set_subsoil_irrigation (flux, sm, from, to);
+    TmpStream tmp;
+    if (flux != 0.0)
+      tmp () << "Subsoil irrigating with " << flux << " mm/h";
+    else
+      tmp () << "Subsoil irrigating turned off";
+    out.message (tmp.str ());
+  }
+
+  ActionIrrigateSubsoilStart (const AttributeList& al)
+    : Action (al),
+      flux (al.number ("flux")),
+      from (al.number ("from")),
+      to (al.number ("to")),
+      sm (al.alist ("solute"))
+  { }
+  ~ActionIrrigateSubsoilStart ()
+  { }
+};
+
+struct ActionIrrigateStop : public Action
+{
+  void doIt (Daisy& daisy, Treelog& out)
+  {
+    IM sm;
+    daisy.field.set_subsoil_irrigation (0.0, sm, 0.0, -0.1);
+    out.message ("Subsoil irrigating turned off");
+  }
+  ActionIrrigateStop (const AttributeList& al)
+    : Action (al)
+  { }
+  ~ActionIrrigateStop ()
+  { }
+};
+
+static struct ActionIrrigateOverheadSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateOverhead (al); }
+  ActionIrrigateOverheadSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    ActionIrrigate::load_syntax (syntax, alist);
+    alist.add ("description", "\
+Irrigate the field from above.");
+    Librarian<Action>::add_type ("irrigate_overhead", alist, syntax, &make);
+  }
+} ActionIrrigateOverhead_syntax;
+
+static struct ActionIrrigateSurfaceSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateSurface (al); }
+  ActionIrrigateSurfaceSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    ActionIrrigate::load_syntax (syntax, alist);
+    alist.add ("description", "\
+Irrigate the field directly on the soil surface, bypassing the canopy.");
+    Librarian<Action>::add_type ("irrigate_surface", alist, syntax, &make);
+  }
+} ActionIrrigateSurface_syntax;
+
+static struct ActionIrrigateTopSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateOverhead (al); }
+  static bool check_alist (const AttributeList&, Treelog& err)
+  {
+    static bool warned = false;
+    if (warned)
+      return true;
+    warned = true;
+    err.entry ("OBSOLETE: Use 'irrigate_overhead' instead of 'irrigate_top'");
+    return true;
+  }
+  ActionIrrigateTopSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    ActionIrrigate::load_syntax (syntax, alist);
+    syntax.add_check (&check_alist);
+    alist.add ("description", "\
+OBSOLETE.  Use 'irrigate_overhead' instead.");
+    Librarian<Action>::add_type ("irrigate_top", alist, syntax, &make);
+  }
+} ActionIrrigateTop_syntax;
+
+static struct ActionIrrigateSubsoilSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateSubsoil (al); }
+
+  static bool check_alist (const AttributeList& al, Treelog& err)
+  { 
+    bool ok = true;
+    const double from = al.number ("from");
+    const double to = al.number ("to");
+    if (from <= to)
+      {
+	err.entry ("'from' must be higher than 'to' in"
+		   " the subsoil irrigation zone");
+	ok = false;
+      }
+    return ok;
+  }
+
+  ActionIrrigateSubsoilSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    ActionIrrigate::load_syntax (syntax, alist);
+    alist.add ("description", "\
+Irrigate the field directly into the soil.\n\
+Currently, the 'temperature' parameter is ignored.");
+    syntax.add ("from", "cm", Check::non_positive (), Syntax::Const, "\
+Height where you want to start the incorporation (a negative number).");
+    alist.add ("from", 0.0);
+    syntax.add ("to", "cm", Check::non_positive (), Syntax::Const, "\
+Height where you want to end the incorporation (a negative number).");
+
+    Librarian<Action>::add_type ("irrigate_subsoil", alist, syntax, &make);
+  }
+
+} ActionIrrigateSubsoil_syntax;
+
+static struct ActionIrrigateSubsoilStartSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateSubsoilStart (al); }
+
+  static bool check_alist (const AttributeList& al, Treelog& err)
+  { 
+    bool ok = true;
+    const double from = al.number ("from");
+    const double to = al.number ("to");
+    if (from <= to)
+      {
+	err.entry ("'from' must be higher than 'to' in"
+		   " the subsoilirrigation zone");
+	ok = false;
+      }
+    return ok;
+  }
+
+  ActionIrrigateSubsoilStartSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    syntax.add_check (&check_alist);
+    alist.add ("description", "\
+Incorporate irrigation water directly in the soil.\n\
+This command specifies the flux, set it to zero to turn off irrigation.");
+    syntax.add ("flux", "mm/h", Check::non_negative (), Syntax::Const, 
+		"Amount of irrigation applied.");
+    syntax.order ("flux");
+    syntax.add ("from", "cm", Check::non_positive (), Syntax::Const, "\
+Height where you want to start the incorporation (a negative number).");
+    alist.add ("from", 0.0);
+    syntax.add ("to", "cm", Check::non_positive (), Syntax::Const, "\
+Height where you want to end the incorporation (a negative number).");
+
+    syntax.add_submodule ("solute", alist, Syntax::Const, "\
+Nitrogen content of irrigation water [mg N/l] (default: none).",
+			  IM::load_ppm);
+
+    Librarian<Action>::add_type ("set_subsoil_irrigation",
+				 alist, syntax, &make);
+  }
+} ActionIrrigateSubsoilStart_syntax;
+
+static struct ActionIrrigateStopSyntax
+{
+  static Action& make (const AttributeList& al)
+  { return *new ActionIrrigateStop (al); }
+
+  ActionIrrigateStopSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    alist.add ("description", "Stop subsoil irrigation.");
+    Librarian<Action>::add_type ("stop_subsoil_irrigation",
+				 alist, syntax, &make);
+  }
+} ActionIrrigateStop_syntax;
---
+++ tags/release_3_70/log_select.C
@@ -0,0 +1,322 @@
+// log_select.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "log_select.h"
+#include "tmpstream.h"
+#include "format.h"
+
+using namespace std;
+
+bool 
+LogSelect::check_leaf (symbol) const
+{ daisy_assert (false); }
+
+bool 
+LogSelect::check_interior (symbol) const
+{ daisy_assert (false); }
+
+bool 
+LogSelect::check_derived (symbol field, symbol /* name */,
+			  const Library& /* library */) const
+{ return check_interior (field); }
+
+bool 
+LogSelect::match (const Daisy& daisy, Treelog& out)
+{
+  condition->tick (daisy, out);
+  is_printing = condition->match (daisy);
+  is_active = is_printing;
+
+  for (vector<Select*>::const_iterator i = entries.begin (); 
+       i < entries.end (); 
+       i++)
+    if ((*i)->match (is_printing))
+      is_active = true;
+
+  return is_active;
+}
+
+void
+LogSelect::done (const Time&)
+{ }
+
+bool
+LogSelect::initial_match (const Daisy&, Treelog&)
+{
+  is_active = false;
+
+  for (vector<Select*>::const_iterator i = entries.begin (); 
+       i < entries.end (); 
+       i++)
+    if ((*i)->initial_match ())
+      is_active = true;
+
+  is_printing = is_active;
+  return is_active;
+}
+
+void
+LogSelect::initial_done (const Time&)
+{ }
+
+void 
+LogSelect::open (symbol)
+{ daisy_assert (false); }
+
+void 
+LogSelect::close ()
+{ daisy_assert (false); }
+
+void 
+LogSelect::open_unnamed ()
+{ }
+
+void 
+LogSelect::close_unnamed ()
+{ }
+
+void 
+LogSelect::open_named (const symbol name)
+{ open (name); }
+
+void 
+LogSelect::close_named ()
+{ close (); }
+
+void 
+LogSelect::open_ordered (int index)
+{ 
+  open (symbol (index));
+}
+
+void 
+LogSelect::close_ordered ()
+{ close (); }
+
+void 
+LogSelect::open_derived (symbol field, symbol type)
+{ open (field); open (type); }
+
+void 
+LogSelect::close_derived ()
+{ close (); close (); }
+
+void 
+LogSelect::open_object (symbol field, symbol type, const AttributeList&)
+{ open_derived (field, type); }
+
+void 
+LogSelect::close_object ()
+{ close_derived (); }
+
+void 
+LogSelect::open_entry (symbol type, const AttributeList&)
+{ open (type); }
+
+void 
+LogSelect::close_entry ()
+{ close (); }
+
+void 
+LogSelect::open_named_entry (symbol name, symbol, 
+			     const AttributeList&)
+{ open (name); }
+
+void 
+LogSelect::close_named_entry ()
+{ close (); }
+
+void 
+LogSelect::output (symbol, const bool)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const double)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const int)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const symbol)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const vector<double>&)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const PLF&)
+{ daisy_assert (false); }
+
+void 
+LogSelect::output (symbol, const Time&)
+{ daisy_assert (false); }
+
+bool 
+LogSelect::check (Treelog& err) const
+{
+  bool ok = true;
+  for (unsigned int i = 0; i < entries.size (); i++)
+    {
+      TmpStream tmp;
+      tmp () << "entries [" << i << "]: " << entries[i]->tag ();
+      Treelog::Open nest (err, tmp.str ());
+      if (!entries[i]->check (err))
+        ok = false;
+    }
+  return ok; 
+}
+
+LogSelect::LogSelect (const AttributeList& al)
+  : Log (al),
+    description (al.name ("description")),
+    condition (Librarian<Condition>::create (al.alist ("when"))),
+    entries (map_create<Select> (al.alist_sequence ("entries"))),
+    conv_vector (al.identifier_sequence ("set")),
+    from (al.number ("from")),
+    to (al.number ("to"))
+{
+  // Create path convertion map.
+  map<symbol, symbol> conv_map;
+  for (unsigned int i = 0; i < conv_vector.size (); i += 2)
+    {
+      daisy_assert (i+1 < conv_vector.size ());
+      conv_map[conv_vector[i]] = conv_vector[i+1];
+    }
+
+  // Initialize entries.
+  for (unsigned int i = 0; i < entries.size (); i++)
+    entries[i]->initialize (conv_map, from, to, condition->timestep ());
+}
+
+  
+LogSelect::~LogSelect ()
+{
+  sequence_delete (entries.begin (), entries.end ());
+}
+
+static bool check_alist (const AttributeList& al, Treelog& err)
+{
+  bool ok = true;
+
+  if ((al.size ("set") % 2) == 1)
+    {
+      err.entry ("'set' should contain an even number of arguments");
+      ok = false;
+    }
+  return ok;
+}
+
+static bool 
+has_interesting_description (const Library& library, 
+                             const AttributeList& alist)
+{
+  // A missing description is boring.
+  if (!alist.check ("description"))
+    return false;
+  
+  // The description of models are always interesting.
+  if (!alist.check ("type"))
+    return true;
+  
+  // If the model has no description, this one is interesting.
+  const symbol type = alist.identifier ("type");
+  if (!library.check (type))
+    {
+      daisy_bug (library.name () + " does not have " + type.name ());
+      return false;
+    }
+  daisy_assert (library.check (type));
+  const AttributeList& super = library.lookup (type);
+  if (!super.check ("description"))
+    return true;
+  
+  // If the model description is different, this one is interesting.
+  return alist.name ("description") != super.name ("description");
+}
+
+void
+LogSelect::document_entries (Format& format, const AttributeList& alist)
+{
+  if (!alist.check ("entries"))
+    return;
+
+  const vector<AttributeList*>& entries = alist.alist_sequence ("entries");
+  if (entries.size () < 1)
+    return;
+
+  // At least one interesting description required.
+  const Library& library = Librarian<Select>::library ();
+  int interesting = 0;
+  for (size_t i = 0; i < entries.size (); i++)
+    if (has_interesting_description (library, *entries[i]))
+      interesting++;
+  if (interesting < 1)
+    return;
+
+  format.bold ("Table columns include:");
+  Format::List dummy (format);
+
+  for (size_t i = 0; i < entries.size (); i++)
+    {
+      const AttributeList& entry = *entries[i];
+      Format::Item d2 (format, Select::select_get_tag (entry).name ());
+      if (has_interesting_description (library, entry))
+        format.text (entry.name ("description"));
+      format.soft_linebreak ();
+    }
+}
+
+void 
+LogSelect::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  syntax.add_check (check_alist);
+  syntax.add ("description", Syntax::String, Syntax::Const,
+	      "Description of this log file format.");
+  alist.add ("description", "\
+Each selected variable is represented by a column in the log.");
+  syntax.add ("when", Librarian<Condition>::library (), 
+	      "Add entries to the log file when this condition is true.");
+  syntax.add ("entries", Librarian<Select>::library (), 
+	      Syntax::State, Syntax::Sequence,
+	      "What to log in each column.");
+  syntax.add ("time_columns", Syntax::Boolean, Syntax::OptionalConst, "\
+Iff true, add columns for year, month, mday and hour in the begining of\n\
+the lines.  By default, this will be true of you have not specified any\n\
+time entries yourself.");
+  syntax.add ("set", Syntax::String, Syntax::Const, Syntax::Sequence, 
+	      "Map path names in the entries.\n\
+The first entry in the sequence is a symbol from the paths (e.g. $crop),\n\
+and the second is the value to replace the symbol with (e.g. Grass).\n\
+The third entry is another symbol to replace, and the fourth is another\n\
+value to replace it with.  And so forth.");
+  const vector<symbol> empty_symbol_vector;
+  alist.add ("set", empty_symbol_vector);
+  syntax.add ("from", "cm", Syntax::Const,
+	      "Default 'from' value for all entries.");
+  alist.add ("from", 0.0);
+  syntax.add ("to", "cm", Syntax::Const,
+	      "Default 'to' value for all entries.");
+  alist.add ("to", 1.0);
+}
