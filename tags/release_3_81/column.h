62c2d9b2821af6a4d3dde888c4d704f34116e113
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-09-22 11:19:15 +0000

    This commit was manufactured by cvs2svn to create tag 'release_3_81'.

---
+++ tags/release_3_81/column.h
@@ -0,0 +1,145 @@
+// column.h
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef COLUMN_H
+#define COLUMN_H
+
+#include "librarian.h"
+
+class Time;
+class Weather;
+class OrganicMatter;
+class IM;
+class Crop;
+class Harvest;
+class Treelog;
+
+class Column
+{
+public:
+  const AttributeList alist;	// Remember attributes for checkpoint.
+  const symbol name;
+  double size;
+  static const char *const description;
+
+  // Actions.
+public:
+  virtual void sow (Treelog&, const AttributeList& crop) = 0;
+  virtual void ridge (const AttributeList& ridge) = 0;
+  virtual void irrigate_overhead (double flux, double temp, const IM&) = 0;
+  virtual void irrigate_surface (double flux, double temp, const IM&) = 0;
+  virtual void irrigate_overhead (double flux, const IM&) = 0;
+  virtual void irrigate_surface (double flux, const IM&) = 0;
+  virtual void irrigate_subsoil (double flux, const IM&, double from, double to) = 0;
+  virtual void fertilize (const AttributeList&, double from, double to) = 0;
+  virtual void fertilize (const AttributeList&) = 0;
+  virtual void clear_second_year_utilization () = 0;
+  virtual void harvest (const Time&, symbol name,
+			double stub_length, 
+			double stem_harvest, 
+			double leaf_harvest, 
+			double sorg_harvest, 
+                        const bool combine,
+			std::vector<const Harvest*>& harvest, Treelog&) = 0;
+  virtual void mix (Treelog&, const Time&,
+		    double from, double to, double penetration = 1.0) = 0;
+  virtual void swap (Treelog&, 
+		     const Time&, double from, double middle, double to) = 0;
+  virtual void set_porosity (double at, double Theta) = 0;
+  virtual void set_heat_source (double at, double value) = 0; // [W/m^2]
+  virtual void spray (symbol chemical, double amount) = 0; // [g/ha]
+  virtual void set_surface_detention_capacity (double height) = 0; // [mm]
+
+  // Conditions.
+public:
+  virtual double daily_air_temperature () const = 0; // [dg C]
+  virtual double daily_precipitation () const = 0; // [mm]
+  virtual double daily_global_radiation () const = 0; // [W/m^2]
+  virtual double soil_temperature (double height) const = 0; // [cm -> dg C]
+  virtual double soil_water_potential (double height) const = 0; // [cm -> cm]
+  virtual double soil_water_content (double from, double to) const = 0; // [cm]
+  virtual double soil_inorganic_nitrogen (double from, // [kg N/ha]
+					  double to) const = 0; 
+  virtual double second_year_utilization () const = 0;
+  // Current development stage for the crop named "crop", or
+  // Crop::DSremove if no such crop is present.
+  virtual double crop_ds (symbol crop) const = 0; 
+  // Drymatter in shoot [kg/ha], or negative if no such crop is present
+  virtual double crop_dm (symbol crop, double height) const = 0; 
+  // All names of all crops on the column.
+  virtual std::string crop_names () const = 0;
+  
+  // Simulation.
+  virtual void clear () = 0;
+  virtual void tick (Treelog&, const Time&, const Weather*) = 0;
+
+  virtual bool check (bool require_weather,
+		      const Time& from, const Time& to, 
+		      Treelog& err) const = 0;
+  virtual bool check_am (const AttributeList& am, Treelog& err) const = 0;
+  virtual bool check_border (const double border, Treelog& err) const = 0;
+  virtual void output (Log&) const;
+
+  // Communication with external model.
+  virtual unsigned int count_layers () const = 0; // Number of num. layers.
+  virtual double get_dz (unsigned int i) const = 0; // Size of layer 'i'. [cm]
+  virtual void put_water_pressure (const std::vector<double>& v) = 0; // [cm]
+  virtual void get_water_sink (std::vector<double>& v) 
+    const = 0; // [cm^3/cm^3/h]
+  virtual void put_no3_m (const std::vector<double>& v) = 0; // [g/cm^3]
+  virtual void get_no3_m (std::vector<double>& v) const = 0; // [g/cm^3]
+  virtual double get_evap_interception () const = 0; // [mm/h]
+  virtual double get_intercepted_water () const = 0; // [mm]
+  virtual double get_net_throughfall () const = 0; // [mm/h]
+  virtual double get_snow_storage () const = 0; // [mm]
+  virtual double get_exfiltration () const = 0; // [mm/h]
+  virtual double get_evap_soil_surface () const = 0; // [mm/h]
+  virtual void put_ponding (double pond) = 0;	// [mm]
+  virtual void put_surface_no3 (double no3) = 0; // [g/cm^2]
+  virtual double get_surface_no3 () const = 0; // [g/cm^2]
+  virtual void put_surface_chemical (symbol, double) = 0; // [g/cm^2]
+  virtual double get_surface_chemical (symbol) const = 0; // [g/cm^2]
+  virtual double get_smb_c_at (unsigned int i) const = 0; // [g C/cm³]
+  virtual double get_co2_production_at (unsigned int i) const = 0; // [g C/cm³]
+  virtual double get_temperature_at (unsigned int i) const = 0; // [°C]
+  virtual double get_crop_h2o_uptake_at (unsigned int i) const = 0; //[cm³/cm³]
+  virtual double get_water_content_at (unsigned int i) const = 0; // [cm³/cm³]
+
+  // Create and Destroy.
+protected:
+  Column (const AttributeList&);
+public:
+  static void load_syntax (Syntax&, AttributeList&);
+  virtual Column& clone (symbol) const = 0;
+  virtual void initialize (const Time&, Treelog& err, const Weather*) = 0;
+
+  virtual ~Column ();
+};
+
+#ifdef FORWARD_TEMPLATES
+EMPTY_TEMPLATE
+Librarian<Column>::Content* Librarian<Column>::content;
+#endif
+
+static Librarian<Column> Column_init ("column");
+
+#endif // COLUMN_H
---
+++ tags/release_3_81/main.C
@@ -0,0 +1,185 @@
+// main.C -- Run the Daisy model from the command line.
+//
+// Copyright 1996-2001 Per Abrahamsen.
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include "daisy.h"
+#include "parser.h"
+#include "syntax.h"
+#include "alist.h"
+#include "library.h"
+#include "treelog_dual.h"
+#include "options.h"
+#include "assertion.h"
+#include "tmpstream.h"
+#include <stdexcept>
+#include <typeinfo>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int
+main (int argc, char* argv[])
+{
+  // We don't use stdio.
+  ios::sync_with_stdio(false);
+
+  time_t start_time = time (NULL);
+#if defined (__unix) || defined (__CYGWIN__)
+  TreelogDual treelog ("daisy.log", cerr);
+#else // MSDOS
+  // stderr can't be redirected under MSDOS
+  TreelogDual treelog ("daisy.log", cout);
+#endif // MSDOS
+
+  Assertion::Register reg (treelog);
+
+  try
+    {
+      // Initialize syntax and attribute list.
+      Syntax syntax;
+      AttributeList alist;
+      Daisy::load_syntax (syntax, alist);
+      alist.add ("type", "Daisy");
+      Library::load_syntax (syntax, alist);
+      
+      syntax.add ("directory", Syntax::String, Syntax::OptionalConst,
+                  "Run program in this directory.\n\
+This can affect both where input files are found and where log files\n\
+are generated.");
+      syntax.add ("path", Syntax::String,
+                  Syntax::OptionalConst, Syntax::Sequence,
+                  "List of directories to search for input files in.\n\
+The special value \".\" means the current directory.");
+      syntax.add ("input", Librarian<Parser>::library (),
+                  Syntax::OptionalConst, Syntax::Singleton,
+                  "Command to add more information about the simulation.");
+      syntax.add ("run", Librarian<Program>::library (), 
+                  Syntax::OptionalState, Syntax::Singleton, 
+                  "Program to run.\n\
+\n\
+If this option is specified, all the 'Daisy' specific top-level attributes\n\
+will be ignored.  If unspecified, run 'Daisy' on the current top-level\n\
+attributes.");
+      Options options (argc, argv, syntax, alist, treelog);
+
+      switch (argc)
+	{
+	case -2:
+	  options.usage (treelog);
+	  return 2;
+	case -1:
+	  return 1;
+	case 0:
+	  return 0;
+	case 1:
+	  // Do nothing.
+	  break;
+	default:
+	  daisy_assert (false);
+	}
+
+      // Explicit or implicit program?
+      const Library& library = Librarian<Program>::library ();
+      const Syntax* run_syntax;
+      const AttributeList* run_alist;
+
+      if (alist.check ("run"))
+        {
+          run_alist = &alist.alist ("run");
+          daisy_assert (run_alist->check ("type"));
+          run_syntax = &library.syntax (run_alist->identifier ("type"));
+        }
+      else
+        {
+          run_alist = &alist;
+          run_syntax = &syntax;
+        }
+
+      // Create, check and run the program.
+      options.copyright (treelog);
+      if (!run_syntax->check (*run_alist, treelog))
+        return 1;
+      auto_ptr<Program> program (Librarian<Program>::create (*run_alist));
+      program->initialize (&syntax, &alist, treelog);
+      if (!program->check (treelog))
+	return 1;
+
+      const string when = string ("Program started ") + ctime (&start_time);
+      TmpStream start_msg;
+      start_msg () << when.substr (0, when.size () - 1);
+      const time_t time_ago = time (NULL) - start_time;
+      if (time_ago == 0)
+	start_msg () << ".";
+      if (time_ago == 1)
+	start_msg () << ", 1 second ago.";
+      else
+	start_msg () << ", " << time_ago << " seconds ago.";
+      treelog.message (start_msg.str ());
+
+      program->run (treelog);
+
+      const time_t time_used = time (NULL) - start_time;
+      const int hours = time_used / 3600;
+      const int minutes = (time_used % 3600) / 60;
+      const int seconds = time_used % 60;
+      TmpStream end_msg;
+      end_msg () << "Program finished after ";
+      if (hours == 1)
+	end_msg () << "1 hour, ";
+      else if (hours > 0)
+	end_msg () << hours << " hours, ";
+      if (minutes == 1)
+	end_msg () << " 1 minute and ";
+      else if (hours > 0 || minutes > 0)
+	end_msg () << minutes << " minutes and ";
+      if (seconds == 1)
+	end_msg () << "1 second.";
+      else
+	end_msg () << seconds << " seconds.";
+      treelog.message (end_msg.str ());
+      
+      // All is well.
+      return 0;
+    }
+  catch (const char* error)
+    {
+      treelog.error (string ("Exception: ") + error);
+    }
+  catch (const string& error)
+    {
+      treelog.error (string ("Exception raised: ") + error);
+    }
+  catch (const exception& e)
+    {
+      treelog.error (string ("Standard exception: ") + typeid (e).name ()
+		     + ": " + e.what ());
+    }
+  catch (const int error)
+    {
+      return error;
+    }
+  catch (...)
+    {
+      treelog.error ("Unknown exception");
+    }
+  return 1;
+}
+      
