9753e17563ca5831040e45cc393f614b974efabf
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-09-26 09:20:38 +0000

    This commit was manufactured by cvs2svn to create tag 'release_3_82'.

---
+++ tags/release_3_82/number.h
@@ -0,0 +1,56 @@
+// number.h --- Numbers in Daisy.
+// 
+// Copyright 2002 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef NUMBER_H
+#define NUMBER_H
+
+#include "librarian.h"
+#include <vector>
+
+class Scope;
+
+class Number
+{
+  // Content.
+public:
+  static const char *const description;
+  const symbol name;
+
+  // Simulation.
+public:
+  virtual double value (const Scope&) const = 0; 
+  virtual const std::string& dimension (const Scope&) const = 0;
+
+
+  // Create and Destroy.
+public:
+  Number (const AttributeList&);
+  virtual ~Number ();
+};
+
+#ifdef FORWARD_TEMPLATES
+EMPTY_TEMPLATE
+Librarian<Number>::Content* Librarian<Number>::content;
+#endif
+
+static Librarian<Number> Number_init ("number");
+
+#endif // NUMBER_H
---
+++ tags/release_3_82/version.C
@@ -0,0 +1,4 @@
+// version.C -- automatically generated file
+ 
+extern const char *const version = "3.82";
+extern const char *const version_date = __DATE__;
