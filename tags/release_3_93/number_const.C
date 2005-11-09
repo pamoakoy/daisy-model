641a5bd26193c144fc0ffa7e80350d54d93ddfd0
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-11-09 08:45:47 +0000

    This commit was manufactured by cvs2svn to create tag 'release_3_93'.

---
+++ tags/release_3_93/number_const.C
@@ -0,0 +1,431 @@
+// number_const.C -- Simple number objects.
+// 
+// Copyright 2004, 2005 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "number.h"
+#include "scope.h"
+#include "units.h"
+
+using namespace std;
+
+struct NumberConst : public Number
+{
+  // Parameters.
+  const double val;
+  const string dim;
+
+  // Simulation.
+  bool missing (const Scope&) const
+  { return false; }
+  double value (const Scope&) const
+  { return val; }
+  const string& dimension (const Scope&) const
+  { return dim; }
+
+  // Create.
+  bool check (const Scope&, Treelog&) const
+  { return true; }
+  NumberConst (Block& al)
+    : Number (al),
+      val (al.number ("value")),
+      dim (al.name ("value"))
+  { }
+};
+
+static struct NumberConstSyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberConst (al); }
+  NumberConstSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "Always give the specified value.");
+    syntax.add ("value", Syntax::User (), Syntax::Const,
+		"Fixed value for this number.");
+    syntax.order ("value");
+    Librarian<Number>::add_type ("const", alist, syntax, &make);
+  }
+} NumberConst_syntax;
+
+struct NumberLeaf : public Number
+{
+  // Parameters.
+  const string dim;
+
+  // Simulation.
+  const string& dimension (const Scope&) const
+  { return dim; }
+
+  // Create.
+  NumberLeaf (Block& al)
+    : Number (al),
+      dim (al.name ("dimension"))
+  { }
+};
+
+struct NumberGet : public NumberLeaf
+{
+  // Parameters.
+  const string name;
+  const std::string& title () const
+  { return name; }
+
+  // Simulation.
+  bool missing (const Scope& scope) const
+  { return !scope.has_number (name); }
+  double value (const Scope& scope) const
+  { 
+    daisy_assert (scope.has_number (name));
+    const double value = scope.number ( name);
+    const string got_dim = scope.dimension ( name);
+    return Units::convert (got_dim, dim, value);
+  }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& err) const
+  {
+    Treelog::Open nest (err, name);
+
+    bool ok = true;
+    if (!scope.has_number (name))
+      {
+        err.error ("'" + name + "' not in scope");
+        ok = false;
+      }
+    return ok;
+  }
+
+  NumberGet (Block& al)
+    : NumberLeaf (al),
+      name (al.name ("name"))
+  { }
+};
+
+static struct NumberGetSyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberGet (al); }
+  NumberGetSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "Get the value of symbol in the current scope.");
+    syntax.add ("name", Syntax::String, Syntax::Const, 
+                "Name of a the symbol.");
+    syntax.add ("dimension", Syntax::String, Syntax::Const, 
+                "Expected dimension for the symbol.");
+    syntax.order ("name", "dimension");
+    Librarian<Number>::add_type ("get", alist, syntax, &make);
+  }
+} NumberGet_syntax;
+
+struct NumberFetch : public Number
+{
+  // Parameters.
+  const string name;
+  const double *const default_value;
+  const std::string default_dimension;
+  const std::string& title () const
+  { return name; }
+
+  static double* fetch_default_value (Block& al, const string& key)
+  {
+    if (al.lookup (key) != Syntax::Number)
+      return NULL;
+    const AttributeList& alist = al.find_alist (key);
+    if (!alist.check (key))
+      return NULL;
+    const Syntax& syntax = al.find_syntax (key);
+    daisy_assert (syntax.lookup (key) == Syntax::Number);
+    if (syntax.size (key) != Syntax::Singleton)
+      {
+	al.msg ().warning ("Parameter '" + key + "' is a sequence, ignored");
+	return NULL;
+      }
+    return new double (alist.number (key));
+  }
+  static std::string fetch_default_dimension (Block& al, const string& key)
+  {
+    if (al.lookup (key) != Syntax::Number)
+      return Syntax::Unknown ();
+    const Syntax& syntax = al.find_syntax (key);
+    daisy_assert (syntax.lookup (key) == Syntax::Number);
+    const std::string dim = syntax.dimension (key);
+    if (dim == Syntax::User ())
+      {
+	const AttributeList& alist = al.find_alist (key);
+	if (alist.check (key))
+	  return alist.name (key);
+      }
+    return dim;
+  }
+  // Simulation.
+  bool missing (const Scope& scope) const
+  { return !default_value && !scope.has_number (name); }
+  double value (const Scope& scope) const
+  { 
+    if (scope.has_number (name))
+      return scope.number (name);
+    daisy_assert (default_value);
+    return *default_value;
+  }
+  const std::string& dimension (const Scope& scope) const
+  { 
+    if (scope.has_number (name))
+      return scope.dimension (name); 
+    return default_dimension;
+  }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& err) const
+  {
+    Treelog::Open nest (err, name);
+
+    bool ok = true;
+    if (!default_value && !scope.has_number (name))
+      {
+        err.error ("'" + name + "' not in scope");
+        ok = false;
+      }
+    return ok;
+  }
+  NumberFetch (Block& al)
+    : Number (al),
+      name (al.name ("name")),
+      default_value (fetch_default_value (al, name)),
+      default_dimension (fetch_default_dimension (al, name))
+  { }
+  ~NumberFetch ()
+  { delete default_value; }
+};
+
+static struct NumberFetchSyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberFetch (al); }
+  NumberFetchSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "Fetch the value and dimension in the current scope.");
+    syntax.add ("name", Syntax::String, Syntax::Const, 
+                "Name of a the symbol.");
+    syntax.order ("name");
+    Librarian<Number>::add_type ("fetch", alist, syntax, &make);
+  }
+} NumberFetch_syntax;
+
+struct NumberIdentity : public Number
+{
+  // Parameters.
+  const auto_ptr<Number> child;
+  const string dim;
+
+  // Simulation.
+  bool missing (const Scope& scope) const 
+  { return child->missing (scope) 
+      || (known (dim) && known (child->dimension (scope))
+          && !Units::can_convert (child->dimension (scope), dim, 
+                                  child->value (scope))); }
+  double value (const Scope& scope) const
+  { 
+    const double v = child->value (scope); 
+    if (known (dim) && known (child->dimension (scope)))
+      return Units::convert (child->dimension (scope), dim, v);
+    return v;
+  }
+  const string& dimension (const Scope& scope) const
+  {
+    if (known (dim))
+      return dim; 
+    return child->dimension (scope);
+  }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& err) const
+  { 
+    Treelog::Open nest (err, name);
+    bool ok = true;
+
+    if (!child->check (scope, err))
+      ok = false;
+    
+    if (known (dim) && known (child->dimension (scope))
+        && !Units::can_convert (child->dimension (scope), dim))
+      {
+        err.error ("Cannot convert [" + child->dimension (scope) 
+                   + "] to [" + dim + "]");
+        ok = false;
+      }
+    return ok;
+  }
+  NumberIdentity (Block& al)
+    : Number (al),
+      child (Librarian<Number>::build_item (al, "value")),
+      dim (al.name ("dimension", Syntax::Unknown ()))
+  { }
+};
+
+static struct NumberIdentitySyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberIdentity (al); }
+  NumberIdentitySyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", "Pass value unchanged.");
+    syntax.add ("value", Librarian<Number>::library (),
+		"Operand for this function.");
+    syntax.add ("dimension", Syntax::String, Syntax::OptionalConst,
+		"Dimension of this value.");
+    Librarian<Number>::add_type ("identity", alist, syntax, &make);
+  }
+} NumberIdentity_syntax;
+
+struct NumberConvert : public Number
+{
+  // Parameters.
+  const auto_ptr<Number> child;
+  const string dim;
+
+  // Simulation.
+  bool missing (const Scope& scope) const 
+  { return child->missing (scope) 
+      || !Units::can_convert (child->dimension (scope), dim, 
+                              child->value (scope)); }
+  double value (const Scope& scope) const
+  { 
+    const double v = child->value (scope); 
+    return Units::convert (child->dimension (scope), dim, v);
+  }
+  const string& dimension (const Scope&) const
+  { return dim; }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& err) const
+  { 
+    Treelog::Open nest (err, name);
+    bool ok = true;
+
+    if (!child->check (scope, err))
+      ok = false;
+    
+    if (!Units::can_convert (child->dimension (scope), dim))
+      {
+        err.error ("Cannot convert [" + child->dimension (scope) 
+                   + "] to [" + dim + "]");
+        ok = false;
+      }
+    return ok;
+  }
+  NumberConvert (Block& al)
+    : Number (al),
+      child (Librarian<Number>::build_item (al, "value")),
+      dim (al.name ("dimension"))
+  { }
+};
+
+static struct NumberConvertSyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberConvert (al); }
+  NumberConvertSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", "Convert to specified dimension.");
+    syntax.add ("value", Librarian<Number>::library (),
+		"Operand for this function.");
+    syntax.add ("dimension", Syntax::String, Syntax::Const,
+		"Dimension to convert to.");
+    syntax.order ("value", "dimension");
+    Librarian<Number>::add_type ("convert", alist, syntax, &make);
+  }
+} NumberConvert_syntax;
+
+struct NumberDim : public Number
+{
+  // Parameters.
+  const auto_ptr<Number> child;
+  const string dim;
+  const bool warn_known;
+
+  // Simulation.
+  bool missing (const Scope& scope) const 
+  { return child->missing (scope); }
+  double value (const Scope& scope) const
+  { return child->value (scope); }
+  const string& dimension (const Scope&) const
+  { return dim; }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& err) const
+  { 
+    Treelog::Open nest (err, name);
+    bool ok = true;
+
+    if (!child->check (scope, err))
+      ok = false;
+    
+    if (warn_known && known (child->dimension (scope)))
+      err.warning ("Dimension for child already known");
+
+    return ok;
+  }
+  NumberDim (Block& al)
+    : Number (al),
+      child (Librarian<Number>::build_item (al, "value")),
+      dim (al.name ("dimension")),
+      warn_known (al.flag ("warn_known"))
+  { }
+};
+
+static struct NumberDimSyntax
+{
+  static Number& make (Block& al)
+  { return *new NumberDim (al); }
+  NumberDimSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", "Specify dimension for number.");
+    syntax.add ("value", Librarian<Number>::library (),
+		"Operand for this function.");
+    syntax.add ("warn_known", Syntax::Boolean, Syntax::Const,
+                "Issue a warning if the dimensions is already known.");
+    alist.add ("warn_known", true);
+    syntax.add ("dimension", Syntax::String, Syntax::Const,
+		"Dimension to use.");
+    syntax.order ("value", "dimension");
+    Librarian<Number>::add_type ("dim", alist, syntax, &make);
+  }
+} NumberDim_syntax;
+
---
+++ tags/release_3_93/source_combine.C
@@ -0,0 +1,262 @@
+// source_combine.C -- Combine data sources for gnuplot interface 
+// 
+// Copyright 2005 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include "source.h"
+#include "number.h"
+#include "scope.h"
+#include "vcheck.h"
+#include "memutils.h"
+#include <sstream>
+
+// ScopeScource
+class ScopeSources : public Scope
+{
+  // Content.
+private:
+  const std::vector<Source*> source;
+  std::vector<size_t> index;
+public:
+  Time now;
+
+  // Interface.
+public:
+  bool has_number (const std::string& tag) const
+  {
+    for (size_t i = 0; i < source.size (); i++)
+      if (source[i]->title () == tag 
+	  && index[i] < source[i]->value ().size ()
+          && source[i]->time ().at (index[i]) == now)
+	return true;
+    
+    return false;
+  }
+  double number (const std::string& tag) const
+  {
+    for (size_t i = 0; i < source.size (); i++)
+      if (source[i]->title () == tag)
+	{
+	  daisy_assert (index[i] < source[i]->value ().size ());
+          daisy_assert (source[i]->time ().at (index[i]) == now);
+	  return source[i]->value ().at (index[i]);
+	}
+    daisy_assert (false);
+  }
+  const std::string& dimension (const std::string& tag) const
+  {
+    for (size_t i = 0; i < source.size (); i++)
+      if (source[i]->title () == tag)
+	return source[i]->dimension ();
+    daisy_assert (false);
+  }
+
+  // Propagate.
+  bool load (Treelog& msg)
+  {
+    daisy_assert (index.size () == source.size ());
+
+    // Propagate.
+    bool ok = true;
+    for (size_t i = 0; i < source.size (); i++)
+      {
+	std::ostringstream tmp;
+	tmp << "[" << i << "] " << source[i]->title ();
+	Treelog::Open nest (msg, tmp.str ());
+	if (!source[i]->load (msg))
+          ok = false;
+      }
+    first ();
+    return ok;
+  }
+
+  std::string with ()
+  {
+    if (source.size () > 0)
+      return source[0]->with ();
+    
+    // No sources => no dates => no data.
+    return "lines";
+  }
+
+  // Loop.
+  void first ()
+  { 
+    fill (index.begin (), index.end (), 0); 
+    now = Time (1, 1, 1, 0);
+    next ();
+  }
+
+  bool done ()
+  {
+    daisy_assert (index.size () == source.size ());
+    for (size_t i = 0; i < index.size (); i++)
+      if (index[i] < source[i]->value ().size ())
+        return false;
+
+    return true;
+  }
+      
+  void next ()
+  {
+    daisy_assert (index.size () == source.size ());
+    for (size_t i = 0; i < index.size (); i++)
+      if (index[i] < source[i]->time ().size ()
+          && source[i]->time ().at (index[i]) <= now)
+        index[i]++;
+    
+    now = Time (9999, 12, 31, 23);
+    
+    for (size_t i = 0; i < index.size (); i++)
+      if (index[i] < source[i]->time ().size ()
+          && source[i]->time ().at (index[i]) <= now)
+        now = source[i]->time ().at (index[i]);
+  }
+
+  // Create and Destroy.
+public:
+  ScopeSources (const std::vector<Source*>& s)
+    : source (s),
+      index (s.size (), 0),
+      now (1, 1, 1, 0)
+  { }
+  ~ScopeSources ()
+  { sequence_delete (source.begin (), source.end ()); }
+};
+
+struct SourceCombine : public Source
+{
+  // Content.
+  ScopeSources scope;
+  const std::auto_ptr<Number> expr;
+  const std::string title_;
+  std::string dimension_;
+  std::string with_;
+  const int style_;
+  std::vector<Time> times;
+  std::vector<double> values;
+
+  // Interface.
+public:
+  const std::string& with () const
+  { return with_; }
+  int style () const 
+  { return style_; }
+  const std::vector<Time>& time () const
+  { return times; }
+  const std::vector<double>& value () const
+  { return values; }
+  const std::vector<double>& ebar () const
+  { daisy_assert (false); }
+  const std::string& title () const
+  { return title_; }
+  const std::string& dimension () const 
+  { return dimension_; }
+
+  // Read. 
+public:
+ bool load (Treelog& msg);
+
+  // Create and Destroy.
+public:
+  explicit SourceCombine (Block& al);
+  ~SourceCombine ()
+  { }
+};
+
+bool
+SourceCombine::load (Treelog& msg)
+{
+  // Propagate.
+  scope.load (msg);
+
+  // Scope
+  if (!expr->check (scope, msg))
+    return false;
+
+  // Extract.
+  dimension_ = expr->dimension (scope);
+  if (with_ == "")
+    with_ = scope.with ();
+
+  // Read data.
+  for (scope.first (); !scope.done (); scope.next ())
+    {
+      if (!expr->missing (scope))
+        {
+          times.push_back (scope.now);
+          values.push_back (expr->value (scope));
+        }
+    }
+  daisy_assert (times.size () == values.size ());
+
+  // Done.
+  return true;
+}
+
+SourceCombine::SourceCombine (Block& al)
+  : Source (al),
+    scope (Librarian<Source>::build_vector (al, "source")),
+    expr (Librarian<Number>::build_item (al, "expr")),
+    title_ (al.name ("title", expr->title ())),
+    dimension_ ("UNINITIALIZED"),
+    with_ (al.name ("with", "")),
+    style_ (al.integer ("style", -1))
+{ }
+
+static struct SourceCombineSyntax
+{
+  static Source& make (Block& al)
+  { return *new SourceCombine (al); }
+
+  SourceCombineSyntax ()
+  { 
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    Source::load_syntax (syntax, alist);
+    alist.add ("description", 
+	       "Combine data from multiple sources with a single expression.");
+    syntax.add ("source", Librarian<Source>::library (), 
+		Syntax::State, Syntax::Sequence, "\
+List of sources for data.  The style information for the sources is\n\
+ignored, but the dates, title and value is used as specified by\n\
+'expr' to calculate the combines date and value pairs.");
+    syntax.add ("expr", Librarian<Number>::library (), 
+		Syntax::Const, Syntax::Singleton, "\
+Expression for calculating the value for this source for each row.\n\
+A row is any date found in any of the member of 'source'.  The\n\
+expression may refer to the value of each source by its title.");
+    syntax.add ("title", Syntax::String, Syntax::OptionalConst, "\
+Name of data legend in plot, by default the name of the 'expr' object.");
+  syntax.add ("with", Syntax::String, Syntax::OptionalConst, "\
+Specify 'points' to plot each point individually, or 'lines' to draw\n\
+lines between them.  By default, let the first source decide.");
+  static VCheck::Enum with ("lines", "points");
+  syntax.add_check ("with", with);
+  syntax.add ("style", Syntax::Integer, Syntax::OptionalConst, "\
+Style to use for this dataset.  By default, gnuplot will use style 1\n\
+for the first source to plot with lines, style 2 for the second, and\n\
+so forth until it runs out of styles and has to start over.  Points\n\
+work similar, but with its own style counter.  For color plots, points\n\
+and lines with the same style number also have the same color.");
+    
+    Librarian<Source>::add_type ("combine", alist, syntax, &make);
+  }
+} SourceCombine_syntax;
+
+// source_std.C ends here.
