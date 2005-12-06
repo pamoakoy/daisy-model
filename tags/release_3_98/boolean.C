4f13d48be1b4bd01f4268c344cd1b7700bf5d47f
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-12-06 15:14:36 +0000

    This commit was manufactured by cvs2svn to create tag 'release_3_98'.

---
+++ tags/release_3_98/boolean.C
@@ -0,0 +1,311 @@
+// boolean.C --- Booleans in Daisy.
+// 
+// Copyright 2005 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "boolean.h"
+#include "memutils.h"
+#include <vector>
+
+template<>
+Librarian<Boolean>::Content* Librarian<Boolean>::content = NULL;
+
+const char *const Boolean::description = "\
+Generic representation of booleans.";
+
+const std::string& 
+Boolean::title () const
+{ return name.name (); }
+
+Boolean::Boolean (Block& al)
+  : name (al.identifier ("type"))
+{ }
+
+Boolean::~Boolean ()
+{ }
+
+struct BooleanStringEqual : public Boolean
+{
+  // Parameters.
+  const std::vector<std::string> values;
+
+  // Simulation.
+  bool missing (const Scope&) const
+  { return false; }
+  bool value (const Scope&) const
+  { 
+    if (values.size () < 2)
+      return true;
+    const std::string first = values[0];
+    for (size_t i = 1; i < values.size (); i++)
+      if (first != values[i])
+	return false; 
+    return true;
+  }
+
+  // Create.
+  bool check (const Scope&, Treelog&) const
+  { return true; }
+  BooleanStringEqual (Block& al)
+    : Boolean (al),
+      values (al.name_sequence ("values"))
+  { }
+};
+
+static struct BooleanStringEqualSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanStringEqual (al); }
+  BooleanStringEqualSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "True iff the supplied strings are identical.");
+    syntax.add ("values", Syntax::String, Syntax::Const, Syntax::Sequence,
+		"Strings to compare.");
+    syntax.order ("values");
+    Librarian<Boolean>::add_type ("string-equal", alist, syntax, &make);
+  }
+} BooleanStringEqual_syntax;
+
+struct BooleanTrue : public Boolean
+{
+  // Simulation.
+  bool missing (const Scope&) const
+  { return false; }
+  bool value (const Scope&) const
+  { return true; }
+
+  // Create.
+  bool check (const Scope&, Treelog&) const
+  { return true; }
+  BooleanTrue (Block& al)
+    : Boolean (al)
+  { }
+};
+
+static struct BooleanTrueSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanTrue (al); }
+  BooleanTrueSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "Always true.");
+    Librarian<Boolean>::add_type ("true", alist, syntax, &make);
+  }
+} BooleanTrue_syntax;
+
+
+struct BooleanFalse : public Boolean
+{
+  // Simulation.
+  bool missing (const Scope&) const
+  { return false; }
+  bool value (const Scope&) const
+  { return false; }
+
+  // Create.
+  bool check (const Scope&, Treelog&) const
+  { return true; }
+  BooleanFalse (Block& al)
+    : Boolean (al)
+  { }
+};
+
+static struct BooleanFalseSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanFalse (al); }
+  BooleanFalseSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+
+    alist.add ("description", 
+	       "Always false.");
+    Librarian<Boolean>::add_type ("false", alist, syntax, &make);
+  }
+} BooleanFalse_syntax;
+
+struct BooleanOperands : public Boolean
+{
+  const std::vector<const Boolean*> operand;
+
+  // Simulation.
+  bool missing (const Scope& scope) const
+  { 
+    for (size_t i = 0; i < operand.size (); i++)
+      if (operand[i]->missing (scope))
+        return true;
+    
+    return false;
+  }
+
+  // Create.
+  bool check (const Scope& scope, Treelog& msg) const
+  { 
+    bool ok = true;
+
+    for (size_t i = 0; i < operand.size (); i++)
+      if (!operand[i]->check (scope, msg))
+        ok = false;
+
+    return ok;
+
+    return true; }
+
+  static void load_syntax (Syntax& syntax, AttributeList&)
+  {
+    syntax.add ("operands", Librarian<Boolean>::library (), 
+                Syntax::Const, Syntax::Sequence, "\
+List of operands to compare.");
+    syntax.order ("operands");
+  }
+  BooleanOperands (Block& al)
+    : Boolean (al),
+      operand (Librarian<Boolean>::build_vector_const (al, "operands"))
+  { }
+  ~BooleanOperands ()
+  { sequence_delete (operand.begin (), operand.end ()); }
+};
+
+struct BooleanAnd : public BooleanOperands 
+{
+  bool value (const Scope& scope) const
+  { 
+    for (size_t i = 0; i < operand.size (); i++)
+      if (!operand[i]->value (scope))
+        return false;
+    return true;
+  }
+  BooleanAnd (Block& al)
+    : BooleanOperands (al)
+  { }
+};
+
+static struct BooleanAndSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanAnd (al); }
+  BooleanAndSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    BooleanOperands::load_syntax (syntax, alist);
+    alist.add ("description", 
+	       "True if and only if all operands are true.");
+    Librarian<Boolean>::add_type ("and", alist, syntax, &make);
+  }
+} BooleanAnd_syntax;
+
+
+struct BooleanOr : public BooleanOperands 
+{
+  bool value (const Scope& scope) const
+  { 
+    for (size_t i = 0; i < operand.size (); i++)
+      if (operand[i]->value (scope))
+        return true;
+    return false;
+  }
+  BooleanOr (Block& al)
+    : BooleanOperands (al)
+  { }
+};
+
+static struct BooleanOrSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanOr (al); }
+  BooleanOrSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    BooleanOperands::load_syntax (syntax, alist);
+    alist.add ("description", 
+	       "True if and only if any operand is true.");
+    Librarian<Boolean>::add_type ("on", alist, syntax, &make);
+  }
+} BooleanOr_syntax;
+
+struct BooleanXOr : public BooleanOperands 
+{
+  bool value (const Scope& scope) const
+  { 
+    daisy_assert (operand.size () == 2);
+    return operand[0]->value (scope) != operand[1]->value (scope);
+  }
+  BooleanXOr (Block& al)
+    : BooleanOperands (al)
+  { }
+};
+
+static struct BooleanXOrSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanXOr (al); }
+  BooleanXOrSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    syntax.add ("operands", Librarian<Boolean>::library (), 
+                Syntax::Const, 2, "\
+The two operands to compare.");
+    syntax.order ("operands");
+    alist.add ("description", 
+	       "True if and only if one operand is true, and one false.");
+    Librarian<Boolean>::add_type ("xor", alist, syntax, &make);
+  }
+} BooleanXOr_syntax;
+
+struct BooleanNot : public BooleanOperands 
+{
+  bool value (const Scope& scope) const
+  { 
+    daisy_assert (operand.size () == 1);
+    return !operand[0]->value (scope);
+  }
+  BooleanNot (Block& al)
+    : BooleanOperands (al)
+  { }
+};
+
+static struct BooleanNotSyntax
+{
+  static Boolean& make (Block& al)
+  { return *new BooleanNot (al); }
+  BooleanNotSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    syntax.add ("operands", Librarian<Boolean>::library (), 
+                Syntax::Const, 1, "\
+The operand to check.");
+    syntax.order ("operands");
+    alist.add ("description", 
+	       "True if and only if the operand is not true.");
+    Librarian<Boolean>::add_type ("not", alist, syntax, &make);
+  }
+} BooleanNot_syntax;
