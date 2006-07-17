09bcb6d2d35a4e82eca2580364adbe742dbd9d89
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2006-07-17 09:54:24 +0000

    This commit was manufactured by cvs2svn to create tag 'release_4_07'.

---
+++ tags/release_4_07/action_while.C
@@ -0,0 +1,114 @@
+// action_while.C
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "action.h"
+#include "log.h"
+#include "memutils.h"
+
+using namespace std;
+
+struct ActionWhile : public Action
+{
+  const vector<Action*> actions;
+
+  void tick (const Daisy& daisy, Treelog& out)
+  { 
+    for (unsigned int i = 0; i < actions.size (); i++)
+      actions[i]->tick (daisy, out);
+  }
+
+  void doIt (Daisy& daisy, Treelog& out)
+  { 
+    for (unsigned int i = 0; i < actions.size (); i++)
+      actions[i]->doIt (daisy, out);
+  }
+
+  bool done (const Daisy& daisy) const
+  {
+    daisy_assert (actions.size () != 0U);
+    return (actions[0]->done (daisy)); 
+  }
+
+  void output (Log& log) const
+  { 
+    output_list (actions, "actions", log, 
+		 Librarian<Action>::library ());
+  }
+
+  bool check (const Daisy& daisy, Treelog& err) const
+  { 
+    Treelog::Open nest (err, "while");
+    bool ok = true;
+    for (vector<Action*>::const_iterator i = actions.begin ();
+	 i != actions.end ();
+	 i++)
+      {
+	if (!(*i)->check (daisy, err))
+	  ok = false;
+      }
+    return ok;
+  }
+
+  ActionWhile (Block& al)
+    : Action (al),
+      actions (Librarian<Action>::build_vector (al, "actions"))
+  { }
+
+  ~ActionWhile ()
+  { 
+    sequence_delete (actions.begin (), actions.end ());
+  }
+};
+
+static struct ActionWhileSyntax
+{
+  static Action& make (Block& al)
+  { return *new ActionWhile (al); }
+
+  static bool check_alist (const AttributeList& al, Treelog& err)
+  {
+    bool ok = true;
+
+    if (al.size ("actions") < 1)
+      {
+	err.entry ("You must specify at least one action");
+	ok = false;
+      }
+    return ok;
+  }
+
+  ActionWhileSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    syntax.add_check (check_alist);
+    alist.add ("description", "\
+Perform all the specified actions in the sequence listed, but in the\n\
+same timestep.  The 'while' action is done when the first action in the\n\
+list is done.");
+    syntax.add ("actions", Librarian<Action>::library (), 
+                Syntax::State, Syntax::Sequence,
+		"List of actions to perform.");
+    syntax.order ("actions");
+    Librarian<Action>::add_type ("while", alist, syntax, &make);
+  }
+} ActionWhile_syntax;
---
+++ tags/release_4_07/treelog.C
@@ -0,0 +1,109 @@
+// treelog.C -- Log hierarchical information.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "treelog.h"
+#include <sstream>
+
+using namespace std;
+
+std::string 
+sequence_id (std::string key, size_t index)
+{
+  std::ostringstream tmp;
+  tmp << key << "[" << index << "]";
+  return tmp.str ();
+}
+
+Treelog::Open::Open (Treelog& l, const symbol name)
+  : log (l)
+{ log.open (name.name ()); }
+
+Treelog::Open::Open (Treelog& l, const string& name)
+  : log (l)
+{ log.open (name); }
+
+Treelog::Open::~Open ()
+{ log.close (); }
+
+void
+Treelog::open (const symbol name)
+{ open (name.name ()); }
+
+void
+Treelog::debug (const string&)
+{ }
+
+void
+Treelog::entry (const string&)
+{ count++; }
+
+void
+Treelog::message (const string& str)
+{ entry (str); }
+
+void
+Treelog::warning (const string& str)
+{ entry (str + " (warning)"); }
+
+void
+Treelog::error (const string& str)
+{ entry (str + " (error)"); }
+
+void
+Treelog::lazy (const string& str)
+{ entry (str); }
+
+class TreelogNull : public Treelog
+{
+  // Nesting.
+public:
+  void open (const string&)
+  { }
+  void close ()
+  { }
+
+  // Use.
+  void touch ()
+  { }
+  void flush ()
+  { }
+
+  // Create and Destroy.
+public:
+  TreelogNull ()
+  { }
+  ~TreelogNull ()
+  { }
+};
+
+static TreelogNull nulllog;
+
+Treelog&
+Treelog::null ()
+{ return nulllog; }
+
+Treelog::Treelog ()
+  : count (0)  
+{ }
+
+Treelog::~Treelog ()
+{ }
