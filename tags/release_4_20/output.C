89417e3fe4efde1825c91ee1eed521b79b5a8ae1
nobody <nobody@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2007-08-24 08:59:49 +0000

    This commit was manufactured by cvs2svn to create tag 'release_4_20'.

---
+++ tags/release_4_20/output.C
@@ -0,0 +1,207 @@
+// output.C -- Handle output from a Daisy simulation.
+// 
+// Copyright 2007 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#define BUILD_DLL
+
+#include "output.h"
+#include "daisy.h"
+#include "log_all.h"
+#include "log_extern.h"
+#include "treelog.h"
+#include "time.h"
+#include "timestep.h"
+#include "syntax.h"
+#include "assertion.h"
+#include "librarian.h"
+
+void
+Output::initial_logs (const Daisy& daisy, Treelog& msg)
+{
+  activate_output->tick (daisy, Scope::null (), msg);
+
+  if (activate_output->match (daisy, Scope::null (), msg))
+    {
+      if (!logging)
+	{
+	  msg.message ("Start logging");
+	  // get initial values for previous day.
+	  const Time previous = daisy.time - *(daisy.timestep);
+	  for (size_t i = 0; i < active_logs.size (); i++)
+	    {
+	      Log& log = *active_logs[i];
+	      if (log.initial_match (daisy, msg))
+		{
+		  output_submodule (previous, "time", log);
+                  daisy.output (log);
+                  output_list (logs, "output", log, Log::component);
+		  log.initial_done (previous, daisy.dt);
+		}
+	    }
+	}
+      logging = true;
+    }
+  else if (logging)
+    {
+      msg.message ("End logging");
+      logging = false;
+    }
+}
+
+void
+Output::tick (const Daisy& daisy, Treelog& msg)
+{
+  if (logging)
+    {
+      for (size_t i = 0; i < active_logs.size (); i++)
+	{
+	  Log& log = *active_logs[i];
+	  if (log.match (daisy, msg))
+	    {
+	      output_submodule (daisy.time, "time", log);
+              daisy.output (log);
+              output_list (logs, "output", log, Log::component);
+	      log.done (daisy.time, daisy.dt);
+	    }
+	}
+    }
+}
+
+void
+Output::summarize (Treelog& msg) const
+{
+  // Print log file summaries at end of simulation.
+  {
+    Treelog::Open nest (msg, "Summary");
+    for (size_t i = 0; i < logs.size (); i++)
+      logs[i]->summarize (msg);
+  }
+}
+
+size_t
+Output::scope_size () const
+{ return scopes.size (); }
+
+Scope&
+Output::scope (size_t i) const
+{ 
+  daisy_assert (i < scopes.size ());
+  return *scopes[i];
+}
+
+bool
+Output::check (const Border& field, Treelog& msg)
+{
+  bool ok = true;
+
+  for (std::vector<Log*>::const_iterator i = logs.begin ();
+       i != logs.end ();
+       i++)
+    {
+      if (*i == NULL || !(*i)-> check (field, msg))
+        ok = false;
+    }
+
+  return ok;
+}
+
+void
+Output::initialize (const Metalib& metalib, Treelog& msg)
+{
+  for (size_t i = 0; i < logs.size (); i++)
+    logs[i]->initialize_common (metalib, msg);
+  log_all->initialize_common (metalib, msg);
+}
+
+void 
+Output::add_log (Log* log)
+{ 
+  if (LogSelect* sel = dynamic_cast<LogSelect*> (log))
+    log_all->attach_log (sel);
+  else
+    active_logs.push_back (log); 
+}
+
+const std::vector<Log*> 
+Output::find_active_logs (const std::vector<Log*>& logs, LogAll& log_all)
+{
+  std::vector<Log*> result;
+
+  for (size_t i = 0; i < logs.size (); i++)
+    if (!dynamic_cast<LogSelect*> (logs[i]))
+      result.push_back (logs[i]);
+  
+  result.push_back (&log_all);
+  
+  return result;
+}
+
+const std::vector<Scope*> 
+Output::find_extern_logs (const std::vector<Log*>& logs, 
+                          const std::vector<Scope*>& exchanges)
+{
+  std::vector<Scope*> result = exchanges;
+
+  for (size_t i = 0; i < logs.size (); i++)
+    if (LogExtern* log = dynamic_cast<LogExtern*> (logs[i]))
+      result.push_back (log);
+  
+  return result;
+}
+
+Output::Output (Block& al)
+  : logging (false),
+    exchanges (Librarian::build_vector<Scope> (al, "exchange")),
+    logs (Librarian::build_vector<Log> (al, "output")),
+    log_all (new LogAll (logs)),
+    active_logs (find_active_logs (logs, *log_all)),
+    scopes (find_extern_logs (logs, exchanges)),
+    activate_output (Librarian::build_item<Condition> (al, "activate_output"))
+{ }
+
+Output::Output ()
+  : logging (false),
+    exchanges (std::vector<Scope*> ()),
+    logs (std::vector<Log*> ()),
+    active_logs (std::vector<Log*> ()),
+    scopes (std::vector<Scope*> ())
+{ }
+
+Output::~Output ()
+{ }
+
+void
+Output::load_syntax (Syntax& syntax, AttributeList& alist)
+{
+  syntax.add_object ("output", Log::component,
+                     Syntax::State, Syntax::Sequence,
+                     "List of logs for output during the simulation.");
+  syntax.add_object ("activate_output", Condition::component,
+                     "Activate output logs when this condition is true.\n\
+You can use the 'after' condition to avoid logging during an initialization\n\
+period.");
+  AttributeList true_alist;
+  true_alist.add ("type", "true");
+  alist.add ("activate_output", true_alist);
+
+  syntax.add_object ("exchange", Scope::component,
+                     Syntax::Const, Syntax::Sequence, "\
+List of exchange items for communicating with external models.");
+  alist.add ("exchange", std::vector<AttributeList*> ());
+}
---
+++ tags/release_4_20/pedo.C
@@ -0,0 +1,95 @@
+// pedo.C --- Pedotransfer functions based on soil attributes.
+// 
+// Copyright 2004 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#define BUILD_DLL
+
+#include "pedo.h"
+#include "soil.h"
+#include "units.h"
+#include "block.h"
+#include "syntax.h"
+#include <sstream>
+#include "treelog.h"
+#include "librarian.h"
+
+using namespace std;
+
+const char *const Pedotransfer::component = "pedotransfer";
+
+void 
+Pedotransfer::set (const Soil& soil, vector<double>& array,
+                   const string& dim) const
+{
+  array.insert (array.end (), soil.size () - array.size (), 0.0);
+
+  for (unsigned int i = 0; i < soil.size (); i++)
+    if (known (dim) && known (dimension ()))
+      array[i] = Units::convert (dimension (), dim, value (soil, i));
+    else
+      array[i] = value (soil, i);
+}
+
+
+bool
+Pedotransfer::check (const Soil& soil, const string& dim, Treelog& msg) const
+{ 
+  Treelog::Open nest (msg, name);
+  bool ok = true;
+
+  if (known (dim) && known (dimension ())
+      && !Units::can_convert (dimension (), dim))
+    {
+      msg.error ("Cannot convert [" + dimension () + "] to [" + dim + "]");
+      ok = false;
+    }
+  
+  if (!check_nested (soil, msg))
+    ok = false;
+
+  return ok; 
+}
+
+bool 
+Pedotransfer::known (const std::string& dim)
+{ return dim != Syntax::Unknown () && (dim.size () < 1 || dim[0] != '?'); }
+
+void 
+Pedotransfer::debug_message (const string& name,
+                             const vector<double>& value, 
+                             const string& dim,
+                             Treelog& msg)
+{ 
+  std::ostringstream tmp;
+  tmp << "(" << name;
+  for (unsigned int i = 0; i < value.size (); i++)
+    tmp << " " << value[i];
+  tmp << " [" << dim << "])";
+  msg.debug (tmp.str ());
+}
+
+Pedotransfer::Pedotransfer (Block& al)
+  : name (al.identifier ("type"))
+{ }
+
+Pedotransfer::~Pedotransfer ()
+{ }
+
+static Librarian Pedotransfer_init (Pedotransfer::component, "\
+Pedotransfer functions based on soil attributes.");
