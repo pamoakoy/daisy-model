e68ed7b7f65736d08df97bcf7067a22a87109948
per.abrahamsen <per.abrahamsen@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2008-07-08 12:01:00 +0000

    New release

---
+++ tags/release_4_34/tertiary.C
@@ -0,0 +1,156 @@
+// tertiary.C --- Transport of water and solute outside the matrix.
+// 
+// Copyright 2008 Per Abrahamsen and KU.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#define BUILD_DLL
+
+#include "tertiary.h"
+#include "geometry.h"
+#include "soil_water.h"
+#include "block.h"
+#include "alist.h"
+#include "librarian.h"
+#include "anystate.h"
+
+// The 'tertiary' component.
+
+const char *const Tertiary::component = "tertiary";
+
+symbol
+Tertiary::library_id () const
+{
+  static const symbol id (component);
+  return id;
+}
+
+Anystate
+Tertiary::get_state () const
+{ return Anystate::none (); }
+
+void
+Tertiary::set_state (const Anystate&)
+{ }
+
+bool 
+Tertiary::converge (const Anystate&)
+{ return true; }
+
+bool
+Tertiary::use_small_timesteps ()
+{ return false; }
+
+void 
+Tertiary::matrix_sink (const Geometry& geo, const Soil& soil,  
+                       const SoilHeat& soil_heat, 
+                       const std::vector<double>& h,
+                       std::vector<double>& S_matrix,
+                       std::vector<double>& S_drain) const
+{ }
+
+bool 
+Tertiary::find_implicit_water (const Anystate& old_state, 
+                               const Geometry& geo, 
+                               const Soil& soil,  
+                               const SoilHeat& soil_heat, 
+                               const std::vector<double>& h,
+                               const double dt)
+{ throw "Implicit tertiary water solution not supported"; }
+
+void
+Tertiary::update_active (const std::vector<double>& h_matrix)
+{ }
+
+Tertiary::Tertiary (Block& al)
+  : ModelAListed (al.alist ())
+{ }
+
+Tertiary::Tertiary (const symbol n)
+  : ModelAListed (n)
+{ }
+
+Tertiary::~Tertiary ()
+{ }
+
+static Librarian Tertiary_init (Tertiary::component, "\
+Transport of water and solute outside the matrix.");
+
+// The 'none' model.
+
+class TertiaryNone : public Tertiary
+{
+  // Identity.
+  bool has_macropores ()
+  { return false; }
+
+  // Simulation.
+  void tick (const Geometry&, const Soil&, const SoilHeat&,
+             const double dt, SoilWater&, Surface&, Treelog&)
+  { }
+  void solute (const Geometry&, const SoilWater&, 
+               const std::map<size_t, double>& J_tertiary,
+               const double /* dt */,
+               Chemical&, Treelog&)
+  { }
+  void output (Log&) const
+  { }
+
+  // Create and Destroy.
+  bool initialize (const Geometry&, const Soil&, const Scope&,  
+                   const double /* pipe_position */, Treelog&)
+  { return true; }
+  bool check (const Geometry&, Treelog&) const
+  { return true; }
+public:
+  TertiaryNone (Block& al)
+    : Tertiary (al)
+  { }
+  TertiaryNone (const symbol n)
+    : Tertiary (n)
+  { }
+};
+
+static struct TertiaryNoneSyntax
+{
+  static Model& make (Block& al)
+  { return *new TertiaryNone (al); }
+  TertiaryNoneSyntax ()
+  {
+    Syntax& syntax = *new Syntax ();
+    AttributeList& alist = *new AttributeList ();
+    alist.add ("description", "No tertiary transport.");
+    Librarian::add_type (Tertiary::component, "none", alist, syntax, &make);
+  }
+} TertiaryNone_syntax;
+
+Tertiary&
+Tertiary::none ()
+{
+  static TertiaryNone none (symbol ("none"));
+  return none;
+}
+
+const AttributeList&
+Tertiary::none_model ()
+{
+  static AttributeList alist;
+  if (!alist.check ("type"))
+    alist.add ("type", "none");
+  return alist;
+}
+// tertiary.C ends here.
---
+++ tags/release_4_34/tertiary.h
@@ -0,0 +1,99 @@
+// tertiary.h --- Transport of water and solutes outside the matrix.
+// 
+// Copyright 2008 Per Abrahamsen and KU.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef TERTIARY_H
+#define TERTIARY_H
+
+#include "model.h"
+#include <map>
+
+class Block;
+class Geometry;
+class Soil;
+class SoilWater;
+class SoilHeat;
+class Surface;
+class Chemical;
+class Scope;
+class Treelog;
+class Log;
+class Anystate;
+
+class Tertiary : public ModelAListed
+{
+  // Identity.
+public:
+  static const char *const component;
+  symbol library_id () const;
+  virtual bool has_macropores () = 0;
+
+  virtual Anystate get_state () const;
+  virtual void set_state (const Anystate&);
+  virtual bool converge (const Anystate& old); // Are current, old state close?
+
+  // Simulation.
+public:
+  virtual bool use_small_timesteps ();
+
+public:
+  // - For use by Column.
+  virtual void tick (const Geometry&, const Soil&, const SoilHeat&,
+                     const double dt, SoilWater&, Surface&, Treelog&) = 0;
+
+public:
+  // - For use inside Richard's Equation.
+  virtual void matrix_sink (const Geometry& geo, const Soil& soil,  
+                            const SoilHeat& soil_heat, 
+                            const std::vector<double>& h,
+                            std::vector<double>& S_matrix,
+                            std::vector<double>& S_drain) const;
+  virtual bool find_implicit_water (const Anystate& old_state, 
+                                    const Geometry& geo, 
+                                    const Soil& soil,  
+                                    const SoilHeat& soil_heat, 
+                                    const std::vector<double>& h,
+                                    const double dt);
+  virtual void update_active (const std::vector<double>& h_matrix);
+
+  // - For use in Movement::solute
+  virtual void solute (const Geometry&, const SoilWater&, 
+                       const std::map<size_t, double>& J_tertiary,
+                       const double dt,
+                       Chemical&, Treelog&) = 0;
+
+  // - Output
+  virtual void output (Log&) const = 0;
+
+  // Create and Destroy.
+public:
+  static Tertiary& none ();
+  static const AttributeList& none_model ();
+  static const AttributeList& old_model ();
+  virtual bool initialize (const Geometry&, const Soil&, const Scope&, 
+                           const double pipe_position, Treelog&) = 0;
+  virtual bool check (const Geometry&, Treelog&) const = 0;
+protected:
+  explicit Tertiary (Block& al);
+  explicit Tertiary (const symbol name);
+public:
+  virtual ~Tertiary ();
+};
+
+#endif // TERTIARY_H
